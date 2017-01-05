e76eeb661ad20bc57b9d148b96788b3ec1a4b2ee
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2017-01-05 16:59:01 +0100

    Initializing values can (and should) be done with this syntax

--- word.cpp
+++ word.cpp
@@ -1,22 +1,25 @@
 #include "word.h"
 
+// Image word
 Word::Word(LazyLoadedImage* image, Type type, const QString& copytext, const QString& tooltip)
+    : m_image(image)
+    , m_text()
+    , m_isImage(true)
+    , m_type(type)
+    , m_copyText(copytext)
+    , m_tooltip(tooltip)
 {
-    this->m_image = image;
-    m_isImage = true;
-    m_type = type;
-    m_copyText = copytext;
-    m_tooltip = tooltip;
 }
 
+// Text word
 Word::Word(const QString& text, Type type, const QString& copytext, const QString& tooltip)
+    : m_image(nullptr)
+    , m_text(text)
+    , m_isImage(true)
+    , m_type(type)
+    , m_copyText(copytext)
+    , m_tooltip(tooltip)
 {
-    this->m_image = NULL;
-    this->m_text = text;
-    m_isImage = false;
-    m_type = type;
-    m_copyText = copytext;
-    m_tooltip = tooltip;
 }
 
 Word::~Word()
