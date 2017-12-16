b5bb49e8e5d11ed5304866ae8c7099895ea6732f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2017-12-16 01:57:34 +0100

    Add method for executing lambda in QObject's thread

--- src/asyncexec.hpp
+++ src/asyncexec.hpp
@@ -25,3 +25,30 @@ public:
 private:
     std::function<void()> action;
 };
+
+// Taken from
+// https://stackoverflow.com/questions/21646467/how-to-execute-a-functor-or-a-lambda-in-a-given-thread-in-qt-gcd-style
+// Qt 5/4 - preferred, has least allocations
+template <typename F>
+static void postToThread(F &&fun, QObject *obj = qApp)
+{
+    struct Event : public QEvent {
+        using Fun = typename std::decay<F>::type;
+        Fun fun;
+        Event(Fun &&fun)
+            : QEvent(QEvent::None)
+            , fun(std::move(fun))
+        {
+        }
+        Event(const Fun &fun)
+            : QEvent(QEvent::None)
+            , fun(fun)
+        {
+        }
+        ~Event()
+        {
+            fun();
+        }
+    };
+    QCoreApplication::postEvent(obj, new Event(std::forward<F>(fun)));
+}
