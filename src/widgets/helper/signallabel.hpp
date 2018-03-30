1c7f397f1bcc8dfb94fe5f1f7304960cfaa2a903
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-03-30 14:39:33 +0200

    Move signallabel code out from its header

--- src/widgets/helper/signallabel.hpp
+++ src/widgets/helper/signallabel.hpp
@@ -10,11 +10,8 @@ class SignalLabel : public QLabel
     Q_OBJECT
 
 public:
-    explicit SignalLabel(QWidget *parent = 0, Qt::WindowFlags f = 0)
-        : QLabel(parent, f)
-    {
-    }
-    virtual ~SignalLabel() = default;
+    explicit SignalLabel(QWidget *parent = nullptr, Qt::WindowFlags f = 0);
+    ~SignalLabel() override = default;
 
 signals:
     void mouseDoubleClick(QMouseEvent *ev);
@@ -23,31 +20,8 @@ signals:
     void mouseUp();
 
 protected:
-    virtual void mouseDoubleClickEvent(QMouseEvent *ev) override
-    {
-        emit this->mouseDoubleClick(ev);
-    }
-
-    virtual void mousePressEvent(QMouseEvent *event) override
-    {
-        if (event->button() == Qt::LeftButton) {
-            emit mouseDown();
-        }
-
-        event->ignore();
-    }
-
-    void mouseReleaseEvent(QMouseEvent *event) override
-    {
-        if (event->button() == Qt::LeftButton) {
-            emit mouseUp();
-        }
-
-        event->ignore();
-    }
-
-    virtual void mouseMoveEvent(QMouseEvent *event) override
-    {
-        event->ignore();
-    }
+    void mouseDoubleClickEvent(QMouseEvent *ev) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
 };
