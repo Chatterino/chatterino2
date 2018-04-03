1119c081826f1d1bcb5fb57b323482ffec4023fa
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-04-03 02:35:02 +0200

    Simplify debug::Log. No need for a second function

--- src/debug/log.hpp
+++ src/debug/log.hpp
@@ -8,19 +8,11 @@
 namespace chatterino {
 namespace debug {
 
-namespace detail {
-
-static void _log(const std::string &message)
-{
-    qDebug().noquote() << QTime::currentTime().toString("hh:mm:ss.zzz") << message.c_str();
-}
-
-}  // namespace detail
-
 template <typename... Args>
 inline void Log(const std::string &formatString, Args &&... args)
 {
-    detail::_log(fS(formatString, std::forward<Args>(args)...));
+    qDebug().noquote() << QTime::currentTime().toString("hh:mm:ss.zzz")
+                       << fS(formatString, std::forward<Args>(args)...).c_str();
 }
 
 }  // namespace debug
