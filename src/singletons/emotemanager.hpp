47814875616c5a74243622a7b719d40f03fd450a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-06-05 19:00:25 +0200

    Remove unused code

--- src/singletons/emotemanager.hpp
+++ src/singletons/emotemanager.hpp
@@ -2,14 +2,11 @@
 
 #define GIF_FRAME_LENGTH 33
 
-#include "messages/image.hpp"
 #include "providers/bttv/bttvemotes.hpp"
 #include "providers/emoji/emojis.hpp"
 #include "providers/ffz/ffzemotes.hpp"
 #include "providers/twitch/twitchemotes.hpp"
 #include "singletons/helper/giftimer.hpp"
-#include "util/concurrentmap.hpp"
-#include "util/emotemap.hpp"
 
 #include <QString>
 
@@ -21,20 +18,14 @@ class EmoteManager
 public:
     ~EmoteManager() = delete;
 
+    void initialize();
+
     providers::twitch::TwitchEmotes twitch;
     providers::bttv::BTTVEmotes bttv;
     providers::ffz::FFZEmotes ffz;
     providers::emoji::Emojis emojis;
 
     GIFTimer gifTimer;
-
-    void initialize();
-
-    util::EmoteData getCheerImage(long long int amount, bool animated);
-
-    // Bit badge/emotes?
-    // TODO: Move to twitch emote provider
-    util::ConcurrentMap<QString, messages::Image *> miscImageCache;
 };
 
 }  // namespace singletons
