434ace985511c0c17ac66e274a85ed59a9f84f4e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-06-05 19:01:33 +0200

    Remove old emoji parsing test code
    it can be recovered from the repo if we decide to make a test suite eShrug

--- src/singletons/emotemanager.cpp
+++ src/singletons/emotemanager.cpp
@@ -3,9 +3,6 @@
 #include "application.hpp"
 #include "controllers/accounts/accountcontroller.hpp"
 
-using namespace chatterino::providers::twitch;
-using namespace chatterino::messages;
-
 namespace chatterino {
 namespace singletons {
 
@@ -26,61 +23,3 @@ void EmoteManager::initialize()
 
 }  // namespace singletons
 }  // namespace chatterino
-
-#if 0
-namespace chatterino {
-
-void EmojiTest()
-{
-    auto &emoteManager = singletons::EmoteManager::getInstance();
-
-    emoteManager.loadEmojis();
-
-    {
-        std::vector<std::tuple<util::EmoteData, QString>> dummy;
-
-        // couple_mm 1f468-2764-1f468
-        // "\154075\156150❤\154075\156150"
-        // [0]            55357    0xd83d    QChar
-        // [1]            56424    0xdc68    QChar
-        // [2]    '❤'     10084    0x2764    QChar
-        // [3]            55357    0xd83d    QChar
-        // [4]            56424    0xdc68    QChar
-        QString text = "👨❤👨";
-
-        emoteManager.parseEmojis(dummy, text);
-
-        assert(dummy.size() == 1);
-    }
-
-    {
-        std::vector<std::tuple<util::EmoteData, QString>> dummy;
-
-        // "✍\154074\157777"
-        // [0]    '✍'     9997    0x270d    QChar
-        // [1]            55356    0xd83c    QChar
-        // [2]            57343    0xdfff    QChar
-        QString text = "✍🏿";
-
-        emoteManager.parseEmojis(dummy, text);
-
-        assert(dummy.size() == 1);
-
-        assert(std::get<0>(dummy[0]).isValid());
-    }
-
-    {
-        std::vector<std::tuple<util::EmoteData, QString>> dummy;
-
-        QString text = "✍";
-
-        emoteManager.parseEmojis(dummy, text);
-
-        assert(dummy.size() == 1);
-
-        assert(std::get<0>(dummy[0]).isValid());
-    }
-}
-
-}  // namespace chatterino
-#endif
