e3ea112257d10601ea2caf5b39160204776930ed
pajlada <rasmus.karlsson@pajlada.com>
2018-06-16 11:08:58 +0200

    oops

--- src/widgets/helper/splitheader.cpp
+++ src/widgets/helper/splitheader.cpp
@@ -387,8 +387,6 @@ void SplitHeader::menuReloadChannelEmotes()
     TwitchChannel *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
 
     if (twitchChannel) {
-        delete twitchChannel;
-        twitchChannel = nullptr;
         twitchChannel->reloadChannelEmotes();
     }
 }
