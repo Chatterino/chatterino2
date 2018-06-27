4e2dbc4f0738762564d0f89628d33473b9ba3e49
Lajamerr Mittesdine <danieljmerritt4394@gmail.com>
2018-06-27 02:35:00 -0400

    Update update-tld-data.sh

--- tools/update-tld-data.sh
+++ tools/update-tld-data.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt' -O tlds.txt
-sed '1d' tlds.txt | sed -e 's/\(.*\)/\L\1/' > tlds.txt
-sed -n -e '/^xn--/p' tlds.txt | sed 's/^\(xn--\)*//'  | idn -d >> tlds.txt
+curl -s 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt' | sed -e '1d' -e 's/\(.*\)/\L\1/' > tlds.txt
+sed -n -e '/^xn--/p' tlds.txt | idn2 -d >> tlds.txt
 mv tlds.txt ../resources/tlds.txt
