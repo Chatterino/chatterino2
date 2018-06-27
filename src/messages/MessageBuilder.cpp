4e05cdef7007defca447ea05d0d11ab2013cd50e
LajamerrMittesdine <danieljmerritt4394@gmail.com>
2018-06-27 13:45:54 -0400

    Initial rework of matchLink

--- src/messages/MessageBuilder.cpp
+++ src/messages/MessageBuilder.cpp
@@ -43,9 +43,44 @@ void MessageBuilder::appendTimestamp(const QTime &time)
 
 QString MessageBuilder::matchLink(const QString &string)
 {
-    static QRegularExpression linkRegex("[[:ascii:]]*\\.[a-zA-Z]+\\/?[[:ascii:]]*");
+    QFile tldFile(":/tlds.txt");
+    tldFile.open(QFile::ReadOnly);
+    QTextStream t1(&tldFile);
+    t1.setCodec("UTF-8");
+    QString tldData = t1.readAll();
+    tldData.replace("\n", "|");
+    const QString urlRegExp = "^"
+    // protocol identifier
+    "(?:(?:https?|ftps?)://)?"
+    // user:pass authentication
+    "(?:\\S+(?::\\S*)?@)?"
+    "(?:"
+    // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broacast addresses
+    // (first & last IP address of each class)
+    "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])"
+    "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}"
+    "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))"
+    "|"
+    // host name
+    "(?:(?:[_a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)"
+    // domain name
+    "(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*"
+    // TLD identifier
+    //"(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,}))"
+    "(?:[\\.](?:" + tldData + "))"
+    "\\.?"
+    ")"
+    // port number
+    "(?::\\d{2,5})?"
+    // resource path
+    "(?:[/?#]\\S*)?"
+    "$";
+    static QRegularExpression linkRegex(urlRegExp, QRegularExpression::CaseInsensitiveOption);
     static QRegularExpression httpRegex("\\bhttps?://");
-
+    static QRegularExpression ftpRegex("\\bftps?://");
     auto match = linkRegex.match(string);
 
     if (!match.hasMatch()) {
@@ -55,7 +90,9 @@ QString MessageBuilder::matchLink(const QString &string)
     QString captured = match.captured();
 
     if (!captured.contains(httpRegex)) {
-        captured.insert(0, "http://");
+        if (!captured.contains(ftpRegex)) {
+            captured.insert(0, "http://");
+        }
     }
 
     return captured;
