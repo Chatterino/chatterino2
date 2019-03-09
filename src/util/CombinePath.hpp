1c78521555c3477110a8f7c66a8921fd7465476a
fourtf <tf.four@gmail.com>
2019-03-09 20:02:06 +0100

    minor changes

--- src/util/CombinePath.hpp
+++ src/util/CombinePath.hpp
@@ -3,12 +3,11 @@
 #include <QDir>
 #include <QString>
 
-namespace chatterino {
-
-// https://stackoverflow.com/a/13014491
-inline QString combinePath(const QString &a, const QString &b)
+namespace chatterino
 {
-    return QDir::cleanPath(a + QDir::separator() + b);
-}
-
+    // https://stackoverflow.com/a/13014491
+    inline QString combinePath(const QString& a, const QString& b)
+    {
+        return QDir::cleanPath(a + QDir::separator() + b);
+    }
 }  // namespace chatterino
