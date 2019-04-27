d749a4771a97a1083c9a49a1a67f94dfb1e1f17e
TranRed <kupfer.nils@gmail.com>
2019-04-27 10:29:21 +0200

    made CommonTexts.hpp look like other headers

--- src/widgets/helper/CommonTexts.hpp
+++ src/widgets/helper/CommonTexts.hpp
@@ -1,10 +1,6 @@
-#ifndef COMMONTEXTS_HPP
-#define COMMONTEXTS_HPP
+#pragma once
 
 #define OPEN_IN_BROWSER "Open in browser"
 #define OPEN_PLAYER_IN_BROWSER "Open in player in browser"
 #define OPEN_IN_STREAMLINK "Open in streamlink"
 #define DONT_OPEN "Don't open"
-
-
-#endif  // COMMONTEXTS_HPP
