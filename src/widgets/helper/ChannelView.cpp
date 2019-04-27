a3f6d2b773c87e398da74ad77bcdd96413e98536
23rd <23rd@vivaldi.net>
2019-04-27 15:42:51 +0300

    Fixed display of text in Go to Bottom button for Retina screen.

--- src/widgets/helper/ChannelView.cpp
+++ src/widgets/helper/ChannelView.cpp
@@ -294,8 +294,12 @@ void ChannelView::scaleChangedEvent(float scale)
 
     if (this->goToBottom_)
     {
+        auto factor = this->qtFontScale();
+#ifdef Q_OS_MACOS
+        factor = scale * 80.f / this->logicalDpiX() * this->devicePixelRatioF();
+#endif
         this->goToBottom_->getLabel().setFont(
-            getFonts()->getFont(FontStyle::UiMedium, this->qtFontScale()));
+            getFonts()->getFont(FontStyle::UiMedium, factor));
     }
 }
 
