3bb40a0458c15a131a8f0d7afdcc25aec5e6b203
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-08-11 20:35:25 +0200

    Try to fix appimage

--- .CI/CreateAppImage.sh
+++ .CI/CreateAppImage.sh
@@ -2,7 +2,27 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/qt512/lib/
 ldd ./bin/chatterino
 make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
 cp ./resources/icon.png ./appdir/chatterino.png
+
 wget -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -no-translations -bundle-non-qt-libs -unsupported-allow-new-glibc -appimage -qmake=/opt/qt512/bin/qmake
-mv Chatterino-*-x86_64.AppImage Chatterino-x86_64.AppImage
+wget -nv "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
+./linuxdeployqt-continuous-x86_64.AppImage \
+    appdir/usr/share/applications/*.desktop \
+    -no-translations \
+    -bundle-non-qt-libs \
+    -unsupported-allow-new-glibc \
+    -qmake=/opt/qt512/bin/qmake
+
+rm -rf appdir/home
+rm appdir/AppRun
+
+# shellcheck disable=SC2016
+echo '#!/bin/sh
+here="$(dirname "$(readlink -f "${0}")")"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$here/usr/plugins"
+cd "$here/usr"
+exec "$here/usr/bin/chatterino" "$@"' > appdir/AppRun
+chmod a+x appdir/AppRun
+
+./appimagetool-x86_64.AppImage appdir
