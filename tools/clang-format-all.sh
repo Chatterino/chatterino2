0e923b1e33edaaae8bcb024a34d3b9e13a8d1a8c
Leon Richardt <leon.richardt@gmail.com>
2019-09-24 23:02:00 +0200

    Fix clang-format only reformatting .cpp files

--- tools/clang-format-all.sh
+++ tools/clang-format-all.sh
@@ -3,5 +3,5 @@
 read -p "Are you sure you want to run clang-format on all files in src/? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    find src/ -iname "*.hpp" -o -iname "*.cpp" -exec clang-format -i {} \;
+    find src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
 fi
