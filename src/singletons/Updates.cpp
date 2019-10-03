30587e18c92fa3328044c7667adeb3be025a1207
fourtf <tf.four@gmail.com>
2019-10-03 20:36:22 +0200

    test #2

--- src/singletons/Updates.cpp
+++ src/singletons/Updates.cpp
@@ -207,8 +207,6 @@ void Updates::checkForUpdates()
     }
 #endif
 
-    return;
-
     QString url =
         "https://notitia.chatterino.com/version/chatterino/" CHATTERINO_OS "/" +
         currentBranch();
@@ -316,7 +314,7 @@ void Updates::setStatus_(Status status)
     if (this->status_ != status)
     {
         this->status_ = status;
-        postToThread([this, status] { this->statusUpdated.invoke(status); });
+        //postToThread([this, status] { this->statusUpdated.invoke(status); });
     }
 }
 
