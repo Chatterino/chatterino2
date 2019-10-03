375aa221eba943d96c4d6e007d9f8894acd03852
fourtf <tf.four@gmail.com>
2019-10-03 10:13:49 +0200

    messed up the operator

--- src/util/QStringHash.hpp
+++ src/util/QStringHash.hpp
@@ -5,7 +5,7 @@
 
 namespace std {
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 template <>
 struct hash<QString> {
     std::size_t operator()(const QString &s) const
