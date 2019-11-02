27935510402a89f1033e99c6789c453723f6052a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-02 11:59:04 +0100

    add dockerfile for checking formatting

---
+++ tools/check-format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+fail="0"
+
+while read -r file; do
+    if ! diff -u <(cat "$file") <(clang-format "$file"); then
+        echo "$file differs!!!!!!!"
+        fail="1"
+    fi
+done < <(find src/ \( -iname "*.hpp" -o -iname "*.cpp" \))
+
+if [ "$fail" = "1" ]; then
+    echo "At least one file is poorly formatted - check the output above"
+    exit 1
+fi
+
+echo "Everything seems to be formatted properly! Good job"
