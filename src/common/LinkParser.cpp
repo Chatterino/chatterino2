8f1df38224add958abf153a86be1942d2f9258c9
Robinlemon <lewis-gibson@hotmail.com>
2019-12-27 19:51:18 +0000

    Fixes URL regular expression

--- src/common/LinkParser.cpp
+++ src/common/LinkParser.cpp
@@ -9,9 +9,27 @@ namespace chatterino {
 
 LinkParser::LinkParser(const QString &unparsedString)
 {
+    /**
+     *  (
+     *      https?:\\/\\/ <-- Must be http(s)
+     *      (?:
+     *          [-;:&=\\+\\$,\\w]+      <-- Username (optional)
+     *          (?:
+     *              @[-;:&=\\+\\$,\\w]+    <-- If a username is present, match an optional "@passsword"
+     *          )?
+     *      )?
+     *      (?:[\\w-]+\\.){1,}    <-- Infinite domain capture group "a." (eg: a.b.c. is valid)
+     *      [\\w-]+        <-- Matches the TLD (.com, .net, etc.). The `.` before the TLD is present from the match above.
+     *      (?::\\d+)? <-- Port (optional)
+     *      (?:\\/[^\\/]*) <-- Infinite path matching (/a/b/c), no traling slash
+     *      *\\/? <-- Optional trailing slash
+     *  )
+     */
+
     static QRegularExpression linkRegex(
-        "^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/"
-        "?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$",
+        "(https?:\\/\\/"
+        "(?:[-;:&=\\+\\$,\\w]+(?:@[-;:&=\\+\\$,\\w]+)?)?(?:[\\w-]+\\.){1,}[\\w-"
+        "]+(?::\\d+)?(?:\\/[^\\/]*)*\\/?)",
         QRegularExpression::CaseInsensitiveOption);
 
     this->match_ = linkRegex.match(unparsedString);
