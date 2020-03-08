386d58fca20535befaaa738f58c28d5b0d4ac7de
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-03-08 14:13:46 +0100

    make map of colors nicer

--- src/messages/MessageElement.cpp
+++ src/messages/MessageElement.cpp
@@ -11,113 +11,59 @@
 
 namespace chatterino {
 
-static auto IRC_COLORS = [] {
-    static QMap<int, QColor> x;
-    if (x.isEmpty())
-    {
-        // Colors taken from https://modern.ircdocs.horse/formatting.html
-        x.insert(0, QColor("white"));
-        x.insert(1, QColor("black"));
-        x.insert(2, QColor("blue"));
-        x.insert(3, QColor("green"));
-        x.insert(4, QColor("red"));
-        x.insert(5, QColor("brown"));
-        x.insert(6, QColor("purple"));
-        x.insert(7, QColor("orange"));
-        x.insert(8, QColor("yellow"));
-        x.insert(9, QColor("lightgreen"));
-        x.insert(10, QColor("cyan"));
-        x.insert(11, QColor("lightcyan"));
-        x.insert(12, QColor("lightblue"));
-        x.insert(13, QColor("pink"));
-        x.insert(14, QColor("gray"));
-        x.insert(15, QColor("lightgray"));
-        x.insert(16, QColor("#470000"));
-        x.insert(17, QColor("#472100"));
-        x.insert(18, QColor("#474700"));
-        x.insert(19, QColor("#324700"));
-        x.insert(20, QColor("#004700"));
-        x.insert(21, QColor("#00472c"));
-        x.insert(22, QColor("#004747"));
-        x.insert(23, QColor("#002747"));
-        x.insert(24, QColor("#000047"));
-        x.insert(25, QColor("#2e0047"));
-        x.insert(26, QColor("#470047"));
-        x.insert(27, QColor("#47002a"));
-        x.insert(28, QColor("#740000"));
-        x.insert(29, QColor("#743a00"));
-        x.insert(30, QColor("#747400"));
-        x.insert(31, QColor("#517400"));
-        x.insert(32, QColor("#007400"));
-        x.insert(33, QColor("#007449"));
-        x.insert(34, QColor("#007474"));
-        x.insert(35, QColor("#004074"));
-        x.insert(36, QColor("#000074"));
-        x.insert(37, QColor("#4b0074"));
-        x.insert(38, QColor("#740074"));
-        x.insert(39, QColor("#740045"));
-        x.insert(40, QColor("#b50000"));
-        x.insert(41, QColor("#b56300"));
-        x.insert(42, QColor("#b5b500"));
-        x.insert(43, QColor("#7db500"));
-        x.insert(44, QColor("#00b500"));
-        x.insert(45, QColor("#00b571"));
-        x.insert(46, QColor("#00b5b5"));
-        x.insert(47, QColor("#0063b5"));
-        x.insert(48, QColor("#0000b5"));
-        x.insert(49, QColor("#7500b5"));
-        x.insert(50, QColor("#b500b5"));
-        x.insert(51, QColor("#b5006b"));
-        x.insert(52, QColor("#ff0000"));
-        x.insert(53, QColor("#ff8c00"));
-        x.insert(54, QColor("#ffff00"));
-        x.insert(55, QColor("#b2ff00"));
-        x.insert(56, QColor("#00ff00"));
-        x.insert(57, QColor("#00ffa0"));
-        x.insert(58, QColor("#00ffff"));
-        x.insert(59, QColor("#008cff"));
-        x.insert(60, QColor("#0000ff"));
-        x.insert(61, QColor("#a500ff"));
-        x.insert(62, QColor("#ff00ff"));
-        x.insert(63, QColor("#ff0098"));
-        x.insert(64, QColor("#ff5959"));
-        x.insert(65, QColor("#ffb459"));
-        x.insert(66, QColor("#ffff71"));
-        x.insert(67, QColor("#cfff60"));
-        x.insert(68, QColor("#6fff6f"));
-        x.insert(69, QColor("#65ffc9"));
-        x.insert(70, QColor("#6dffff"));
-        x.insert(71, QColor("#59b4ff"));
-        x.insert(72, QColor("#5959ff"));
-        x.insert(73, QColor("#c459ff"));
-        x.insert(74, QColor("#ff66ff"));
-        x.insert(75, QColor("#ff59bc"));
-        x.insert(76, QColor("#ff9c9c"));
-        x.insert(77, QColor("#ffd39c"));
-        x.insert(78, QColor("#ffff9c"));
-        x.insert(79, QColor("#e2ff9c"));
-        x.insert(80, QColor("#9cff9c"));
-        x.insert(81, QColor("#9cffdb"));
-        x.insert(82, QColor("#9cffff"));
-        x.insert(83, QColor("#9cd3ff"));
-        x.insert(84, QColor("#9c9cff"));
-        x.insert(85, QColor("#dc9cff"));
-        x.insert(86, QColor("#ff9cff"));
-        x.insert(87, QColor("#ff94d3"));
-        x.insert(88, QColor("#000000"));
-        x.insert(89, QColor("#131313"));
-        x.insert(90, QColor("#282828"));
-        x.insert(91, QColor("#363636"));
-        x.insert(92, QColor("#4d4d4d"));
-        x.insert(93, QColor("#656565"));
-        x.insert(94, QColor("#818181"));
-        x.insert(95, QColor("#9f9f9f"));
-        x.insert(96, QColor("#bcbcbc"));
-        x.insert(97, QColor("#e2e2e2"));
-        x.insert(98, QColor("#ffffff"));
-    }
-    return x;
-}();
+// Colors taken from https://modern.ircdocs.horse/formatting.html
+static QMap<int, QColor> IRC_COLORS = {
+    {0, QColor("white")},      {1, QColor("black")},
+    {2, QColor("blue")},       {3, QColor("green")},
+    {4, QColor("red")},        {5, QColor("brown")},
+    {6, QColor("purple")},     {7, QColor("orange")},
+    {8, QColor("yellow")},     {9, QColor("lightgreen")},
+    {10, QColor("cyan")},      {11, QColor("lightcyan")},
+    {12, QColor("lightblue")}, {13, QColor("pink")},
+    {14, QColor("gray")},      {15, QColor("lightgray")},
+    {16, QColor("#470000")},   {17, QColor("#472100")},
+    {18, QColor("#474700")},   {19, QColor("#324700")},
+    {20, QColor("#004700")},   {21, QColor("#00472c")},
+    {22, QColor("#004747")},   {23, QColor("#002747")},
+    {24, QColor("#000047")},   {25, QColor("#2e0047")},
+    {26, QColor("#470047")},   {27, QColor("#47002a")},
+    {28, QColor("#740000")},   {29, QColor("#743a00")},
+    {30, QColor("#747400")},   {31, QColor("#517400")},
+    {32, QColor("#007400")},   {33, QColor("#007449")},
+    {34, QColor("#007474")},   {35, QColor("#004074")},
+    {36, QColor("#000074")},   {37, QColor("#4b0074")},
+    {38, QColor("#740074")},   {39, QColor("#740045")},
+    {40, QColor("#b50000")},   {41, QColor("#b56300")},
+    {42, QColor("#b5b500")},   {43, QColor("#7db500")},
+    {44, QColor("#00b500")},   {45, QColor("#00b571")},
+    {46, QColor("#00b5b5")},   {47, QColor("#0063b5")},
+    {48, QColor("#0000b5")},   {49, QColor("#7500b5")},
+    {50, QColor("#b500b5")},   {51, QColor("#b5006b")},
+    {52, QColor("#ff0000")},   {53, QColor("#ff8c00")},
+    {54, QColor("#ffff00")},   {55, QColor("#b2ff00")},
+    {56, QColor("#00ff00")},   {57, QColor("#00ffa0")},
+    {58, QColor("#00ffff")},   {59, QColor("#008cff")},
+    {60, QColor("#0000ff")},   {61, QColor("#a500ff")},
+    {62, QColor("#ff00ff")},   {63, QColor("#ff0098")},
+    {64, QColor("#ff5959")},   {65, QColor("#ffb459")},
+    {66, QColor("#ffff71")},   {67, QColor("#cfff60")},
+    {68, QColor("#6fff6f")},   {69, QColor("#65ffc9")},
+    {70, QColor("#6dffff")},   {71, QColor("#59b4ff")},
+    {72, QColor("#5959ff")},   {73, QColor("#c459ff")},
+    {74, QColor("#ff66ff")},   {75, QColor("#ff59bc")},
+    {76, QColor("#ff9c9c")},   {77, QColor("#ffd39c")},
+    {78, QColor("#ffff9c")},   {79, QColor("#e2ff9c")},
+    {80, QColor("#9cff9c")},   {81, QColor("#9cffdb")},
+    {82, QColor("#9cffff")},   {83, QColor("#9cd3ff")},
+    {84, QColor("#9c9cff")},   {85, QColor("#dc9cff")},
+    {86, QColor("#ff9cff")},   {87, QColor("#ff94d3")},
+    {88, QColor("#000000")},   {89, QColor("#131313")},
+    {90, QColor("#282828")},   {91, QColor("#363636")},
+    {92, QColor("#4d4d4d")},   {93, QColor("#656565")},
+    {94, QColor("#818181")},   {95, QColor("#9f9f9f")},
+    {96, QColor("#bcbcbc")},   {97, QColor("#e2e2e2")},
+    {98, QColor("#ffffff")},
+};
 
 MessageElement::MessageElement(MessageElementFlags flags)
     : flags_(flags)
