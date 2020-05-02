0befb0bc914f0aabc96e30d3ebc7c89f27b4d198
cinemassacres <cinemassacres@gmail.com>
2020-05-02 10:06:05 -0600

    Create FFZBadges.cpp

---
+++ src/singletons/FFZBadges.cpp
@@ -0,0 +1,40 @@
+{
+  "badges": [
+    {
+      "alpha_image": "//cdn.frankerfacez.com/script/devtransicon.png",
+      "color": "#FAAF19",
+      "css": null,
+      "id": 1,
+      "image": "//cdn.frankerfacez.com/script/devicon.png",
+      "name": "developer",
+      "replaces": null,
+      "slot": 5,
+      "title": "FFZ Developer"
+    },
+    {
+      "alpha_image":null,
+      "color": "#595959",
+      "css": null,
+      "id": 2,
+      "image": "//cdn.frankerfacez.com/script/boticon.png",
+      "name": "bot",
+      "replaces": "moderator",
+      "slot": 1,
+      "title": "Bot"
+    },
+    {
+      "alpha_image":null,
+      "color": "#755000",
+      "css": null,
+      "id": 3,
+      "image": "//cdn.frankerfacez.com/script/devicon.png",
+      "name": "supporter",
+      "replaces": null,
+      "slot": 5,
+      "title": "FFZ Supporter"
+    }
+  ],
+  "users": {
+      ...
+  }
+}
