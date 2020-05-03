8f2b958cfbc0579a6964053b1d3adebdad8cb713
gempir <daniel.pasch.s@gmail.com>
2020-05-03 12:45:27 +0200

    lint

--- src/providers/LinkResolver.hpp
+++ src/providers/LinkResolver.hpp
@@ -3,16 +3,17 @@
 #include <QString>
 #include <functional>
 
-#include "messages/Link.hpp"
 #include "messages/Image.hpp"
+#include "messages/Link.hpp"
 
 namespace chatterino {
 
 class LinkResolver
 {
 public:
-    static void getLinkInfo(const QString url, QObject *caller,
-                            std::function<void(QString, Link, ImagePtr)> callback);
+    static void getLinkInfo(
+        const QString url, QObject *caller,
+        std::function<void(QString, Link, ImagePtr)> callback);
 };
 
 }  // namespace chatterino
