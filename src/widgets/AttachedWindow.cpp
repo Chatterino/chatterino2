de9b3b418a65946630a123575f69f86dbddf5a18
ALazymeme <jkeasley@icloud.com>
2020-06-01 20:02:01 +1000

    Update AttachedWindow.cpp

--- src/widgets/AttachedWindow.cpp
+++ src/widgets/AttachedWindow.cpp
@@ -267,10 +267,10 @@ void AttachedWindow::updateWindowRect(void *_attachedPtr)
         this->ui_.split->setFixedWidth(int(this->width_ * scale));
 
         // offset
-        int o = this->fullscreen_ ? 0;
+        int o = this->fullscreen_ ? 0 : 8;
 
         ::MoveWindow(hwnd, int(rect.right - this->width_ * scale - o),
-                     int(rect.bottom - this->height_ * scale - o) + 4,
+                     int(rect.bottom - this->height_ * scale - o) + 100,
                      int(this->width_ * scale) - 5,
                      int(this->height_ * scale) - 5, true);
     }
