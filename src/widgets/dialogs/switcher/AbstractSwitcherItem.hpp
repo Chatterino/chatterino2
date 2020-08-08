96b007e697eec734f2f5b1790a90c31cb6f2c2b1
Leon Richardt <leon.richardt@gmail.com>
2020-08-08 23:20:12 +0200

    Add documentation for AbstractSwitcherItem

--- src/widgets/dialogs/switcher/AbstractSwitcherItem.hpp
+++ src/widgets/dialogs/switcher/AbstractSwitcherItem.hpp
@@ -6,19 +6,29 @@ class AbstractSwitcherItem
 {
 public:
     /**
-     * Attempt to obtain an AbstractSwitcherItem * from the passed QVariant.
+     * @brief   Attempt to obtain an AbstractSwitcherItem * from the passed QVariant.
      *
-     * @param variant   variant to try to convert to AbstractSwitcherItem *
+     * @param   variant variant to try to convert to AbstractSwitcherItem *
      *
-     * @return an AbstractSwitcherItem * if the QVariant could be converted,
-     *         or nullptr if the variant did not contain AbstractSwitcherItem *
+     * @return  an AbstractSwitcherItem * if the QVariant could be converted,
+     *          or nullptr if the variant did not contain AbstractSwitcherItem *
      */
     static AbstractSwitcherItem *fromVariant(const QVariant &variant);
 
     virtual ~AbstractSwitcherItem() = default;
 
+    /**
+     * @brief   Since all switcher items are required to have an icon, we require it
+     *          in the base class constructor.
+     *
+     * @param   icon    icon to be displayed in the switcher list
+     */
     AbstractSwitcherItem(const QIcon &icon);
 
+    /**
+     * @brief   Action to perform when this item is activated. Must be implemented in
+     *          subclasses.
+     */
     virtual void action() = 0;
 
     virtual void paint(QPainter *painter, const QRect &rect) const = 0;
@@ -31,4 +41,5 @@ protected:
 
 }  // namespace chatterino
 
+// This allows us to store AbstractSwitcherItem * as a QVariant
 Q_DECLARE_METATYPE(chatterino::AbstractSwitcherItem *);
