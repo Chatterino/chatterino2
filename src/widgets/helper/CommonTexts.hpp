8661c833123d06e2b8229fa3609487996a57911d
fourtf <tf.four@gmail.com>
2020-08-13 17:36:45 +0200

    updated descriptions in notification settings

--- src/widgets/helper/CommonTexts.hpp
+++ src/widgets/helper/CommonTexts.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#define OPEN_IN_BROWSER "Open in browser"
+#define OPEN_IN_BROWSER "Open stream in browser"
 #define OPEN_PLAYER_IN_BROWSER "Open player in browser"
 #define OPEN_IN_STREAMLINK "Open in streamlink"
 #define DONT_OPEN "Don't open"
