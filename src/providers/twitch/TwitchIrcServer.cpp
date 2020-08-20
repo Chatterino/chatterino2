4c473461c105c27657764a53caa1e363e73af2c1
Auro <35087590+MrAuro@users.noreply.github.com>
2020-08-20 15:28:12 -0400

    Changed too fast to too quickly

--- src/providers/twitch/TwitchIrcServer.cpp
+++ src/providers/twitch/TwitchIrcServer.cpp
@@ -336,7 +336,7 @@ void TwitchIrcServer::onMessageSendRequested(TwitchChannel *channel,
             if (this->lastErrorTimeSpeed_ + 30s < now)
             {
                 auto errorMessage =
-                    makeSystemMessage("You are sending messages too fast.");
+                    makeSystemMessage("You are sending messages too quickly.");
 
                 channel->addMessage(errorMessage);
 
