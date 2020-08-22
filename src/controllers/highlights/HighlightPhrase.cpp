f3225275ea1f02a25adf7bedf040b4bbf5708e91
fourtf <tf.four@gmail.com>
2020-08-22 16:48:40 +0200

    changed channel point messages color

--- src/controllers/highlights/HighlightPhrase.cpp
+++ src/controllers/highlights/HighlightPhrase.cpp
@@ -11,7 +11,7 @@ namespace {
 
 QColor HighlightPhrase::FALLBACK_HIGHLIGHT_COLOR = QColor(127, 63, 73, 127);
 QColor HighlightPhrase::FALLBACK_REDEEMED_HIGHLIGHT_COLOR =
-    QColor(28, 126, 141, 90);
+    QColor(28, 126, 141, 60);
 QColor HighlightPhrase::FALLBACK_SUB_COLOR = QColor(196, 102, 255, 100);
 
 bool HighlightPhrase::operator==(const HighlightPhrase &other) const
