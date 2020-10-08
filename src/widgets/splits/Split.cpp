4565ae8041efbe2676e9334e481b82c4aa55b299
David Myers <dave-myers@outlook.com>
2020-10-07 22:15:50 -0400

    fix formatting

--- src/widgets/splits/Split.cpp
+++ src/widgets/splits/Split.cpp
@@ -314,7 +314,7 @@ void Split::setChannel(IndirectChannel newChannel)
     this->header_->updateChannelText();
     this->header_->updateRoomModes();
 
-    if(newChannel.getType() == Channel::Type::Twitch)
+    if (newChannel.getType() == Channel::Type::Twitch)
     {
         this->header_->setViewersButtonVisible(true);
     }
