e82e0d77d488315345eac7597073e9ae5adeae31
David Myers <dave-myers@outlook.com>
2020-10-15 23:03:54 -0400

    formatting

--- src/widgets/splits/Split.cpp
+++ src/widgets/splits/Split.cpp
@@ -634,11 +634,11 @@ void Split::showViewerList()
         return item;
     };
 
-    static QStringList labels = {"Broadcaster", "Moderators", "VIPs",
-                                 "Staff", "Admins", "Global Moderators",
-                                 "Viewers"};
+    static QStringList labels = {
+        "Broadcaster", "Moderators",        "VIPs",   "Staff",
+        "Admins",      "Global Moderators", "Viewers"};
     static QStringList jsonLabels = {"broadcaster", "moderators", "vips",
-                                     "staff", "admins", "global_mods",
+                                     "staff",       "admins"     "global_mods",
                                      "viewers"};
     QList<QListWidgetItem *> labelList;
     for (auto &x : labels)
