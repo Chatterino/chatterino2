a3225c22e34a8067309c878cd4486a5a7483f17e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-10-18 15:02:54 +0200

    Add script to check line endings for all source code

---
+++ tools/check-line-endings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+fail="0"
+
+while read -r file; do
+    num_dos_line_endings=$(dos2unix -id "$file" | awk '/[0-9]+/{print $(NF-1)}')
+    if [ "$num_dos_line_endings" -gt "0" ]; then
+        >&2 echo "File '$file' contains $num_dos_line_endings DOS line-endings, it should only be using unix line-endings!"
+        fail="1"
+    fi
+done < <(find src/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
+
+if [ "$fail" = "1" ]; then
+    >&2 echo "At least one file is not using unix line-endings - check the output above"
+    exit 1
+fi
+
+>&2 echo "Every file seems to be using unix line-endings. Good job!"
