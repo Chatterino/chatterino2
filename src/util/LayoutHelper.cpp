3d9d7d54d5c1e11696445247fb954d48c6aa1b85
fourtf <tf.four@gmail.com>
2020-10-22 23:46:36 +0200

    added missing file

---
+++ src/util/LayoutHelper.cpp
@@ -0,0 +1,34 @@
+#include "util/LayoutHelper.hpp"
+
+#include <QScrollArea>
+#include <QWidget>
+
+namespace chatterino {
+
+QWidget *wrapLayout(QLayout *layout)
+{
+    auto widget = new QWidget;
+    widget->setLayout(layout);
+    return widget;
+}
+
+QScrollArea *makeScrollArea(LayoutItem item)
+{
+    auto area = new QScrollArea();
+
+    switch (item.which())
+    {
+        case 0:
+            area->setWidget(boost::get<QWidget *>(item));
+            break;
+        case 1:
+            area->setWidget(wrapLayout(boost::get<QLayout *>(item)));
+            break;
+    }
+
+    area->setWidgetResizable(true);
+
+    return area;
+}
+
+}  // namespace chatterino
