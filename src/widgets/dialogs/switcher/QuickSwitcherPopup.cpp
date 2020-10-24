Chatterino/chatterino2

commit f7237dccdd5a4994b057967c3b3cdd7fa5166d55

added colon emote popup for ffz and bttv

src/widgets/dialogs/switcher/QuickSwitcherPopup.cpp
@@ -9,6 +9,7 @@
 #include "widgets/dialogs/switcher/NewTabItem.hpp"
 #include "widgets/dialogs/switcher/SwitchSplitItem.hpp"
 #include "widgets/helper/NotebookTab.hpp"
+#include "widgets/listview/GenericListView.hpp"
 
 namespace chatterino {
 
@@ -36,7 +37,6 @@ QuickSwitcherPopup::QuickSwitcherPopup(QWidget *parent)
                                              BaseWindow::Flags::TopMost},
                 parent)
     , switcherModel_(this)
-    , switcherItemDelegate_(this)
 {
     this->setWindowFlag(Qt::Dialog);
     this->setActionOnFocusLoss(BaseWindow::ActionOnFocusLoss::Delete);
@@ -51,10 +51,8 @@ QuickSwitcherPopup::QuickSwitcherPopup(QWidget *parent)
                                           this->size(), geom));
 
     this->themeChangedEvent();
-}
 
-QuickSwitcherPopup::~QuickSwitcherPopup()
-{
+    this->installEventFilter(this->ui_.list);
 }
 
 void QuickSwitcherPopup::initWidgets()
@@ -72,24 +70,12 @@ void QuickSwitcherPopup::initWidgets()
     }
 
     {
-        vbox.emplace<QListView>().assign(&this->ui_.list);
-        this->ui_.list->setSelectionMode(QAbstractItemView::SingleSelection);
-        this->ui_.list->setSelectionBehavior(QAbstractItemView::SelectItems);
-        this->ui_.list->setModel(&this->switcherModel_);
-        this->ui_.list->setItemDelegate(&this->switcherItemDelegate_);
+        auto listView = vbox.emplace<GenericListView>().assign(&this->ui_.list);
+        listView->setModel(&this->switcherModel_);
 
-        /*
-         * I also tried handling key events using the according slots but
-         * it lead to all kind of problems that did not occur with the
-         * eventFilter approach.
-         */
-        QObject::connect(
-            this->ui_.list, &QListView::clicked, this,
-            [this](const QModelIndex &index) {
-                auto *item = AbstractSwitcherItem::fromVariant(index.data());
-                item->action();
-                this->close();
-            });
+        QObject::connect(listView.getElement(),
+                         &GenericListView::closeRequested, this,
+                         [this] { this->close(); });
     }
 }
 
@@ -145,61 +131,6 @@ void QuickSwitcherPopup::updateSuggestions(const QString &text)
     QTimer::singleShot(0, [this] { this->adjustSize(); });
 }
 
-bool QuickSwitcherPopup::eventFilter(QObject *watched, QEvent *event)
-{
-    if (event->type() == QEvent::KeyPress)
-    {
-        auto *keyEvent = static_cast<QKeyEvent *>(event);
-        int key = keyEvent->key();
-
-        const QModelIndex &curIdx = this->ui_.list->currentIndex();
-        const int curRow = curIdx.row();
-        const int count = this->switcherModel_.rowCount(curIdx);
-
-        if (key == Qt::Key_Down || key == Qt::Key_Tab)
-        {
-            if (count <= 0)
-                return true;
-
-            const int newRow = (curRow + 1) % count;
-
-            this->ui_.list->setCurrentIndex(curIdx.siblingAtRow(newRow));
-            return true;
-        }
-        else if (key == Qt::Key_Up || key == Qt::Key_Backtab)
-        {
-            if (count <= 0)
-                return true;
-
-            int newRow = curRow - 1;
-            if (newRow < 0)
-                newRow += count;
-
-            this->ui_.list->setCurrentIndex(curIdx.siblingAtRow(newRow));
-            return true;
-        }
-        else if (key == Qt::Key_Enter || key == Qt::Key_Return)
-        {
-            if (count <= 0)
-                return true;
-
-            const auto index = this->ui_.list->currentIndex();
-            auto *item = AbstractSwitcherItem::fromVariant(index.data());
-
-            item->action();
-
-            this->close();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    return false;
-}
-
 void QuickSwitcherPopup::themeChangedEvent()
 {
     BasePopup::themeChangedEvent();
@@ -220,7 +151,7 @@ void QuickSwitcherPopup::themeChangedEvent()
             .arg(selCol);
 
     this->ui_.searchEdit->setStyleSheet(this->theme->splits.input.styleSheet);
-    this->ui_.list->setStyleSheet(listStyle);
+    this->ui_.list->refreshTheme(*this->theme);
 }
 
 }  // namespace chatterino

chatterino.pro
@@ -239,11 +239,9 @@ SOURCES += \
     src/widgets/dialogs/QualityPopup.cpp \
     src/widgets/dialogs/SelectChannelDialog.cpp \
     src/widgets/dialogs/SettingsDialog.cpp \
-    src/widgets/dialogs/switcher/AbstractSwitcherItem.cpp \
+    src/widgets/listview/GenericItemDelegate.cpp \
     src/widgets/dialogs/switcher/NewTabItem.cpp \
-    src/widgets/dialogs/switcher/QuickSwitcherModel.cpp \
     src/widgets/dialogs/switcher/QuickSwitcherPopup.cpp \
-    src/widgets/dialogs/switcher/SwitcherItemDelegate.cpp \
     src/widgets/dialogs/switcher/SwitchSplitItem.cpp \
     src/widgets/dialogs/TextInputDialog.cpp \
     src/widgets/dialogs/UpdateDialog.cpp \
@@ -268,6 +266,9 @@ SOURCES += \
     src/widgets/Label.cpp \
     src/widgets/Notebook.cpp \
     src/widgets/Scrollbar.cpp \
+    src/widgets/listview/GenericListItem.cpp \
+    src/widgets/listview/GenericListModel.cpp \
+    src/widgets/listview/GenericListView.cpp \
     src/widgets/settingspages/AboutPage.cpp \
     src/widgets/settingspages/AccountsPage.cpp \
     src/widgets/settingspages/CommandPage.cpp \
@@ -280,6 +281,8 @@ SOURCES += \
     src/widgets/settingspages/NotificationPage.cpp \
     src/widgets/settingspages/SettingsPage.cpp \
     src/widgets/splits/ClosedSplits.cpp \
+    src/widgets/splits/EmoteInputItem.cpp \
+    src/widgets/splits/EmoteInputPopup.cpp \
     src/widgets/splits/Split.cpp \
     src/widgets/splits/SplitContainer.cpp \
     src/widgets/splits/SplitHeader.cpp \
@@ -474,10 +477,10 @@ HEADERS += \
     src/widgets/dialogs/SelectChannelDialog.hpp \
     src/widgets/dialogs/SettingsDialog.hpp \
     src/widgets/dialogs/switcher/AbstractSwitcherItem.hpp \
+    src/widgets/listview/GenericItemDelegate.hpp \
     src/widgets/dialogs/switcher/NewTabItem.hpp \
     src/widgets/dialogs/switcher/QuickSwitcherModel.hpp \
     src/widgets/dialogs/switcher/QuickSwitcherPopup.hpp \
-    src/widgets/dialogs/switcher/SwitcherItemDelegate.hpp \
     src/widgets/dialogs/switcher/SwitchSplitItem.hpp \
     src/widgets/dialogs/TextInputDialog.hpp \
     src/widgets/dialogs/UpdateDialog.hpp \
@@ -504,6 +507,9 @@ HEADERS += \
     src/widgets/Label.hpp \
     src/widgets/Notebook.hpp \
     src/widgets/Scrollbar.hpp \
+    src/widgets/listview/GenericListItem.hpp \
+    src/widgets/listview/GenericListModel.hpp \
+    src/widgets/listview/GenericListView.hpp \
     src/widgets/settingspages/AboutPage.hpp \
     src/widgets/settingspages/AccountsPage.hpp \
     src/widgets/settingspages/CommandPage.hpp \
@@ -516,6 +522,8 @@ HEADERS += \
     src/widgets/settingspages/NotificationPage.hpp \
     src/widgets/settingspages/SettingsPage.hpp \
     src/widgets/splits/ClosedSplits.hpp \
+    src/widgets/splits/EmoteInputItem.hpp \
+    src/widgets/splits/EmoteInputPopup.hpp \
     src/widgets/splits/Split.hpp \
     src/widgets/splits/SplitContainer.hpp \
     src/widgets/splits/SplitHeader.hpp \

src/util/QObjectRef.hpp
@@ -22,6 +22,11 @@ public:
         this->set(t);
     }
 
+    QObjectRef(const QObjectRef &other)
+    {
+        this->set(other.t_);
+    }
+
     ~QObjectRef()
     {
         this->set(nullptr);

src/widgets/BasePopup.hpp
@@ -11,8 +11,6 @@ public:
     explicit BasePopup(FlagsEnum<BaseWindow::Flags> flags_ = None,
                        QWidget *parent = nullptr);
 
-    virtual ~BasePopup() = default;
-
 protected:
     void keyPressEvent(QKeyEvent *e) override;
 };

src/widgets/BaseWindow.cpp
@@ -57,6 +57,18 @@ BaseWindow::BaseWindow(FlagsEnum<Flags> _flags, QWidget *parent)
         this->setWindowFlag(Qt::FramelessWindowHint);
     }
 
+    if (_flags.has(DontFocus))
+    {
+        this->setAttribute(Qt::WA_ShowWithoutActivating);
+#ifdef Q_OS_LINUX
+        this->setWindowFlags(Qt::ToolTip);
+#else
+        this->setWindowFlags(Qt::Tool | Qt::FramelessWindowHint |
+                             Qt::X11BypassWindowManagerHint |
+                             Qt::BypassWindowManagerHint);
+#endif
+    }
+
     this->init();
 
     getSettings()->uiScale.connect(

src/widgets/BaseWindow.hpp
@@ -29,6 +29,7 @@ public:
         TopMost = 4,
         DisableCustomScaling = 8,
         FramelessDraggable = 16,
+        DontFocus = 32,
     };
 
     enum ActionOnFocusLoss { Nothing, Delete, Close, Hide };

src/widgets/TooltipWidget.cpp
@@ -21,7 +21,7 @@ TooltipWidget *TooltipWidget::instance()
 }
 
 TooltipWidget::TooltipWidget(BaseWidget *parent)
-    : BaseWindow(BaseWindow::TopMost, parent)
+    : BaseWindow({BaseWindow::TopMost, BaseWindow::DontFocus}, parent)
     , displayImage_(new QLabel())
     , displayText_(new QLabel())
 {
@@ -30,15 +30,6 @@ TooltipWidget::TooltipWidget(BaseWidget *parent)
     //this->setWindowOpacity(0.8);
     this->updateFont();
     this->setStayInScreenRect(true);
-
-    this->setAttribute(Qt::WA_ShowWithoutActivating);
-#ifdef Q_OS_LINUX
-    this->setWindowFlags(Qt::ToolTip);
-#else
-    this->setWindowFlags(Qt::Tool | Qt::FramelessWindowHint |
-                         Qt::X11BypassWindowManagerHint |
-                         Qt::BypassWindowManagerHint);
-#endif
 
     displayImage_->hide();
     displayImage_->setAlignment(Qt::AlignHCenter);

src/widgets/dialogs/switcher/AbstractSwitcherItem.cpp
@@ -1,20 +1,0 @@
-#include "widgets/dialogs/switcher/AbstractSwitcherItem.hpp"
-
-#include "Application.hpp"
-
-namespace chatterino {
-
-const QSize AbstractSwitcherItem::ICON_SIZE(32, 32);
-
-AbstractSwitcherItem *AbstractSwitcherItem::fromVariant(const QVariant &variant)
-{
-    // See https://stackoverflow.com/a/44503822 .
-    return static_cast<AbstractSwitcherItem *>(variant.value<void *>());
-}
-
-AbstractSwitcherItem::AbstractSwitcherItem(const QIcon &icon)
-    : icon_(icon)
-{
-}
-
-}  // namespace chatterino

commit 0049a5ebb2b5bdb8feb7637f9a7acffc7b3cdf76

Fix tabbing in Quick Switcher (#2065)
It was broken in f7237dc due to the migration to the GenericListView approach

src/widgets/dialogs/switcher/QuickSwitcherPopup.cpp
@@ -65,8 +65,6 @@ void QuickSwitcherPopup::initWidgets()
         lineEdit->setPlaceholderText("Jump to a channel or open a new one");
         QObject::connect(this->ui_.searchEdit, &QLineEdit::textChanged, this,
                          &QuickSwitcherPopup::updateSuggestions);
-
-        this->ui_.searchEdit->installEventFilter(this);
     }
 
     {
@@ -76,6 +74,8 @@ void QuickSwitcherPopup::initWidgets()
         QObject::connect(listView.getElement(),
                          &GenericListView::closeRequested, this,
                          [this] { this->close(); });
+
+        this->ui_.searchEdit->installEventFilter(listView.getElement());
     }
 }
 