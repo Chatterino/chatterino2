8d5383ddc99fb070553ec3859fb8c9354b0605fa
Paweł <44851575+zneix@users.noreply.github.com>
2020-10-31 14:51:02 +0100

    Added placeholder for split's textEdit

--- src/widgets/splits/Split.cpp
+++ src/widgets/splits/Split.cpp
@@ -1,5 +1,6 @@
 #include "widgets/splits/Split.hpp"
 
+#include "Application.hpp"
 #include "common/Common.hpp"
 #include "common/Env.hpp"
 #include "common/NetworkRequest.hpp"
@@ -130,6 +131,11 @@ Split::Split(QWidget *parent)
     // CreateShortcut(this, "ALT+SHIFT+DOWN", &Split::doDecFlexY);
 
     this->input_->ui_.textEdit->installEventFilter(parent);
+
+    this->SignalHolder_.managedConnect(
+        getApp()->accounts->twitch.currentUserChanged,
+        [this] { this->onAccountSelected(); });
+    this->onAccountSelected();
 
     this->view_->mouseDown.connect([this](QMouseEvent *) {  //
         this->giveFocus(Qt::MouseFocusReason);
@@ -272,6 +278,25 @@ bool Split::isInContainer() const
 void Split::setContainer(SplitContainer *container)
 {
     this->container_ = container;
+}
+
+void Split::onAccountSelected()
+{
+    auto user = getApp()->accounts->twitch.getCurrent();
+    QString placeholderText;
+
+    if (user->isAnon())
+    {
+        placeholderText = "Log in to send messages...";
+    }
+    else
+    {
+        placeholderText =
+            QString("Chat as %1...")
+                .arg(getApp()->accounts->twitch.getCurrent()->getUserName());
+    }
+
+    this->input_->ui_.textEdit->setPlaceholderText(placeholderText);
 }
 
 IndirectChannel Split::getIndirectChannel()
