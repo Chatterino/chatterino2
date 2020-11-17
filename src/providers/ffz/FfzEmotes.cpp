1ef2b5a4e0c91be6cfa640c74941cc76acb9d996
MelonLord145 <JCRAFT54321MC@gmail.com>
2020-11-16 20:47:13 -0500

    Replace assert() with check to prevent crashes when emote.isString() is false

--- src/providers/ffz/FfzEmotes.cpp
+++ src/providers/ffz/FfzEmotes.cpp
@@ -14,12 +14,10 @@ namespace {
     Url getEmoteLink(const QJsonObject &urls, const QString &emoteScale)
     {
         auto emote = urls.value(emoteScale);
-        if (emote.isUndefined())
+        if (emote.isUndefined() || !emote.isString())
         {
             return {""};
         }
-
-        assert(emote.isString());
 
         return {"https:" + emote.toString()};
     }
