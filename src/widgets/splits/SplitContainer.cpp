8ccba2d5f1afb602a962a6daa11fb1c2050b64b7
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-12-06 14:57:44 +0100

    Use ManagedConnect instead of barebones connect
    we needed to disconnect the signals when the spitcontainer died - this
    solves it for us

--- src/widgets/splits/SplitContainer.cpp
+++ src/widgets/splits/SplitContainer.cpp
@@ -211,19 +211,19 @@ void SplitContainer::addSplit(Split *split)
 
     this->refreshTab();
 
-    split->getChannelView().tabHighlightRequested.connect(
-        [this](HighlightState state) {
-            if (this->tab_ != nullptr)
-            {
-                this->tab_->setHighlightState(state);
-            }
-        });
+    this->managedConnect(split->getChannelView().tabHighlightRequested,
+                         [this](HighlightState state) {
+                             if (this->tab_ != nullptr)
+                             {
+                                 this->tab_->setHighlightState(state);
+                             }
+                         });
 
-    split->getChannelView().liveStatusChanged.connect([this]() {
+    this->managedConnect(split->getChannelView().liveStatusChanged, [this]() {
         this->refreshTabLiveStatus();
     });
 
-    split->focused.connect([this, split] {
+    this->managedConnect(split->focused, [this, split] {
         this->setSelected(split);
     });
 
