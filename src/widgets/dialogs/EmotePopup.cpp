a7977ef116f2fb12b9e95eeaa11c86c3af77e5e3
zneix <zneix@zneix.eu>
2020-12-07 12:00:06 +0100

    Added Ctrl + 1/2/3/... shortcuts to emote popup

--- src/widgets/dialogs/EmotePopup.cpp
+++ src/widgets/dialogs/EmotePopup.cpp
@@ -156,6 +156,23 @@ EmotePopup::EmotePopup(QWidget *parent)
 
     this->loadEmojis();
 
+    // CTRL + 1-8 to open corresponding tab
+    for (auto i = 0; i < 8; i++)
+    {
+        char hotkey[7];
+        std::sprintf(hotkey, "CTRL+%d", i + 1);
+        const auto openTab = [this, i, notebook] {
+            notebook->selectIndex(i);
+        };
+        createWindowShortcut(this, hotkey, openTab);
+    }
+
+    // Open last tab (first one from right)
+    createWindowShortcut(this, "CTRL+9", [=] {
+        notebook->selectLastTab();
+    });
+
+    // Cycle through tabs
     createWindowShortcut(this, "CTRL+Tab", [=] {
         notebook->selectNextTab();
     });
