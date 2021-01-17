126af86e8652f06c635f8560b8b9d3ac9e1285d9
zneix <zneix@zneix.eu>
2021-01-17 16:35:50 +0100

    Moved `/clip` back up
    another victim of my merge conflict resolveness

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -454,6 +454,19 @@ void CommandController::initialize(Settings &, Paths &paths)
             return "";
         });
 
+    this->registerCommand("/clip", [](const auto &words, auto channel) {
+        if (!channel->isTwitchChannel())
+        {
+            return "";
+        }
+
+        auto *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
+
+        twitchChannel->createClip();
+
+        return "";
+    });
+
     this->registerCommand("/marker", [](const QStringList &words,
                                         auto channel) {
         // Avoid Helix calls without Client ID and/or OAuth Token
@@ -517,19 +530,6 @@ void CommandController::initialize(Settings &, Paths &paths)
 
                 channel->addMessage(makeSystemMessage(errorMessage));
             });
-
-        return "";
-    });
-
-    this->registerCommand("/clip", [](const auto &words, auto channel) {
-        if (!channel->isTwitchChannel())
-        {
-            return "";
-        }
-
-        auto *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
-
-        twitchChannel->createClip();
 
         return "";
     });
