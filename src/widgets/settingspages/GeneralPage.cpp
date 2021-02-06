a4fb20c72c1fefce60002919df5c9e5b56597345
Paweł <zneix@zneix.eu>
2021-02-06 18:07:02 +0100

    Added millisecond timestamp formats to settings (#2375)

--- src/widgets/settingspages/GeneralPage.cpp
+++ src/widgets/settingspages/GeneralPage.cpp
@@ -257,9 +257,10 @@ void GeneralPage::initLayout(GeneralPageView &layout)
     layout.addCheckbox("Highlight messages redeemed with Channel Points",
                        s.enableRedeemedHighlight);
     layout.addDropdown<QString>(
-        "Timestamp format (a = am/pm)",
+        "Timestamp format (a = am/pm, zzz = milliseconds)",
         {"Disable", "h:mm", "hh:mm", "h:mm a", "hh:mm a", "h:mm:ss", "hh:mm:ss",
-         "h:mm:ss a", "hh:mm:ss a"},
+         "h:mm:ss a", "hh:mm:ss a", "h:mm:ss.zzz", "h:mm:ss.zzz a",
+         "hh:mm:ss.zzz", "hh:mm:ss.zzz a"},
         s.timestampFormat,
         [](auto val) {
             return getSettings()->showTimestamps.getValue()
