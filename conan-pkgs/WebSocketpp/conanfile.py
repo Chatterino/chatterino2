bb0f2ae97244029c76f50804bc92d0e7ccc39cad
Edgar <Edgar@AnotherFoxGuy.com>
2021-02-12 12:41:28 +0100

    :tada: Added serialize WebSocketpp pkg

---
+++ conan-pkgs/WebSocketpp/conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake, tools
+
+
+class LibCommuniConan(ConanFile):
+    name = "WebSocketpp"
+    version = "0.8.1"
+    license = "MIT"
+    author = "Edgar Edgar@AnotherFoxGuy.com"
+    url = "https://github.com/Chatterino/chatterino2"
+    description = "WebSocket++ is a header only C++ library that implements RFC6455 The WebSocket Protocol"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://github.com/ziocleto/websocketpp.git")
+        git.checkout("1e0138c7ccedc6be859d28270ccd6195f235a94e")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
