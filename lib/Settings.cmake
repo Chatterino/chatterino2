2df55138f8288dfa92c6f810833605446a342103
Edgar <Edgar@AnotherFoxGuy.com>
2021-02-12 12:52:50 +0100

    :wrench: Replaced find_libs with conan

--- lib/Settings.cmake
+++
@@ -1,9 +0,0 @@
-project(PajladaSettings)
-
-add_library(Settings ${CMAKE_SOURCE_DIR}/lib/settings/src/settings/settingdata.cpp
-        ${CMAKE_SOURCE_DIR}/lib/settings/src/settings/settingmanager.cpp
-        ${CMAKE_SOURCE_DIR}/lib/settings/src/settings/detail/realpath.cpp)
-
-target_include_directories(Settings PRIVATE ${RapidJSON_INCLUDE_DIRS} ${Serialize_INCLUDE_DIR})
-target_include_directories(Settings PUBLIC ${CMAKE_SOURCE_DIR}/lib/settings/include/)
-target_link_libraries(Settings Serialize::Serialize)
