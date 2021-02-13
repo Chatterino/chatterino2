f15d966134cdf3dda732bbad5f2dbdc21064da81
Edgar <Edgar@AnotherFoxGuy.com>
2021-02-13 13:46:18 +0100

    :bug: Fixed missing libcommuni-cmake

--- conan-pkgs/add_conan_pkgs.cmake
+++ conan-pkgs/add_conan_pkgs.cmake
@@ -2,6 +2,7 @@ macro(add_pkg NAME)
     execute_process(COMMAND conan export ${CMAKE_CURRENT_LIST_DIR}/${NAME} chatterino/local)
 endmacro()
 
+add_pkg("libcommuni")
 add_pkg("QtKeychain")
 add_pkg("Serialize")
 add_pkg("Settings")
