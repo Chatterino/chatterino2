bbbad4bee2227a1da73cecb645cdfd1b1c9c3ade
Edgar <Edgar@AnotherFoxGuy.com>
2021-02-15 15:10:49 +0100

    :tada: Added ccache and git values back

--- conan-pkgs/QtKeychain/conanfile.py
+++ conan-pkgs/QtKeychain/conanfile.py
@@ -13,20 +13,19 @@ class QtKeychainConan(ConanFile):
 
     def source(self):
         git = tools.Git()
-        git.clone("https://github.com/Chatterino/qtkeychain.git")
-        git.checkout("308ea7e709113dc277be1653fe2044bb20236836")
+        git.clone("https://github.com/frankosterfeld/qtkeychain.git")
+        git.checkout("9a22739ea5d36bb3de46dbb93b22da2b2c119461")
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions['BUILD_TRANSLATIONS'] = 'OFF'
         cmake.definitions['BUILD_TEST_APPLICATION'] = 'OFF'
-        cmake.definitions['QTKEYCHAIN_STATIC'] = 'ON'
+        # cmake.definitions['QTKEYCHAIN_STATIC'] = 'ON'
         cmake.configure()
         cmake.build()
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
-
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
