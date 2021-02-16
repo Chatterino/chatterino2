38b51d69bf9169d3c0b3677198513584f9f01a4b
Edgar <Edgar@AnotherFoxGuy.com>
2021-02-16 11:38:37 +0100

    :tada: Added Cpack for building installer for windows

--- cmake/pmm.cmake
+++ cmake/pmm.cmake
@@ -27,7 +27,7 @@
 ## SOFTWARE.
 
 # Bump this version to change what PMM version is downloaded
-set(PMM_VERSION_INIT 1.9.5)
+set(PMM_VERSION_INIT 1.9.6)
 
 # Helpful macro to set a variable if it isn't already set
 macro(_pmm_set_if_undef varname)
