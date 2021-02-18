0bb9752e904058a80466b2ac0c03ea75cf6aef73
Mm2PL <jakis128@gmail.com>
2021-02-18 22:18:23 +0100

    WIP user interface

--- src/controllers/hotkeys/HotkeyModel.cpp
+++ src/controllers/hotkeys/HotkeyModel.cpp
@@ -5,23 +5,49 @@
 namespace chatterino {
 
 HotkeyModel::HotkeyModel(QObject *parent)
-    : SignalVectorModel<std::shared_ptr<Hotkey>>(1, parent)
+    : SignalVectorModel<std::shared_ptr<Hotkey>>(4, parent)
 {
 }
 
 // turn a vector item into a model row
 std::shared_ptr<Hotkey> HotkeyModel::getItemFromRow(
-    std::vector<QStandardItem *> &, const std::shared_ptr<Hotkey> &original)
+    std::vector<QStandardItem *> &row, const std::shared_ptr<Hotkey> &original)
 {
-    return original;
+    auto keySeq = QKeySequence(row[1]->data(Qt::EditRole).toString());
+    auto action = row[2]->data(Qt::EditRole).toString();
+    auto args = row[3]->data(Qt::EditRole).toString();
+    return std::make_shared<Hotkey>(original->scope(), keySeq, action,
+                                    original->arguments(), original->name());
 }
 
 // turns a row in the model into a vector item
 void HotkeyModel::getRowFromItem(const std::shared_ptr<Hotkey> &item,
                                  std::vector<QStandardItem *> &row)
 {
-    setStringItem(row[0], item->toString(), false);
+    setStringItem(row[0], item->name(), false);
     row[0]->setData(QFont("Segoe UI", 10), Qt::FontRole);
+
+    setStringItem(row[1], item->toPortableString(), true);
+    row[1]->setData(QFont("Segoe UI", 10), Qt::FontRole);
+
+    setStringItem(row[2], item->action(), true);
+    row[2]->setData(QFont("Segoe UI", 10), Qt::FontRole);
+
+    if (item->arguments().size() == 0)
+    {
+        setStringItem(row[3], "", true);
+    }
+    else
+    {
+        QString text;
+        for (auto &arg : item->arguments())
+        {
+            text.append(arg + ", ");
+        }
+        text.chop(2);  // remove last ", "
+        setStringItem(row[3], text, true);
+    }
+    row[3]->setData(QFont("Segoe UI", 10), Qt::FontRole);
 }
 
 int HotkeyModel::beforeInsert(const std::shared_ptr<Hotkey> &item,
--- src/widgets/settingspages/KeyboardSettingsPage.cpp
+++ src/widgets/settingspages/KeyboardSettingsPage.cpp
@@ -1,77 +1,45 @@
 #include "KeyboardSettingsPage.hpp"
 
+#include "Application.hpp"
+#include "common/QLogging.hpp"
+#include "controllers/hotkeys/HotkeyController.hpp"
+#include "controllers/hotkeys/HotkeyModel.hpp"
 #include "util/LayoutCreator.hpp"
+#include "widgets/helper/EditableModelView.hpp"
 
 #include <QFormLayout>
 #include <QLabel>
+#include <QTableView>
 
 namespace chatterino {
 
 KeyboardSettingsPage::KeyboardSettingsPage()
 {
-    auto layout =
-        LayoutCreator<KeyboardSettingsPage>(this).setLayoutType<QVBoxLayout>();
+    auto *app = getApp();
 
-    auto scroll = layout.emplace<QScrollArea>();
+    LayoutCreator<KeyboardSettingsPage> layoutCreator(this);
+    auto layout = layoutCreator.emplace<QVBoxLayout>().withoutMargin();
 
-    this->setStyleSheet("QLabel, #container { background: #333 }");
+    EditableModelView *view = layout
+                                  .emplace<EditableModelView>(
+                                      app->hotkeys->createModel(nullptr), false)
+                                  .getElement();
 
-    auto form = new QFormLayout(this);
-    scroll->setWidgetResizable(true);
-    auto widget = new QWidget();
-    widget->setLayout(form);
-    widget->setObjectName("container");
-    scroll->setWidget(widget);
+    view->setTitles({"Name", "Key Combo", "Action", "Arguments"});
+    view->getTableView()->horizontalHeader()->setVisible(true);
+    view->getTableView()->horizontalHeader()->setStretchLastSection(true);
 
-    form->addRow(new QLabel("Hold Ctrl"), new QLabel("Show resize handles"));
-    form->addRow(new QLabel("Hold Ctrl + Alt"),
-                 new QLabel("Show split overlay"));
+    view->addButtonPressed.connect([] {
+        qCDebug(chatterinoHotkeys) << "xd";
+    });
+    QTimer::singleShot(1, [view] {
+        view->getTableView()->resizeColumnsToContents();
+        view->getTableView()->setColumnWidth(0, 175);
+        view->getTableView()->setColumnWidth(1, 80);
+        view->getTableView()->setColumnWidth(2, 100);
+    });
 
-    form->addItem(new QSpacerItem(16, 16));
-    form->addRow(new QLabel("Ctrl + ScrollDown/-"), new QLabel("Zoom out"));
-    form->addRow(new QLabel("Ctrl + ScrollUp/+"), new QLabel("Zoom in"));
-    form->addRow(new QLabel("Ctrl + 0"), new QLabel("Reset zoom size"));
-
-    form->addItem(new QSpacerItem(16, 16));
-    form->addRow(new QLabel("Ctrl + T"), new QLabel("Create new split"));
-    form->addRow(new QLabel("Ctrl + W"), new QLabel("Close current split"));
-    form->addRow(new QLabel("Ctrl + N"),
-                 new QLabel("Open current split as a popup"));
-    form->addRow(new QLabel("Ctrl + K"), new QLabel("Jump to split"));
-    form->addRow(new QLabel("Ctrl + G"),
-                 new QLabel("Reopen last closed split"));
-
-    form->addRow(new QLabel("Ctrl + Shift + T"), new QLabel("Create new tab"));
-    form->addRow(new QLabel("Ctrl + Shift + W"),
-                 new QLabel("Close current tab"));
-    form->addRow(new QLabel("Ctrl + H"),
-                 new QLabel("Hide/Show similar messages (See General->R9K)"));
-
-    form->addItem(new QSpacerItem(16, 16));
-    form->addRow(new QLabel("Ctrl + 1/2/3/..."),
-                 new QLabel("Select tab 1/2/3/..."));
-    form->addRow(new QLabel("Ctrl + 9"), new QLabel("Select last tab"));
-    form->addRow(new QLabel("Ctrl + Tab"), new QLabel("Select next tab"));
-    form->addRow(new QLabel("Ctrl + Shift + Tab"),
-                 new QLabel("Select previous tab"));
-
-    form->addRow(new QLabel("Alt + ←/↑/→/↓"),
-                 new QLabel("Select left/upper/right/bottom split"));
-
-    form->addItem(new QSpacerItem(16, 16));
-    form->addRow(new QLabel("Ctrl + R"), new QLabel("Change channel"));
-    form->addRow(new QLabel("Ctrl + F"),
-                 new QLabel("Search in current channel"));
-    form->addRow(new QLabel("Ctrl + E"), new QLabel("Open Emote menu"));
-    form->addRow(new QLabel("Ctrl + P"), new QLabel("Open Settings menu"));
-    form->addRow(new QLabel("F5"),
-                 new QLabel("Reload subscriber and channel emotes"));
-    form->addRow(new QLabel("Ctrl + F5"), new QLabel("Reconnect channels"));
-    form->addRow(new QLabel("Alt + X"), new QLabel("Create a clip"));
-
-    form->addItem(new QSpacerItem(16, 16));
-    form->addRow(new QLabel("PageUp"), new QLabel("Scroll up"));
-    form->addRow(new QLabel("PageDown"), new QLabel("Scroll down"));
+    view->getTableView()->setStyleSheet("background: #333");
 }
 
 }  // namespace chatterino
