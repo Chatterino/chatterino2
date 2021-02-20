a592784dd4456ec03ddfc3650b5ac314e65af1e8
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-02-20 15:51:52 +0100

    Rename Settings to PajladaSettings

--- cmake/FindPajladaSettings.cmake
+++ cmake/FindPajladaSettings.cmake
@@ -1,16 +1,16 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(Settings_INCLUDE_DIR pajlada/settings.hpp)
-find_library(Settings_LIBRARY PajladaSettings)
+find_path(PajladaSettings_INCLUDE_DIR pajlada/settings.hpp)
+find_library(PajladaSettings_LIBRARY PajladaSettings)
 
-find_package_handle_standard_args(Settings DEFAULT_MSG Settings_INCLUDE_DIR Settings_LIBRARY)
+find_package_handle_standard_args(PajladaSettings DEFAULT_MSG PajladaSettings_INCLUDE_DIR PajladaSettings_LIBRARY)
 
-if (Settings_FOUND)
-    add_library(Settings::Settings INTERFACE IMPORTED)
-    set_target_properties(Settings::Settings PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${Settings_INCLUDE_DIR}"
-            INTERFACE_LINK_LIBRARIES "${Settings_LIBRARY}"
+if (PajladaSettings_FOUND)
+    add_library(Pajlada::Settings INTERFACE IMPORTED)
+    set_target_properties(Pajlada::Settings PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${PajladaSettings_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${PajladaSettings_LIBRARY}"
             )
 endif ()
 
-mark_as_advanced(Settings_INCLUDE_DIR Settings_LIBRARY)
+mark_as_advanced(PajladaSettings_INCLUDE_DIR PajladaSettings_LIBRARY)
