ac98528ace6a55f011b0c47dba788aa6474f4214
James Upjohn <jammehcow@jammehcow.co.nz>
2021-03-08 16:13:03 +1300

    Add comment explaining cheer emote tooltip construction

--- src/providers/twitch/TwitchChannel.cpp
+++ src/providers/twitch/TwitchChannel.cpp
@@ -909,6 +909,7 @@ void TwitchChannel::refreshCheerEmotes()
                     // We will continue to do so for now since we haven't had to
                     // solve that anywhere else
 
+                    // Combine the prefix (e.g. BibleThump) with the tier (1, 100 etc.)
                     auto emoteTooltip = set.prefix + tier.id + "<br/>Twitch Cheer Emote";
                     cheerEmote.animatedEmote = std::make_shared<Emote>(
                         Emote{EmoteName{"cheer emote"},
