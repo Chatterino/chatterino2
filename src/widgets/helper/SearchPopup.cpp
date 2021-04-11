e74fa1e2eb5227a70ba7802662ca9bdde654e4e4
Tal Neoran <talneoran@gmail.com>
2021-04-12 01:33:50 +0300

    Add in:channel predicate for searching of messages by channel

--- src/widgets/helper/SearchPopup.cpp
+++ src/widgets/helper/SearchPopup.cpp
@@ -8,6 +8,7 @@
 #include "common/Channel.hpp"
 #include "messages/Message.hpp"
 #include "messages/search/AuthorPredicate.hpp"
+#include "messages/search/ChannelPredicate.hpp"
 #include "messages/search/LinkPredicate.hpp"
 #include "messages/search/SubstringPredicate.hpp"
 #include "util/Shortcut.hpp"
@@ -167,6 +168,7 @@ std::vector<std::unique_ptr<MessagePredicate>> SearchPopup::parsePredicates(
     auto predicates = std::vector<std::unique_ptr<MessagePredicate>>();
     auto words = input.split(' ', QString::SkipEmptyParts);
     auto authors = QStringList();
+    auto channels = QStringList();
 
     for (auto it = words.begin(); it != words.end();)
     {
@@ -186,6 +188,10 @@ std::vector<std::unique_ptr<MessagePredicate>> SearchPopup::parsePredicates(
             {
                 predicates.push_back(std::make_unique<LinkPredicate>());
             }
+            else if (name == "in")
+            {
+                channels.append(value);
+            }
             else
             {
                 remove = false;
@@ -202,6 +208,9 @@ std::vector<std::unique_ptr<MessagePredicate>> SearchPopup::parsePredicates(
 
     if (!authors.empty())
         predicates.push_back(std::make_unique<AuthorPredicate>(authors));
+
+    if (!channels.empty())
+        predicates.push_back(std::make_unique<ChannelPredicate>(channels));
 
     if (!words.empty())
         predicates.push_back(
