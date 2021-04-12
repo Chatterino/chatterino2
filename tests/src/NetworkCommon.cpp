2758dd9acac09e78d7bf300e8241a3a59e44b31e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-04-12 23:56:00 +0200

    Add tests testing what happens if the user doesn't provide enough colons

--- tests/src/NetworkCommon.cpp
+++ tests/src/NetworkCommon.cpp
@@ -29,3 +29,31 @@ TEST(NetworkCommon, parseHeaderListTrimmed)
 
     ASSERT_EQ(expected, actual);
 }
+
+TEST(NetworkCommon, parseHeaderListBadPair)
+{
+    // The input values first header pair contains an invalid value, too many colons. We expect this value to be skipped
+    const QString input = "Authorization:  secretKey:bad; NextHeader   :boo";
+    const std::vector<std::pair<QString, QString>> expected = {
+        {"NextHeader", "boo"},
+    };
+
+    const auto actual = parseHeaderList(input);
+
+    ASSERT_EQ(expected, actual);
+    ASSERT_EQ(1, actual.size());
+}
+
+TEST(NetworkCommon, parseHeaderListBadPair2)
+{
+    // The input values first header pair doesn't have a colon, so we don't know where the header name and value start/end
+    const QString input = "Authorization  secretKeybad; NextHeader   :boo";
+    const std::vector<std::pair<QString, QString>> expected = {
+        {"NextHeader", "boo"},
+    };
+
+    const auto actual = parseHeaderList(input);
+
+    ASSERT_EQ(expected, actual);
+    ASSERT_EQ(1, actual.size());
+}
