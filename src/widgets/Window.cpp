1b9da3e36a6c42cdb5f19404498d778c5555e29b
zneix <zneix@zneix.eu>
2021-04-19 10:03:51 +0200

    Revert change to `Ctrl+Shift+T` shortcut

--- src/widgets/Window.cpp
+++ src/widgets/Window.cpp
@@ -356,7 +356,7 @@ void Window::addShortcuts()
 
     // New tab
     createWindowShortcut(this, "CTRL+SHIFT+T", [this] {
-        auto *newPage = this->notebook_->addPage(true);
+        newPage = this->notebook_->addPage(true);
     });
 
     // Close tab
