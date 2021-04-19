a9cfd582daf52ad9b1762db9223aa1709b44d711
zneix <zneix@zneix.eu>
2021-04-19 18:18:24 +0200

    Replaced horizontal with vertical

--- src/widgets/helper/NotebookTab.cpp
+++ src/widgets/helper/NotebookTab.cpp
@@ -600,7 +600,7 @@ void NotebookTab::mouseMoveEvent(QMouseEvent *event)
 void NotebookTab::wheelEvent(QWheelEvent *event)
 {
     const auto defaultMouseDelta = 120;
-    const auto horizontalDelta = event->angleDelta().y();
+    const auto verticalDelta = event->angleDelta().y();
     const auto selectTab = [this](int delta) {
         delta > 0 ? this->notebook_->selectPreviousTab()
                   : this->notebook_->selectNextTab();
@@ -608,9 +608,9 @@ void NotebookTab::wheelEvent(QWheelEvent *event)
     // If it's true
     // Then the user uses the trackpad or perhaps the most accurate mouse
     // Which has small delta.
-    if (std::abs(horizontalDelta) < defaultMouseDelta)
+    if (std::abs(verticalDelta) < defaultMouseDelta)
     {
-        this->mouseWheelDelta_ += horizontalDelta;
+        this->mouseWheelDelta_ += verticalDelta;
         if (std::abs(this->mouseWheelDelta_) >= defaultMouseDelta)
         {
             selectTab(this->mouseWheelDelta_);
@@ -619,7 +619,7 @@ void NotebookTab::wheelEvent(QWheelEvent *event)
     }
     else
     {
-        selectTab(horizontalDelta);
+        selectTab(verticalDelta);
     }
 }
 
