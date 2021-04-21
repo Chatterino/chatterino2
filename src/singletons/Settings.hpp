cd7e5ba21f63987a155d43331716be44490c485a
Mm2PL <jakis128@gmail.com>
2021-04-21 18:05:46 +0200

    move and rename setting

--- src/singletons/Settings.hpp
+++ src/singletons/Settings.hpp
@@ -118,8 +118,6 @@ public:
                                           -0.5f};
     // BoolSetting useCustomWindowFrame = {"/appearance/useCustomWindowFrame",
     // false};
-    BoolSetting removeSpacesBetweenEmotes = {
-        "/appearance/removeSpacesBetweenEmotes", false};
 
     // Badges
     BoolSetting showBadgesGlobalAuthority = {
@@ -176,6 +174,8 @@ public:
     QStringSetting emojiSet = {"/emotes/emojiSet", "Twitter"};
 
     BoolSetting stackBits = {"/emotes/stackBits", false};
+    BoolSetting removeSpacesBetweenEmotes = {
+        "/emotes/removeSpacesBetweenEmotes", false};
 
     /// Links
     BoolSetting linksDoubleClickOnly = {"/links/doubleClickToOpen", false};
