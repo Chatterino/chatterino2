232be2f32e2c48e774c8dfcfada795ef342a5513
zneix <zneix@zneix.eu>
2021-04-25 17:57:04 +0200

    30s -> 20s
    Because 30 seconds seems a bit too much, 20s is also used by Ffz::loadChannel

--- src/providers/bttv/BttvEmotes.cpp
+++ src/providers/bttv/BttvEmotes.cpp
@@ -161,7 +161,7 @@ void BttvEmotes::loadChannel(std::weak_ptr<Channel> channel,
                              bool manualRefresh)
 {
     NetworkRequest(QString(bttvChannelEmoteApiUrl) + channelId)
-        .timeout(30000)
+        .timeout(20000)
         .onSuccess([callback = std::move(callback), channel,
                     &channelDisplayName,
                     manualRefresh](auto result) -> Outcome {
