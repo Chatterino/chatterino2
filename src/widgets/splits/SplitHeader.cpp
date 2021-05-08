ab26363df06a7d78f49acdda6627c3ac05ad9b98
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-08 20:06:55 +1200

    Remove unnecessary conditional from newline check

--- src/widgets/splits/SplitHeader.cpp
+++ src/widgets/splits/SplitHeader.cpp
@@ -171,8 +171,7 @@ namespace {
             title += " - " + s.title;
 
         // Strip out phantom newlines/trailing whitespace to prevent formatting issues
-        if (title.endsWith('\n'))
-            title.replace(QRegExp("[\n\\s]+$"), "");
+        title.replace(QRegExp("[\n\\s]+$"), "");
 
         return title;
     }
