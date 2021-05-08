5e32c83b434bf765c24b57efcaa30cb0ba27e1ea
apa420 <apa420@apa420.com>
2021-05-08 17:27:53 +0200

    forgot to set init value

--- src/common/Channel.hpp
+++ src/common/Channel.hpp
@@ -102,7 +102,7 @@ protected:
 
 private:
     const QString name_;
-    QDate lastDate_;
+    QDate lastDate_ = QDate::currentDate();
     LimitedQueue<MessagePtr> messages_;
     Type type_;
     QTimer clearCompletionModelTimer_;
