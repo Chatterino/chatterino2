dcf910cbc5d1d54598e03a8548b89332ec0b2a17
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-26 20:18:53 +1200

    Capture gameName variable in lambda

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -735,7 +735,7 @@ void CommandController::initialize(Settings &, Paths &paths)
 
             getHelix()->searchGames(
                 gameName,
-                [channel, twitchChannel](std::vector<HelixGame> games) {
+                [channel, twitchChannel, gameName](std::vector<HelixGame> games) {
                     if (games.empty())
                     {
                         channel->addMessage(
