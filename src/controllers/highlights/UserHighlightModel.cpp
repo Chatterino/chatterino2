4aac63e86cd62cce774944a364837492d01720c7
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-12 13:31:31 +1200

    Add missing row to HighlightModel mapping

--- src/controllers/highlights/UserHighlightModel.cpp
+++ src/controllers/highlights/UserHighlightModel.cpp
@@ -26,6 +26,7 @@ HighlightPhrase UserHighlightModel::getItemFromRow(
         row[Column::Color]->data(Qt::DecorationRole).value<QColor>();
 
     return HighlightPhrase{
+        row[Column::Identifier]->data(Qt::DisplayRole).toString(),
         row[Column::Pattern]->data(Qt::DisplayRole).toString(),
         row[Column::ShowInMentions]->data(Qt::CheckStateRole).toBool(),
         row[Column::FlashTaskbar]->data(Qt::CheckStateRole).toBool(),
@@ -41,6 +42,7 @@ HighlightPhrase UserHighlightModel::getItemFromRow(
 void UserHighlightModel::getRowFromItem(const HighlightPhrase &item,
                                         std::vector<QStandardItem *> &row)
 {
+    setStringItem(row[Column::Identifier], item.getUniqueIdentifier());
     setStringItem(row[Column::Pattern], item.getPattern());
     setBoolItem(row[Column::ShowInMentions], item.showInMentions());
     setBoolItem(row[Column::FlashTaskbar], item.hasAlert());
