6355742b6ed01ff54b66752655e4c5c4290c0231
Paweł <zneix@zneix.eu>
2021-06-28 00:22:34 +0200

    Updated filelist in chatterino.pro (#2932)

--- tools/update_filelist.py
+++ tools/update_filelist.py
@@ -5,19 +5,19 @@ import re
 import subprocess
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = 'chatterino.pro'
+filename = '../chatterino.pro'
 data = ""
 
 with open(filename, 'r') as project:
     data = project.read()
-    sources_list = subprocess.getoutput("find ./src -type f -regex '.*\.cpp' | sed 's_\./_    _g'").splitlines()
+    sources_list = subprocess.getoutput("find ../src -path ../src/CMakeFiles -prune -false -o -type f -name '*.cpp' | sed 's_\../_    _g'").splitlines()
     sources_list.sort(key=str.lower)
     sources = "\n".join(sources_list)
     sources = re.sub(r'$', r' \\\\', sources, flags=re.MULTILINE)
     sources += "\n"
     data = re.sub(r'^SOURCES(.|\r|\n)*?^$', 'SOURCES += \\\n' + sources, data, flags=re.MULTILINE)
 
-    headers_list = subprocess.getoutput("find ./src -type f -regex '.*\.hpp' | sed 's_\./_    _g'").splitlines()
+    headers_list = subprocess.getoutput("find ../src -path ../src/CMakeFiles -prune -false -o -type f -name '*.hpp' | sed 's_\../_    _g'").splitlines()
     headers_list.sort(key=str.lower)
     headers = "\n".join(headers_list)
     headers = re.sub(r'$', r' \\\\', headers, flags=re.MULTILINE)
