7f3468f0b4983fb4809203db282d9aabc29bb389
zneix <zneix@zneix.eu>
2021-07-04 00:58:57 +0200

    Move if check from emote to emoteset iteration
    Whole emoteset can be either local or not, so it's better to early out if we know that set isn't local.

--- src/common/CompletionModel.cpp
+++ src/common/CompletionModel.cpp
@@ -101,14 +101,16 @@ void CompletionModel::refresh(const QString &prefix, bool isFirstWord)
         {
             for (const auto &emoteSet : account->accessEmotes()->emoteSets)
             {
-                for (const auto &emote : emoteSet->emotes)
+                // Some emotes (e.g. follower ones) are only available in their origin channel
+                if (!emoteSet->local ||
+                    (emoteSet->local &&
+                     this->channel_.getName() == emoteSet->channelName))
                 {
-                    // Some emotes (e.g. follower ones) are only available in the channel they're from
-                    if (!emoteSet->local ||
-                        (emoteSet->local &&
-                         this->channel_.getName() == emoteSet->channelName))
+                    for (const auto &emote : emoteSet->emotes)
+                    {
                         addString(emote.name.string,
                                   TaggedString::Type::TwitchGlobalEmote);
+                    }
                 }
             }
         }
