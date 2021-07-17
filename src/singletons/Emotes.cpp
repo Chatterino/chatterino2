fab44544a7c78a530dac98522fef1f91e4d6b556
zneix <zneix@zneix.eu>
2021-07-17 11:20:13 +0200

    Removed unnecessary connection
    We now load emotes after `GLOBALUSERSTATE`, so this isn't needed anymore

--- src/singletons/Emotes.cpp
+++ src/singletons/Emotes.cpp
@@ -11,12 +11,6 @@ Emotes::Emotes()
 
 void Emotes::initialize(Settings &settings, Paths &paths)
 {
-    getApp()->accounts->twitch.currentUserChanged.connect([] {
-        qDebug() << "penis123"
-                 << getApp()->accounts->twitch.getCurrent()->getUserName();
-        // getApp()->accounts->twitch.getCurrent()->loadEmotes();
-    });
-
     this->emojis.load();
 
     this->gifTimer.initialize();
