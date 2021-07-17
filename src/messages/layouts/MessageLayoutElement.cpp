89b86b94e7a2fb5327cfbad46614e8022b1358b2
Paweł <zneix@zneix.eu>
2021-07-17 18:45:02 +0200

    Cleanup debug logging (#3015)

--- src/messages/layouts/MessageLayoutElement.cpp
+++ src/messages/layouts/MessageLayoutElement.cpp
@@ -1,7 +1,6 @@
 #include "messages/layouts/MessageLayoutElement.hpp"
 
 #include "Application.hpp"
-#include "common/QLogging.hpp"
 #include "messages/Emote.hpp"
 #include "messages/Image.hpp"
 #include "messages/MessageElement.hpp"
@@ -433,7 +432,6 @@ void MultiColorTextLayoutElement::paint(QPainter &painter)
 
     for (const auto &segment : this->segments_)
     {
-        qCDebug(chatterinoMessage) << "Draw segment:" << segment.text;
         painter.setPen(segment.color);
         painter.drawText(QRectF(this->getRect().x() + xOffset,
                                 this->getRect().y(), 10000, 10000),
