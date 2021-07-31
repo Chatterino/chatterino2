6c6edc10bb52258655a62d3130b7a9d942ce7ce5
zneix <zneix@zneix.eu>
2021-07-31 12:43:04 +0200

    QLatin1Literal no longer exists (?)
    QStringLiteral seems like a fine workaround

--- src/singletons/helper/LoggingChannel.cpp
+++ src/singletons/helper/LoggingChannel.cpp
@@ -102,7 +102,7 @@ void LoggingChannel::addMessage(MessagePtr message)
 
 QString LoggingChannel::generateOpeningString(const QDateTime &now) const
 {
-    QString ret = QLatin1Literal("# Start logging at ");
+    QString ret = QStringLiteral("# Start logging at ");
 
     ret.append(now.toString("yyyy-MM-dd HH:mm:ss "));
     ret.append(now.timeZoneAbbreviation());
@@ -113,7 +113,7 @@ QString LoggingChannel::generateOpeningString(const QDateTime &now) const
 
 QString LoggingChannel::generateClosingString(const QDateTime &now) const
 {
-    QString ret = QLatin1Literal("# Stop logging at ");
+    QString ret = QStringLiteral("# Stop logging at ");
 
     ret.append(now.toString("yyyy-MM-dd HH:mm:ss"));
     ret.append(now.timeZoneAbbreviation());
