e7b89e61e8f2ed197c5168051efbe98c9fe5c4db
zneix <zneix@zneix.eu>
2021-07-31 22:32:47 +0200

    Move QAction to QtGui on Qt6
    Reference:
    - https://doc.qt.io/qt-6/widgets-changes-qt6.html#qaction-qactiongroup

--- src/PrecompiledHeader.hpp
+++ src/PrecompiledHeader.hpp
@@ -107,7 +107,11 @@
 #    include <QWidget>
 #    include <QtCore/QVariant>
 #    include <QtGlobal>
-#    include <QtWidgets/QAction>
+#    if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#        include <QtGui/QAction>
+#    else
+#        include <QtWidgets/QAction>
+#    endif
 #    include <QtWidgets/QApplication>
 #    include <QtWidgets/QButtonGroup>
 #    include <QtWidgets/QDialog>
