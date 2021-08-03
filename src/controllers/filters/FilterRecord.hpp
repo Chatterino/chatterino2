Chatterino/chatterino2

commit d7e8f4eabd25d55f69b3de96f7ab5207e923092a

Add channel.live filter variable (#3092)
Co-authored-by: pajlada <rasmus.karlsson@pajlada.com>

src/controllers/filters/parser/FilterParser.cpp
@@ -61,8 +61,7 @@ ContextMap buildContextMap(const MessagePtr &m)
             subLength = m->badgeInfos.at(subBadge).toInt();
         }
     }
-
-    return {
+    ContextMap vars = {
         {"author.badges", std::move(badges)},
         {"author.color", m->usernameColor},
         {"author.name", m->displayName},
@@ -82,6 +81,20 @@ ContextMap buildContextMap(const MessagePtr &m)
         {"message.content", m->messageText},
         {"message.length", m->messageText.length()},
     };
+    {
+        using namespace chatterino;
+        auto channel = getApp()->twitch2->getChannelOrEmpty(m->channelName);
+        auto *tc = dynamic_cast<TwitchChannel *>(channel.get());
+        if (!channel->isEmpty() && tc)
+        {
+            vars["channel.live"] = tc->isLive();
+        }
+        else
+        {
+            vars["channel.live"] = false;
+        }
+    }
+    return vars;
 }
 
 FilterParser::FilterParser(const QString &text)

src/controllers/filters/parser/Tokenizer.hpp
@@ -17,6 +17,7 @@ static const QMap<QString, QString> validIdentifiersMap = {
     {"author.sub_length", "author sub length"},
     {"channel.name", "channel name"},
     {"channel.watching", "/watching channel?"},
+    {"channel.live", "Channel live?"},
     {"flags.highlighted", "highlighted?"},
     {"flags.points_redeemed", "redeemed points?"},
     {"flags.sub_message", "sub/resub message?"},

commit 95044efeedd8315ac700c4242e528d203da36a01

Fix filter crash introduced in #3092 (#3110)

src/controllers/filters/FilterRecord.hpp
@@ -60,11 +60,6 @@ public:
         return this->parser_->valid();
     }
 
-    bool filter(const MessagePtr &message) const
-    {
-        return this->parser_->execute(message);
-    }
-
     bool filter(const filterparser::ContextMap &context) const
     {
         return this->parser_->execute(context);

src/controllers/filters/FilterSet.hpp
@@ -36,12 +36,13 @@ public:
         this->listener_.disconnect();
     }
 
-    bool filter(const MessagePtr &m) const
+    bool filter(const MessagePtr &m, ChannelPtr channel) const
     {
         if (this->filters_.size() == 0)
             return true;
 
-        filterparser::ContextMap context = filterparser::buildContextMap(m);
+        filterparser::ContextMap context =
+            filterparser::buildContextMap(m, channel.get());
         for (const auto &f : this->filters_.values())
         {
             if (!f->valid() || !f->filter(context))

src/controllers/filters/parser/FilterParser.cpp
@@ -1,12 +1,13 @@
 #include "FilterParser.hpp"
 
 #include "Application.hpp"
+#include "common/Channel.hpp"
 #include "controllers/filters/parser/Types.hpp"
 #include "providers/twitch/TwitchIrcServer.hpp"
 
 namespace filterparser {
 
-ContextMap buildContextMap(const MessagePtr &m)
+ContextMap buildContextMap(const MessagePtr &m, chatterino::Channel *channel)
 {
     auto watchingChannel =
         chatterino::getApp()->twitch.server->watchingChannel.get();
@@ -83,9 +84,8 @@ ContextMap buildContextMap(const MessagePtr &m)
     };
     {
         using namespace chatterino;
-        auto channel = getApp()->twitch2->getChannelOrEmpty(m->channelName);
-        auto *tc = dynamic_cast<TwitchChannel *>(channel.get());
-        if (!channel->isEmpty() && tc)
+        auto *tc = dynamic_cast<TwitchChannel *>(channel);
+        if (channel && !channel->isEmpty() && tc)
         {
             vars["channel.live"] = tc->isLive();
         }
@@ -102,12 +102,6 @@ FilterParser::FilterParser(const QString &text)
     , tokenizer_(Tokenizer(text))
     , builtExpression_(this->parseExpression(true))
 {
-}
-
-bool FilterParser::execute(const MessagePtr &message) const
-{
-    auto context = buildContextMap(message);
-    return this->execute(context);
 }
 
 bool FilterParser::execute(const ContextMap &context) const

src/controllers/filters/parser/FilterParser.hpp
@@ -3,15 +3,20 @@
 #include "controllers/filters/parser/Tokenizer.hpp"
 #include "controllers/filters/parser/Types.hpp"
 
+namespace chatterino {
+
+class Channel;
+
+}  // namespace chatterino
+
 namespace filterparser {
 
-ContextMap buildContextMap(const MessagePtr &m);
+ContextMap buildContextMap(const MessagePtr &m, chatterino::Channel *channel);
 
 class FilterParser
 {
 public:
     FilterParser(const QString &text);
-    bool execute(const MessagePtr &message) const;
     bool execute(const ContextMap &context) const;
     bool valid() const;
 

src/widgets/helper/ChannelView.cpp
@@ -754,7 +754,7 @@ bool ChannelView::shouldIncludeMessage(const MessagePtr &m) const
                 m->loginName, Qt::CaseInsensitive) == 0)
             return true;
 
-        return this->channelFilters_->filter(m);
+        return this->channelFilters_->filter(m, this->channel_);
     }
 
     return true;

src/widgets/helper/SearchPopup.cpp
@@ -44,7 +44,7 @@ ChannelPtr SearchPopup::filter(const QString &text, const QString &channelName,
         }
 
         if (accept && filterSet)
-            accept = filterSet->filter(message);
+            accept = filterSet->filter(message, channel);
 
         // If all predicates match, add the message to the channel
         if (accept)