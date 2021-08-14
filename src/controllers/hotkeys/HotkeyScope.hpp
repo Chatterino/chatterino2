950c5dcc0ae906711418c1ac2fd43c6924ab5d02
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-08-14 13:12:52 +0200

    Move comment in HotkeyScope

--- src/controllers/hotkeys/HotkeyScope.hpp
+++ src/controllers/hotkeys/HotkeyScope.hpp
@@ -2,13 +2,14 @@
 
 namespace chatterino {
 
+// HotkeyScope describes where the hotkeys's action takes place.
+// Each HotkeyScope represents a widget that has customizable hotkeys. This
+// is needed because more than one widget can have the same or similar action.
 enum class HotkeyScope {
-    // The HotkeyScope enum describes where the hotkeys's action takes place.
-    // Each HotkeyScope represents a widget that has customizable hotkeys. This
-    // is needed because more than one widget can have the same or similar action.
     PopupWindow,
     Split,
     SplitInput,
     Window,
 };
+
 }  // namespace chatterino
