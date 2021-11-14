20b7a61b22038d8b6c440b337e494c55dc49bc78
Felanbird <41973452+Felanbird@users.noreply.github.com>
2021-11-13 20:42:13 -0500

    alphabet

--- src/widgets/helper/ChannelView.cpp
+++ src/widgets/helper/ChannelView.cpp
@@ -1934,9 +1934,9 @@ void ChannelView::addContextMenuItems(
             R"(^(?:https?:\/\/)?(?:www\.|go\.)?twitch\.tv\/(?:popout\/)?(?<username>[a-z0-9_]{3,}))",
             QRegularExpression::CaseInsensitiveOption);
         static QSet<QString> ignoredUsernames{
-            "videos",    "settings", "directory",     "jobs",     "friends",
-            "inventory", "payments", "subscriptions", "messages", "drops",
-            "wallet",    "profile",  "downloads",     "turbo",    "security",
+            "directory", "downloads",     "drops",    "friends", "inventory",
+            "jobs",      "messages",      "payments", "profile", "security",
+            "settings",  "subscriptions", "turbo",    "videos",  "wallet",
         };
 
         auto twitchMatch =
