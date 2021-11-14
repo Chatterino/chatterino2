bca5428af6ace05f33f2fe31d2d42fe1e7f5fdbf
Mm2PL <mm2pl+gh@kotmisia.pl>
2021-11-14 20:25:18 +0000

    Typo
    Co-authored-by: Felanbird <41973452+Felanbird@users.noreply.github.com>

--- src/controllers/hotkeys/HotkeyCategory.hpp
+++ src/controllers/hotkeys/HotkeyCategory.hpp
@@ -4,7 +4,7 @@
 
 namespace chatterino {
 
-// HotkeyCategory describes where the hotkeys's action takes place.
+// HotkeyCategory describes where the hotkeys action takes place.
 // Each HotkeyCategory represents a widget that has customizable hotkeys. This
 // is needed because more than one widget can have the same or similar action.
 enum class HotkeyCategory {
