07dd8c560bcb73d0e1a76bf330771624cca82f47
kornes <28986062+kornes@users.noreply.github.com>
2022-05-08 10:27:25 +0000

    Prevent user from entering incorrect characters in Live Notifications channels list (#3715)
    Co-authored-by: Sidd <iProdigy@users.noreply.github.com>
    Co-authored-by: Rasmus Karlsson <rasmus.karlsson@pajlada.com>

--- src/util/Twitch.hpp
+++ src/util/Twitch.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QRegularExpression>
 #include <QString>
 
 namespace chatterino {
@@ -11,5 +12,17 @@ void stripUserName(QString &userName);
 
 // stripChannelName removes any @ prefix or , suffix to make it more suitable for command use
 void stripChannelName(QString &channelName);
+
+// Matches a strict Twitch user login.
+// May contain lowercase a-z, 0-9, and underscores
+// Must contain between 1 and 25 characters
+// Must not start with an underscore
+QRegularExpression twitchUserLoginRegexp();
+
+// Matches a loose Twitch user login name.
+// May contain lowercase and uppercase a-z, 0-9, and underscores
+// Must contain between 1 and 25 characters
+// Must not start with an underscore
+QRegularExpression twitchUserNameRegexp();
 
 }  // namespace chatterino
