c9c9c977a4ea0a37c65281d23d645ebca86ff604
Daniel Sage <sagedanielr@gmail.com>
2022-05-09 22:56:01 -0400

    Add flags.reply to filters

--- src/controllers/filters/parser/Tokenizer.hpp
+++ src/controllers/filters/parser/Tokenizer.hpp
@@ -25,6 +25,7 @@ static const QMap<QString, QString> validIdentifiersMap = {
     {"flags.reward_message", "channel point reward message?"},
     {"flags.first_message", "first message?"},
     {"flags.whisper", "whisper message?"},
+    {"flags.reply", "reply message?"},
     {"message.content", "message text"},
     {"message.length", "message length"}};
 
