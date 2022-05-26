c950ca0b13570e8fe7e78aa4510c18d1cb60bc0b
zneix <zneix@zneix.eu>
2022-05-26 15:22:36 +0200

    Removed Badge::extraValue_ field
    It's been unused, making it obsolete. Said 'extra value' is present in `badge-info` tag (and also sourced from there, where necessary) anyway.
    
    [skip ci]

--- src/providers/twitch/TwitchBadge.hpp
+++ src/providers/twitch/TwitchBadge.hpp
@@ -11,9 +11,8 @@ class Badge
 public:
     Badge(const std::pair<const QString, QString> keyValuePair);
 
-    QString key_;           // e.g. bits
-    QString value_;         // e.g. 100
-    QString extraValue_{};  // e.g. 5 (the number of months subscribed)
+    QString key_;    // e.g. bits
+    QString value_;  // e.g. 100
     MessageElementFlag flag_{
         MessageElementFlag::BadgeVanity};  // badge slot it takes up
 };
