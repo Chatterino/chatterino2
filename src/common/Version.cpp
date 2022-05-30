25c70950cfc2922d71fb040c796009b8cb7789e6
zneix <zneix@zneix.eu>
2022-05-30 02:32:07 +0200

    Windows is special

--- src/common/Version.cpp
+++ src/common/Version.cpp
@@ -18,9 +18,9 @@ Version::Version()
 
     // Whether or not the vcs tree had any changes at the time of build
     // Non-empty value of GIT_MODIFIED means it had
-    this->isModified_ = !QString(FROM_EXTERNAL_DEFINE(CHATTERINO_GIT_MODIFIED))
-                             .remove('"')
-                             .isEmpty();
+    this->isModified_ = !(QString(FROM_EXTERNAL_DEFINE(CHATTERINO_GIT_MODIFIED))
+                              .remove('"')
+                              .isEmpty());
 
     // Date of build file generation (≈ date of build)
 #ifdef CHATTERINO_CMAKE_GEN_DATE
