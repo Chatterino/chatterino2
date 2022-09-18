8bc848729380731daa9d0cf3e0c83f71923010fc
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-09-18 12:39:57 +0200

    Make `deleteMessagesLambda` messageID parameter into a const reference to appease clang-tidy

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -1270,7 +1270,7 @@ void CommandController::initialize(Settings &, Paths &paths)
         return "";
     });
 
-    auto deleteMessagesLambda = [](auto channel, QString messageID) {
+    auto deleteMessagesLambda = [](auto channel, const QString &messageID) {
         const auto *commandName = messageID.isEmpty() ? "/clear" : "/delete";
         auto *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
         if (twitchChannel == nullptr)
