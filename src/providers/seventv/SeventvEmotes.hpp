9cf2787c72225cc3cc8db489d97ac2299cf0711f
Nerixyz <nerixdev@outlook.de>
2022-09-19 10:17:21 +0200

    chore: update emote flags docs

--- src/providers/seventv/SeventvEmotes.hpp
+++ src/providers/seventv/SeventvEmotes.hpp
@@ -9,20 +9,22 @@
 
 namespace chatterino {
 
-// TODO(nerix): update these links when v3 is announced
-//              add docs for each enum variant
-
-// https://github.com/SevenTV/API/blob/8fbfc702a3fe0ada59f4b9593c65748d36ac7c0b/data/model/emote-set.model.go#L33-L38
+// https://github.com/SevenTV/API/blob/a84e884b5590dbb5d91a5c6b3548afabb228f385/data/model/emote-set.model.go#L29-L36
 enum class SeventvActiveEmoteFlag : int64_t {
     None = 0LL,
 
+    // Emote is zero-width
     ZeroWidth = (1LL << 0),
 
+    // Overrides Twitch Global emotes with the same name
     OverrideTwitchGlobal = (1 << 16),
+    // Overrides Twitch Subscriber emotes with the same name
     OverrideTwitchSubscriber = (1 << 17),
+    // Overrides BetterTTV emotes with the same name
     OverrideBetterTTV = (1 << 18),
 };
 
+// https://github.com/SevenTV/API/blob/a84e884b5590dbb5d91a5c6b3548afabb228f385/data/model/emote.model.go#L57-L70
 enum class SeventvEmoteFlag : int64_t {
     None = 0LL,
     // The emote is private and can only be accessed by its owner, editors and moderators
