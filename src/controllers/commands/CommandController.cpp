95c00fabf9d7f05cd0a9c42b34de25bdad3aca8e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-09-24 12:29:28 +0200

    Add some todo's for myself

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -1515,6 +1515,7 @@ void CommandController::initialize(Settings &, Paths &paths)
                         switch (error)
                         {
                             case Error::UserMissingScope: {
+                                // TODO(pajlada): Phrase MISSING_REQUIRED_SCOPE
                                 errorMessage += "Missing required scope. "
                                                 "Re-login with your "
                                                 "account and try again.";
@@ -1522,6 +1523,7 @@ void CommandController::initialize(Settings &, Paths &paths)
                             break;
 
                             case Error::UserNotAuthorized: {
+                                // TODO(pajlada): Phrase MISSING_PERMISSION
                                 errorMessage += "You don't have permission to "
                                                 "perform that action.";
                             }
