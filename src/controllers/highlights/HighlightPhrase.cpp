469fa9aff4ab2a8576830c300cab58d561b70712
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-09-30 00:13:39 -0400

    Less strong Alpha value

--- src/controllers/highlights/HighlightPhrase.cpp
+++ src/controllers/highlights/HighlightPhrase.cpp
@@ -15,7 +15,7 @@ QColor HighlightPhrase::FALLBACK_REDEEMED_HIGHLIGHT_COLOR =
 QColor HighlightPhrase::FALLBACK_FIRST_MESSAGE_HIGHLIGHT_COLOR =
     QColor(72, 127, 63, 60);
 QColor HighlightPhrase::FALLBACK_ELEVATED_MESSAGE_HIGHLIGHT_COLOR =
-    QColor(255, 174, 66, 255);
+    QColor(255, 174, 66, 60);
 QColor HighlightPhrase::FALLBACK_SUB_COLOR = QColor(196, 102, 255, 100);
 
 bool HighlightPhrase::operator==(const HighlightPhrase &other) const
