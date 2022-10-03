23e919a6711ad4ce9bc1aab50c164c6f4b8fd7da
Nerixyz <nerixdev@outlook.de>
2022-10-03 14:14:10 +0200

    refactor: move `/followers{,off}` to helix api

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -2390,6 +2390,99 @@ void CommandController::initialize(Settings &, Paths &paths)
                 });
             return "";
         });
+
+    this->registerCommand("/followers", [formatChatSettingsError](
+                                            const QStringList &words,
+                                            auto channel) {
+        auto currentUser = getApp()->accounts->twitch.getCurrent();
+        if (currentUser->isAnon())
+        {
+            channel->addMessage(makeSystemMessage(
+                "You must be logged in to update chat settings!"));
+            return "";
+        }
+
+        auto *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
+        if (twitchChannel == nullptr)
+        {
+            channel->addMessage(makeSystemMessage(
+                "The /followers command only works in Twitch channels"));
+            return "";
+        }
+
+        int duration = 0;
+        if (words.length() >= 2)
+        {
+            auto parsed = parseDurationToSeconds(words.mid(1).join(' '), 60);
+            duration = (int)(parsed / 60);
+            // -1 / 60 == 0 => use parsed
+            if (parsed < 0)
+            {
+                channel->addMessage(makeSystemMessage(
+                    "Usage: \"/followers [duration]\" - Enables followers-only"
+                    " mode (only users who have followed for 'duration' may "
+                    "chat). Examples: \"30m\", \"1 week\", \"5 days 12 "
+                    "hours\". Must be less than 3 months. "));
+                return "";
+            }
+        }
+
+        if (twitchChannel->accessRoomModes()->followerOnly == duration)
+        {
+            channel->addMessage(makeSystemMessage(
+                QString("This room is already in %1 followers-only mode.")
+                    .arg(formatTime(duration * 60))));
+            return "";
+        }
+
+        getHelix()->updateFollowerMode(
+            twitchChannel->roomId(), currentUser->getUserId(), duration,
+            [](auto) {
+                //we'll get a message from irc
+            },
+            [channel, formatChatSettingsError](auto error, auto message) {
+                channel->addMessage(
+                    makeSystemMessage(formatChatSettingsError(error, message)));
+            });
+        return "";
+    });
+
+    this->registerCommand("/followersoff", [formatChatSettingsError](
+                                               const QStringList & /* words */,
+                                               auto channel) {
+        auto currentUser = getApp()->accounts->twitch.getCurrent();
+        if (currentUser->isAnon())
+        {
+            channel->addMessage(makeSystemMessage(
+                "You must be logged in to update chat settings!"));
+            return "";
+        }
+        auto *twitchChannel = dynamic_cast<TwitchChannel *>(channel.get());
+        if (twitchChannel == nullptr)
+        {
+            channel->addMessage(makeSystemMessage(
+                "The /followersoff command only works in Twitch channels"));
+            return "";
+        }
+
+        if (twitchChannel->accessRoomModes()->followerOnly < 0)
+        {
+            channel->addMessage(
+                makeSystemMessage("This room is not in followers-only mode. "));
+            return "";
+        }
+
+        getHelix()->updateFollowerMode(
+            twitchChannel->roomId(), currentUser->getUserId(), boost::none,
+            [](auto) {
+                // we'll get a message from irc
+            },
+            [channel, formatChatSettingsError](auto error, auto message) {
+                channel->addMessage(
+                    makeSystemMessage(formatChatSettingsError(error, message)));
+            });
+        return "";
+    });
 }
 
 void CommandController::save()
