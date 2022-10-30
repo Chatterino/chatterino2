3fed08e4b8ce3ca30c0a8e2956ba4dde428a8043
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-10-30 19:31:30 +0100

    Document the MessageOverflow enum

--- src/widgets/splits/SplitInput.hpp
+++ src/widgets/splits/SplitInput.hpp
@@ -22,7 +22,18 @@ class EffectLabel;
 class MessageThread;
 class ResizingTextEdit;
 
-enum MessageOverflow { Highlight, Prevent, Allow };
+// MessageOverflow is used for controlling how to guide the user into not
+// sending a message that will be discarded by Twitch
+enum MessageOverflow {
+    // Allow overflowing characters to be inserted into the input box, but highlight them in red
+    Highlight,
+
+    // Prevent more characters from being inserted into the input box
+    Prevent,
+
+    // Do nothing
+    Allow,
+};
 
 class SplitInput : public BaseWidget
 {
