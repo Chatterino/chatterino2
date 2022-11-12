ac083b894120cb7ed76fbf7ce00bc2ba9e073fdc
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-11-12 12:54:40 +0100

    Comment definitions

--- src/widgets/dialogs/UserInfoPopup.hpp
+++ src/widgets/dialogs/UserInfoPopup.hpp
@@ -59,6 +59,8 @@ private:
     std::unique_ptr<pajlada::Signals::ScopedConnection> refreshConnection_;
 
     // If we should close the dialog automatically if the user clicks out
+    // Initially set based on the "Automatically close usercard when it loses focus" setting
+    // If that setting is enabled, this can be toggled on and off using the pin in the top-right corner
     bool closeAutomatically_;
 
     struct {
@@ -70,6 +72,7 @@ private:
         Label *followerCountLabel = nullptr;
         Label *createdDateLabel = nullptr;
         Label *userIDLabel = nullptr;
+        // Can be uninitialized if usercard is not configured to close on focus loss
         Button *pinButton = nullptr;
         Label *followageLabel = nullptr;
         Label *subageLabel = nullptr;
