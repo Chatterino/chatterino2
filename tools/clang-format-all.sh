31b7af969c75f60460096cc868f256646ef3e666
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-11-20 17:15:18 +0100

    Have `clang-format-all.sh` also reformat test files

--- tools/clang-format-all.sh
+++ tools/clang-format-all.sh
@@ -4,4 +4,5 @@ read -p "Are you sure you want to run clang-format on all files in src/? (y/n) "
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     find src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
+    find tests/src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
 fi
