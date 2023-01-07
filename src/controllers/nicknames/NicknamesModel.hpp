7b7b716cd3f4ea2b2a38531fb5fb937a9b3f83d9
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-01-07 14:59:52 +0100

    NicknamesModel.hpp: Remove `virtual` when `override` is used

--- src/controllers/nicknames/NicknamesModel.hpp
+++ src/controllers/nicknames/NicknamesModel.hpp
@@ -17,12 +17,12 @@ public:
 
 protected:
     // turn a vector item into a model row
-    virtual Nickname getItemFromRow(std::vector<QStandardItem *> &row,
-                                    const Nickname &original) override;
+    Nickname getItemFromRow(std::vector<QStandardItem *> &row,
+                            const Nickname &original) override;
 
     // turns a row in the model into a vector item
-    virtual void getRowFromItem(const Nickname &item,
-                                std::vector<QStandardItem *> &row) override;
+    void getRowFromItem(const Nickname &item,
+                        std::vector<QStandardItem *> &row) override;
 };
 
 }  // namespace chatterino
