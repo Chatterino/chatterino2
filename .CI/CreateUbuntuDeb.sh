db4e5ec7d8afaef8bdf5164e21a2b4b948b683dd
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-01-14 12:08:30 +0100

    Fixed `Could not load the Qt platform plugin "xcb" in "" even though it was found.`

--- .CI/CreateUbuntuDeb.sh
+++ .CI/CreateUbuntuDeb.sh
@@ -29,7 +29,7 @@ Priority: optional
 Architecture: amd64
 Maintainer: Mm2PL <mm2pl@kotmisia.pl>
 Description: Testing out chatterino as a Ubuntu package
-Depends: libboost-filesystem-dev
+Depends: libboost-filesystem-dev, libxcb-xinerama0
 EOF
 echo "Version: $chatterino_version" >> "$packaging_dir/DEBIAN/control"
 cat >> "$packaging_dir/DEBIAN/postinst" << EOF
