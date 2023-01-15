51e24d71bccb2cb08d9b33d92b9baf375755219c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-01-15 12:02:00 +0100

    Remove ChanneLoggingModel source file comments

--- src/controllers/logging/ChannelLoggingModel.cpp
+++ src/controllers/logging/ChannelLoggingModel.cpp
@@ -4,20 +4,17 @@
 
 namespace chatterino {
 
-// commandmodel
 ChannelLoggingModel ::ChannelLoggingModel(QObject *parent)
     : SignalVectorModel<ChannelLog>(Column::COUNT, parent)
 {
 }
 
-// turn a vector item into a model row
 ChannelLog ChannelLoggingModel::getItemFromRow(
     std::vector<QStandardItem *> &row, const ChannelLog &channelLog)
 {
     return ChannelLog(row[Column::Channel]->data(Qt::DisplayRole).toString());
 }
 
-// turns a row in the model into a vector item
 void ChannelLoggingModel::getRowFromItem(const ChannelLog &item,
                                          std::vector<QStandardItem *> &row)
 {
