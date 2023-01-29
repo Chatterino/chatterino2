b904dbc1c4b531308efd22f4c43471036f937f41
Mm2PL <miau@mail.kotmisia.pl>
2023-01-29 22:08:59 +0100

    Add some callbacks back and forth

--- src/controllers/plugins/PluginController.hpp
+++ src/controllers/plugins/PluginController.hpp
@@ -3,19 +3,59 @@
 #include "common/Singleton.hpp"
 #include "singletons/Paths.hpp"
 
-#include <QDebug>
 #include <QDir>
+#include <QFileInfo>
+#include <QString>
+
+#include <map>
+#include <memory>
+#include <utility>
+#include <vector>
+
+class lua_State;
 
 namespace chatterino {
+
+//class Registration
+//{
+//public:
+//    enum Type {
+//        COMMAND,
+//    };
+//
+//    Type type;
+//    QString name;
+//    const char *receiverFunctionName;
+//};
+
+class Plugin
+{
+public:
+    QString name;
+    Plugin(QString name, lua_State *state)
+        : name(std::move(name))
+        , state_(state)
+    {
+    }
+
+private:
+    lua_State *state_;
+
+    friend class PluginController;
+};
 
 class PluginController : public Singleton
 {
 public:
     void initialize(Settings &settings, Paths &paths) override;
     void save() override{};
+    void callEvery(const QString &functionName);
 
 private:
     void load(QFileInfo index, QDir pluginDir);
+    void loadChatterinoLib(lua_State *l);
+
+    std::map<QString, std::unique_ptr<Plugin>> plugins;
 };
 
 };  // namespace chatterino
