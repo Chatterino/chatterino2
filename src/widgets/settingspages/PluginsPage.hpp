dc5141dfc774bc05eed8c6538c02f574e305616a
Mm2PL <miau@mail.kotmisia.pl>
2023-01-31 15:46:01 +0100

    Rebuild plugin settings UI on plugin reload

--- src/widgets/settingspages/PluginsPage.hpp
+++ src/widgets/settingspages/PluginsPage.hpp
@@ -1,9 +1,20 @@
 #pragma once
+#include "util/LayoutCreator.hpp"
 #include "widgets/settingspages/SettingsPage.hpp"
+
+#include <QFormLayout>
+#include <QGroupBox>
 namespace chatterino {
+class Plugin;
+
 class PluginsPage : public SettingsPage
 {
 public:
     PluginsPage();
+
+private:
+    void rebuildContent();
+
+    LayoutCreator<QScrollArea> scrollArea_;
 };
 }  // namespace chatterino
