179af53a7d0c8874bf426cd96851a59a62f75b92
Mm2PL <miau@mail.kotmisia.pl>
2023-02-01 22:42:17 +0100

    Move Plugin and PluginMeta into plugin.{c,h}pp

---
+++ src/controllers/plugins/Plugin.cpp
@@ -0,0 +1,29 @@
+#include "Plugin.hpp"
+
+namespace chatterino {
+bool Plugin::registerCommand(const QString &name, const QString &functionName)
+{
+    if (this->ownedCommands.find(name) != this->ownedCommands.end())
+    {
+        return false;
+    }
+
+    auto ok = getApp()->commands->registerPluginCommand(name);
+    if (!ok)
+    {
+        return false;
+    }
+    this->ownedCommands.insert({name, functionName});
+    return true;
+}
+
+std::set<QString> Plugin::listRegisteredCommands()
+{
+    std::set<QString> out;
+    for (const auto &[name, _] : this->ownedCommands)
+    {
+        out.insert(name);
+    }
+    return out;
+}
+}  // namespace chatterino
---
+++ src/controllers/plugins/Plugin.hpp
@@ -0,0 +1,97 @@
+#pragma once
+#include "Application.hpp"
+#include "controllers/commands/CommandController.hpp"
+
+#include <QDir>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QString>
+#include <semver/semver.hpp>
+
+#include <set>
+#include <vector>
+
+struct lua_State;
+
+namespace chatterino {
+
+struct PluginMeta {
+    QString name;
+    QString description;
+    QString authors;
+    QString homepage;
+
+    QString license;
+    semver::version version;
+
+    std::vector<QString> tags;
+
+    std::set<QString> libraryPermissions;
+
+    explicit PluginMeta(const QJsonObject &obj)
+        : name(obj.value("name").toString("A Plugin with no name"))
+        , description(obj.value("description").toString("Nothing here"))
+        , authors(
+              obj.value("authors").toString("[please tell me who made this]"))
+        , homepage(obj.value("homepage").toString("[https://example.com]"))
+        , license(obj.value("license").toString("[unknown]"))
+    {
+        auto v = semver::from_string_noexcept(
+            obj.value("version").toString().toStdString());
+        if (v.has_value())
+        {
+            this->version = v.value();
+        }
+        else
+        {
+            this->version = semver::version(0, 0, 0);
+            description.append("\nWarning: invalid version. Use semver.");
+        }
+        for (const auto &t : obj.value("tags").toArray())
+        {
+            this->tags.push_back(t.toString());
+        }
+        for (const auto &t : obj.value("library_permissions").toArray())
+        {
+            this->libraryPermissions.insert(t.toString());
+        }
+    }
+
+    bool hasDangerousLibraries()
+    {
+        const auto *perms = &this->libraryPermissions;
+        return perms->contains("io") || perms->contains("package") ||
+               perms->contains("os");
+    }
+};
+
+class Plugin
+{
+public:
+    QString codename;
+    PluginMeta meta;
+    bool isDupeName{};
+
+    Plugin(QString codename, lua_State *state, PluginMeta meta,
+           const QDir &loadDirectory)
+        : codename(std::move(codename))
+        , meta(std::move(meta))
+        , loadDirectory_(loadDirectory)
+        , state_(state)
+    {
+    }
+
+    bool registerCommand(const QString &name, const QString &functionName);
+
+    std::set<QString> listRegisteredCommands();
+
+private:
+    QDir loadDirectory_;
+    lua_State *state_;
+
+    // maps command name -> function name
+    std::map<QString, QString> ownedCommands;
+
+    friend class PluginController;
+};
+}  // namespace chatterino
