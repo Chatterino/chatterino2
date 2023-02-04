2050776fdaeea135d9653f17eec2bd36170db204
Mm2PL <miau@mail.kotmisia.pl>
2023-02-04 01:55:13 +0100

    Clean up diff

--- src/Application.cpp
+++ src/Application.cpp
@@ -148,8 +148,7 @@ void Application::initialize(Settings &settings, Paths &paths)
                     if (auto channel = split->getChannel(); !channel->isEmpty())
                     {
                         channel->addMessage(makeSystemMessage(
-                            "Chatterino unexpectedly crashed and "
-                            "restarted. "
+                            "Chatterino unexpectedly crashed and restarted. "
                             "You can disable automatic restarts in the "
                             "settings."));
                     }
