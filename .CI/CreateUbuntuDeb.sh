e22e996f6d2bd971429a462f4fb7f5dead6d688f
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-02-04 17:46:37 +0100

    Change back to bundle all of qt again

--- .CI/CreateUbuntuDeb.sh
+++ .CI/CreateUbuntuDeb.sh
@@ -40,16 +40,16 @@ echo "Version: $chatterino_version" >> "$packaging_dir/DEBIAN/control"
 # EOF
 # chmod 555 "$packaging_dir/DEBIAN/postinst"
 
-mkdir -p $packaging_dir/usr
-cp -R ./appdir/usr/bin $packaging_dir/usr
-mkdir -p $packaging_dir/usr/share
-cp -R ./appdir/usr/share/icons $packaging_dir/usr/share
-cp -R ./appdir/usr/share/applications $packaging_dir/usr/share
+# mkdir -p $packaging_dir/usr
+# cp -R ./appdir/usr/bin $packaging_dir/usr
+# mkdir -p $packaging_dir/usr/share
+# cp -R ./appdir/usr/share/icons $packaging_dir/usr/share
+# cp -R ./appdir/usr/share/applications $packaging_dir/usr/share
 
 sed -i 's/Exec=chatterino/Exec=env LD_LIBRARY_PATH=\/lib\/Qt\/5.15.2\/gcc_64\/lib\/ chatterino/g' $packaging_dir/usr/share/applications/com.chatterino.chatterino.desktop
 
-# echo "Running make install in package dir"
-# DESTDIR="$packaging_dir" make INSTALL_ROOT="$packaging_dir" -j"$(nproc)" install; find "$packaging_dir/"
+echo "Running make install in package dir"
+DESTDIR="$packaging_dir" make INSTALL_ROOT="$packaging_dir" -j"$(nproc)" install; find "$packaging_dir/"
 # DESTDIR="$packaging_dir" make -j"$(nproc)" install; find "$packaging_dir/"
 # make -j"$(nproc)" install; find "$packaging_dir/"
 echo ""
@@ -57,17 +57,17 @@ echo ""
 echo "$packaging_dir$(pwd)/appdir/usr"
 echo "$packaging_dir/"
 # move directory up
-# mv "$packaging_dir$(pwd)/appdir/usr" "$packaging_dir/" # remove INSTALL_ROOT
+mv "$packaging_dir$(pwd)/appdir/usr" "$packaging_dir/" # remove INSTALL_ROOT
 
-# mkdir -p "$packaging_dir/lib/qt"
-# cp -R "../qt" "$packaging_dir/lib"
-mkdir -p "$packaging_dir/lib/Qt/5.15.2/gcc_64/lib"
-for i in "5.12.12" "5.15.2"; do
-    for j in "libQt5Core" "libQt5Concurrent" "libQt5Gui" "libQt5Network" "libQt5Svg" "libQt5Widgets" "libQt5DBus"; do
-        cp ../qt/Qt/$i/gcc_64/lib/$j* $packaging_dir/lib/Qt/$i/gcc_64/lib/ 2>/dev/null || true
-    done
-done
-# rm -vrf "$packaging_dir/home" || true
+mkdir -p "$packaging_dir/lib/qt"
+cp -R "../qt" "$packaging_dir/lib"
+# mkdir -p "$packaging_dir/lib/Qt/5.15.2/gcc_64/lib"
+# for i in "5.12.12" "5.15.2"; do
+#     for j in "libQt5Core" "libQt5Concurrent" "libQt5Gui" "libQt5Network" "libQt5Svg" "libQt5Widgets" "libQt5DBus"; do
+#         cp ../qt/Qt/$i/gcc_64/lib/$j* $packaging_dir/lib/Qt/$i/gcc_64/lib/ 2>/dev/null || true
+#     done
+# done
+rm -vrf "$packaging_dir/home" || true
 
 echo "Building package..."
 dpkg-deb --build "$packaging_dir" "Chatterino-x86_64.deb"
