04b8b65a24a5797ca97e92e85d5aac0616e39b03
Mm2PL <miau@mail.kotmisia.pl>
2023-02-11 12:57:16 +0100

    Update docs

--- src/controllers/plugins/LuaApi.hpp
+++ src/controllers/plugins/LuaApi.hpp
@@ -6,16 +6,21 @@ namespace chatterino::lua::api {
 // names in this namespace reflect what's visible inside Lua and follow the lua naming scheme
 
 // NOLINTBEGIN(readability-identifier-naming)
+// Following functions are exposed in c2 table.
 int c2_register_command(lua_State *L);
 int c2_send_msg(lua_State *L);
 int c2_system_msg(lua_State *L);
 int c2_log(lua_State *L);
 
+// These ones are global
 int g_load(lua_State *L);
-int g_dofile(lua_State *L);
 int g_print(lua_State *L);
+
+// this one is exposed as execfile
+int g_dofile(lua_State *L);
 // NOLINTEND(readability-identifier-naming)
 
+// Exposed as c2.LogLevel
 // Represents "calls" to qCDebug, qCInfo ...
 enum LogLevel { Debug, Info, Warning, Critical };
 
