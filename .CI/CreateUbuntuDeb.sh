60caebbc52e94541f5e52237a74c2992dee6dc1f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-02-11 02:20:41 +0100

    Clean up .deb creation script

--- .CI/CreateUbuntuDeb.sh
+++ .CI/CreateUbuntuDeb.sh
@@ -2,10 +2,36 @@
 
 set -e
 
+breakline() {
+    printf "================================================================================\n\n"
+}
+
+# Configured in the CI step
+install_prefix="appdir/usr"
+
+# The directory we finally pack into our .deb package
+packaging_dir="package"
+
 # Get the Ubuntu Release (e.g. 20.04 or 22.04)
 ubuntu_release="$(lsb_release -rs)"
 
+# Refactor opportunity:
+case "$ubuntu_release" in
+    20.04)
+        dependencies="libc6, libstdc++6, libqt5core5a, libqt5concurrent5, libqt5dbus5, libqt5gui5, libqt5network5, libqt5svg5, libqt5widgets5"
+        ;;
+    22.04)
+        echo "Dependencies for Ubuntu 22.04 are not yet figured out"
+        exit 1
+        ;;
+    *)
+        echo "Unsupported Ubuntu release $ubuntu_release"
+        exit 1
+        ;;
+esac
+
 echo "Building Ubuntu .deb file on '$ubuntu_release'"
+echo "Dependencies: $dependencies"
 
 if [ ! -f ./bin/chatterino ] || [ ! -x ./bin/chatterino ]; then
     echo "ERROR: No chatterino binary file found. This script must be run in the build folder, and chatterino must be built first."
@@ -14,65 +40,53 @@ fi
 
 chatterino_version=$(git describe 2>/dev/null | cut -c 2-) || true
 if [ -z "$chatterino_version" ]; then
+    # Fall back to this in case the build happened outside of a git repo
     chatterino_version="0.0.0-dev"
-    echo "Falling back to setting the version to '$chatterino_version'"
-else
-    echo "Found Chatterino version $chatterino_version via git"
 fi
 
-rm -vrf "./package" || true  # delete any old packaging dir
+# Make sure no old remnants of a previous packaging remains
+rm -vrf "$packaging_dir"
 
-# create ./package/ from scratch
-mkdir package/DEBIAN -p
-packaging_dir="$(realpath ./package)"
+mkdir -p "$packaging_dir/DEBIAN"
 
 echo "Making control file"
 cat >> "$packaging_dir/DEBIAN/control" << EOF
 Package: chatterino
-Section: net
-Priority: optional
+Version: $chatterino_version
 Architecture: amd64
 Maintainer: Mm2PL <mm2pl@kotmisia.pl>
+Depends: $dependencies
+Section: net
+Priority: optional
+Homepage: https://github.com/Chatterino/chatterino2
 Description: Testing out chatterino as a Ubuntu package
-# Depends: libboost-filesystem1.71.0, libxcb-xinerama0
 EOF
-echo "Version: $chatterino_version" >> "$packaging_dir/DEBIAN/control"
-# cat >> "$packaging_dir/DEBIAN/postinst" << EOF
-# export LD_LIBRARY_PATH=/lib/qt/Qt/5.15.2/gcc_64/lib:"$LD_LIBRARY_PATH"
-# EOF
-# chmod 555 "$packaging_dir/DEBIAN/postinst"
+cat "$packaging_dir/DEBIAN/control"
+breakline
 
-# mkdir -p $packaging_dir/usr
-# cp -R ./appdir/usr/bin $packaging_dir/usr
-# mkdir -p $packaging_dir/usr/share
-# cp -R ./appdir/usr/share/icons $packaging_dir/usr/share
-# cp -R ./appdir/usr/share/applications $packaging_dir/usr/share
 
-echo "Running make install in package dir"
-DESTDIR="$packaging_dir" make INSTALL_ROOT="$packaging_dir" -j"$(nproc)" install; find "$packaging_dir/"
-# DESTDIR="$packaging_dir" make -j"$(nproc)" install; find "$packaging_dir/"
-# make -j"$(nproc)" install; find "$packaging_dir/"
-echo ""
+echo "Running make install"
+make install
+find "$install_prefix"
+breakline
 
-echo "$packaging_dir$(pwd)/appdir/usr"
-echo "$packaging_dir/"
-# move directory up
-mv "$packaging_dir$(pwd)/appdir/usr" "$packaging_dir/" # remove INSTALL_ROOT
 
-# Add LD_LIBRARY_PATH to environment of .desktop file
-sed -i 's/Exec=chatterino/Exec=env LD_LIBRARY_PATH=\/usr\/lib\/qt\/Qt\/5.15.2\/gcc_64\/lib\/ chatterino/g' $packaging_dir/usr/share/applications/com.chatterino.chatterino.desktop
+echo "Merge install into packaging dir"
+cp -rv "$install_prefix/" "$packaging_dir/"
+find "$packaging_dir"
+breakline
 
-mkdir -p "$packaging_dir/lib/qt"
-cp -R "../qt" "$packaging_dir/lib"
-# mkdir -p "$packaging_dir/lib/Qt/5.15.2/gcc_64/lib"
-# for i in "5.12.12" "5.15.2"; do
-#     for j in "libQt5Core" "libQt5Concurrent" "libQt5Gui" "libQt5Network" "libQt5Svg" "libQt5Widgets" "libQt5DBus"; do
-#         cp ../qt/Qt/$i/gcc_64/lib/$j* $packaging_dir/lib/Qt/$i/gcc_64/lib/ 2>/dev/null || true
-#     done
-# done
-rm -vrf "$packaging_dir/home" || true
 
-echo "Building package..."
+echo "Building package"
 dpkg-deb --build "$packaging_dir" "Chatterino-x86_64.deb"
-dpkg -I Chatterino-x86_64.deb # Shows Info from controlfile
-dpkg -c Chatterino-x86_64.deb # Shows folders and files inside .deb file
+breakline
+
+
+echo "Package info"
+dpkg --info Chatterino-x86_64.deb
+breakline
+
+
+echo "Package contents"
+dpkg --contents Chatterino-x86_64.deb # Shows folders and files inside .deb file
+breakline
