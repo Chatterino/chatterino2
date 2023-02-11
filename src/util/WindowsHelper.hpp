1d3ca0bfa39fbd60a24f784368e8949ec88fdb6a
Sam Heybey <sam@heybey.org>
2023-02-10 21:21:09 -0500

    Use `AssocQueryString` instead of directly querying the registry (#4362)
    Co-authored-by: Rasmus Karlsson <rasmus.karlsson@pajlada.com>

--- src/util/WindowsHelper.hpp
+++ src/util/WindowsHelper.hpp
@@ -7,11 +7,15 @@
 
 namespace chatterino {
 
+enum class AssociationQueryType { Protocol, FileExtension };
+
 boost::optional<UINT> getWindowDpi(HWND hwnd);
 void flushClipboard();
 
 bool isRegisteredForStartup();
 void setRegisteredForStartup(bool isRegistered);
+
+QString getAssociatedCommand(AssociationQueryType queryType, LPCWSTR query);
 
 }  // namespace chatterino
 
