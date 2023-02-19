64e69aaf8a5233c086aca0e0dbb9ec2d6aa44849
Nerixyz <nerixdev@outlook.de>
2023-02-19 12:30:41 +0100

    fix: only store the five most recent dumps
    Fixes #4391.

--- src/RunGui.cpp
+++ src/RunGui.cpp
@@ -20,6 +20,7 @@
 #include <QtConcurrent>
 
 #include <csignal>
+#include <ranges>
 
 #ifdef USEWINSDK
 #    include "util/WindowsHelper.hpp"
@@ -185,6 +186,24 @@ namespace {
         }
         qCDebug(chatterinoCache) << "Deleted" << deletedCount << "files";
     }
+
+    // We delete all but the five most recent crashdumps. This strategy may be
+    // improved in the future.
+    void clearCrashes(QDir dir)
+    {
+        dir.setNameFilters({"*.dmp"});
+
+        size_t deletedCount = 0;
+        for (auto &&info :
+             dir.entryInfoList(QDir::Files, QDir::Time) | std::views::drop(5))
+        {
+            if (QFile(info.absoluteFilePath()).remove())
+            {
+                deletedCount++;
+            }
+        }
+        qCDebug(chatterinoApp) << "Deleted" << deletedCount << "crashdumps";
+    }
 }  // namespace
 
 void runGui(QApplication &a, Paths &paths, Settings &settings)
@@ -215,10 +234,20 @@ void runGui(QApplication &a, Paths &paths, Settings &settings)
     });
 
     // Clear the cache 1 minute after start.
-    QTimer::singleShot(60 * 1000, [cachePath = paths.cacheDirectory()] {
+    QTimer::singleShot(60 * 1000, [cachePath = paths.cacheDirectory(),
+                                   crashDirectory = paths.crashdumpDirectory] {
         QtConcurrent::run([cachePath]() {
             clearCache(cachePath);
         });
+
+        auto crashReportsDir = QDir(crashDirectory);
+        // check if the /reports directory exists
+        if (crashReportsDir.cd("reports"))
+        {
+            QtConcurrent::run([crashReportsDir]() {
+                clearCrashes(crashReportsDir);
+            });
+        }
     });
 
     chatterino::NetworkManager::init();
