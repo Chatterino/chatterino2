094abf57400736f0b126785108f4032bab33bad0
killertofus <cinemassacres@gmail.com>
2023-02-26 22:31:40 +0000

    Update Message.cpp

--- src/messages/Message.cpp
+++ src/messages/Message.cpp
@@ -46,12 +46,6 @@ SBHighlight Message::getScrollBarHighlight() const
             ColorProvider::instance().color(ColorType::RedeemedHighlight),
             SBHighlight::Default, true);
     }
-    else if (this->flags.has(MessageFlag::ElevatedMessage))
-    {
-        return SBHighlight(ColorProvider::instance().color(
-                               ColorType::ElevatedMessageHighlight),
-                           SBHighlight::Default, false, false, true);
-    }
     else if (this->flags.has(MessageFlag::FirstMessage))
     {
         return SBHighlight(
