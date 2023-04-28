ec31dc642b5c9b7710b6d312c405a75a709fda2d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-04-29 00:26:07 +0200

    Split up CreateDMG.sh script

--- .CI/CreateDMG.sh
+++ .CI/CreateDMG.sh
@@ -2,43 +2,14 @@
 
 set -eo pipefail
 
-if [ -d bin/chatterino.app ] && [ ! -d chatterino.app ]; then
-    >&2 echo "Moving bin/chatterino.app down one directory"
-    mv bin/chatterino.app chatterino.app
+if [ ! -d chatterino.app ]; then
+    echo "ERROR: No 'chatterino.app' dir found in the build directory. Make sure you've run ./CI/MacDeploy.sh"
+    exit 1
 fi
 
 if [ -z "$OUTPUT_DMG_PATH" ]; then
     echo "ERROR: Must specify the path for where to save the final .dmg"
     exit 1
-fi
-
-if [ -n "$Qt5_DIR" ]; then
-    echo "Using Qt DIR from Qt5_DIR: $Qt5_DIR"
-    _QT_DIR="$Qt5_DIR"
-elif [ -n "$Qt6_DIR" ]; then
-    echo "Using Qt DIR from Qt6_DIR: $Qt6_DIR"
-    _QT_DIR="$Qt6_DIR"
-fi
-
-if [ -n "$_QT_DIR" ]; then
-    export PATH="${_QT_DIR}/bin:$PATH"
-else
-    echo "No Qt environment variable set, assuming system-installed Qt"
-fi
-
-echo "Running MACDEPLOYQT"
-
-_macdeployqt_args=()
-
-if [ -n "$MACOS_CODESIGN_CERTIFICATE" ]; then
-    _macdeployqt_args+=("-codesign=$MACOS_CODESIGN_CERTIFICATE")
-fi
-
-macdeployqt chatterino.app "${_macdeployqt_args[@]}"
-
-if [ -n "$MACOS_CODESIGN_CERTIFICATE" ]; then
-    # Validate that chatterino.app was codesigned correctly
-    codesign -v chatterino.app
 fi
 
 if [ -z "$SKIP_VENV" ]; then
