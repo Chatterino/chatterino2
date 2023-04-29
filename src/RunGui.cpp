80588151ba4c9418f88d4befa59e4687d52db75b
CycloneTM <ianbuddy278@gmail.com>
2023-04-29 09:32:08 -0500

    Change to ifndef

--- src/RunGui.cpp
+++ src/RunGui.cpp
@@ -78,7 +78,11 @@ namespace {
         QApplication::setAttribute(Qt::AA_Use96Dpi, true);
 #ifdef Q_OS_WIN32
         QApplication::setAttribute(Qt::AA_DisableHighDpiScaling, true);
+
+#    ifndef
         QApplication::setWindowIcon(QIcon(":/icon.ico"));
+
+#    endif
 #endif
 
         QApplication::setStyle(QStyleFactory::create("Fusion"));
