4c23f4bceaa688416e793c91f7dcc18d9781e9b3
pajlada <rasmus.karlsson@pajlada.com>
2023-04-29 16:07:20 +0200

    Allow for local macOS signed builds (#4582)
    Split up the CreateDMG.sh macos script to two scripts:
    MacDeploy.sh - this calls macdeployqt on the built app
    CreateDMG.sh - this calls dmgbuild on the built & deployed app
    
    Add a `SKIP_VENV` environment variable to CreateDMG.sh, this can be used to use the system version of dmgbuild
    Add the ability to codesign the created dmg and its contents using the `MACOS_CODESIGN_CERTIFICATE` environment variable
    Moved the output name logic from CreateDMG to the `OUTPUT_DMG_PATH` environment variable
    
    The nightly release create job also doesn't remove artifacts now, it only replaces artifacts that are conflicting.
    The downside to this is that if we change the name of an artifact, we need to manually delete the old artifact.
    The upside to this is that we can now upload artifacts that are not handled in the same CI job.

--- .CI/CreateDMG.sh
+++ .CI/CreateDMG.sh
@@ -1,32 +1,38 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ -d bin/chatterino.app ] && [ ! -d chatterino.app ]; then
-    >&2 echo "Moving bin/chatterino.app down one directory"
-    mv bin/chatterino.app chatterino.app
+set -eo pipefail
+
+if [ ! -d chatterino.app ]; then
+    echo "ERROR: No 'chatterino.app' dir found in the build directory. Make sure you've run ./CI/MacDeploy.sh"
+    exit 1
 fi
 
-if [ -n "$Qt5_DIR" ]; then
-    echo "Using Qt DIR from Qt5_DIR: $Qt5_DIR"
-    _QT_DIR="$Qt5_DIR"
-elif [ -n "$Qt6_DIR" ]; then
-    echo "Using Qt DIR from Qt6_DIR: $Qt6_DIR"
-    _QT_DIR="$Qt6_DIR"
+if [ -z "$OUTPUT_DMG_PATH" ]; then
+    echo "ERROR: Must specify the path for where to save the final .dmg. Make sure you've set the OUTPUT_DMG_PATH environment variable."
+    exit 1
 fi
 
-if [ -n "$_QT_DIR" ]; then
-    export PATH="${_QT_DIR}/bin:$PATH"
-else
-    echo "No Qt environment variable set, assuming system-installed Qt"
+if [ -z "$SKIP_VENV" ]; then
+    echo "Creating python3 virtual environment"
+    python3 -m venv venv
+    echo "Entering python3 virtual environment"
+    . venv/bin/activate
+    echo "Installing dmgbuild"
+    python3 -m pip install dmgbuild
 fi
 
-echo "Running MACDEPLOYQT"
-macdeployqt chatterino.app
-echo "Creating python3 virtual environment"
-python3 -m venv venv
-echo "Entering python3 virtual environment"
-. venv/bin/activate
-echo "Installing dmgbuild"
-python3 -m pip install dmgbuild
+if [ -n "$MACOS_CODESIGN_CERTIFICATE" ]; then
+    echo "Codesigning force deep inside the app"
+    codesign -s "$MACOS_CODESIGN_CERTIFICATE" --deep --force chatterino.app
+    echo "Done!"
+fi
+
 echo "Running dmgbuild.."
-dmgbuild --settings ./../.CI/dmg-settings.py -D app=./chatterino.app Chatterino2 chatterino-macos-Qt-$1.dmg
+dmgbuild --settings ./../.CI/dmg-settings.py -D app=./chatterino.app Chatterino2 "$OUTPUT_DMG_PATH"
 echo "Done!"
+
+if [ -n "$MACOS_CODESIGN_CERTIFICATE" ]; then
+    echo "Codesigning the dmg"
+    codesign -s "$MACOS_CODESIGN_CERTIFICATE" --deep --force "$OUTPUT_DMG_PATH"
+    echo "Done!"
+fi
