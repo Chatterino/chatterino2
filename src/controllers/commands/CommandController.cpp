521dd27c4eba505d3692a040ee4698f46ddf38fe
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-05-07 11:42:36 +0200

    move the shieldmode command to its own builtin class

--- src/controllers/commands/CommandController.cpp
+++ src/controllers/commands/CommandController.cpp
@@ -8,6 +8,7 @@
 #include "common/SignalVector.hpp"
 #include "controllers/accounts/AccountController.hpp"
 #include "controllers/commands/builtin/twitch/ChatSettings.hpp"
+#include "controllers/commands/builtin/twitch/ShieldMode.hpp"
 #include "controllers/commands/Command.hpp"
 #include "controllers/commands/CommandContext.hpp"
 #include "controllers/commands/CommandModel.hpp"
@@ -3208,86 +3209,8 @@ void CommandController::initialize(Settings &, Paths &paths)
         return "";
     });
 
-    const auto toggleShieldMode = [](const CommandContext &ctx,
-                                     bool isActivating) -> QString {
-        const QString command = isActivating ? QStringLiteral("/shield")
-                                             : QStringLiteral("/shieldoff");
-        if (ctx.twitchChannel == nullptr)
-        {
-            ctx.channel->addMessage(makeSystemMessage(
-                QStringLiteral("The %1 command only works in Twitch channels")
-                    .arg(command)));
-            return {};
-        }
-
-        auto user = getApp()->accounts->twitch.getCurrent();
-
-        // Avoid Helix calls without Client ID and/or OAuth Token
-        if (user->isAnon())
-        {
-            ctx.channel->addMessage(makeSystemMessage(
-                QStringLiteral("You must be logged in to use the %1 command")
-                    .arg(command)));
-            return {};
-        }
-
-        getHelix()->updateShieldMode(
-            ctx.twitchChannel->roomId(), user->getUserId(), isActivating,
-            [channel = ctx.channel](const auto &res) {
-                if (!res.isActive)
-                {
-                    channel->addMessage(
-                        makeSystemMessage("Shield mode was deactivated."));
-                    return;
-                }
-
-                channel->addMessage(
-                    makeSystemMessage("Shield mode was activated."));
-            },
-            [channel = ctx.channel](const auto error, const auto &message) {
-                using Error = HelixUpdateShieldModeError;
-                QString errorMessage = "Failed to update shield mode - ";
-
-                switch (error)
-                {
-                    case Error::UserMissingScope: {
-                        errorMessage +=
-                            "Missing required scope. Re-login with your "
-                            "account and try again.";
-                    }
-                    break;
-
-                    case Error::MissingPermission: {
-                        errorMessage +=
-                            "You must be a moderator of the channel.";
-                    }
-                    break;
-
-                    case Error::Forwarded: {
-                        errorMessage += message;
-                    }
-                    break;
-
-                    case Error::Unknown:
-                    default: {
-                        errorMessage +=
-                            QString("An unknown error has occurred (%1).")
-                                .arg(message);
-                    }
-                    break;
-                }
-                channel->addMessage(makeSystemMessage(errorMessage));
-            });
-
-        return {};
-    };
-
-    this->registerCommand("/shield", [toggleShieldMode](const auto &ctx) {
-        return toggleShieldMode(ctx, true);
-    });
-    this->registerCommand("/shieldoff", [toggleShieldMode](const auto &ctx) {
-        return toggleShieldMode(ctx, false);
-    });
+    this->registerCommand("/shield", &commands::shieldModeOn);
+    this->registerCommand("/shieldoff", &commands::shieldModeOff);
 }
 
 void CommandController::save()
