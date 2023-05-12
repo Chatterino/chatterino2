920212f3b23543d15b1850b108e96bb80d3d69ee
Mm2PL <miau@mail.kotmisia.pl>
2023-05-12 13:27:07 +0200

    Set easyArgsLabel to the action argumentDescription

--- src/widgets/dialogs/EditHotkeyDialog.cpp
+++ src/widgets/dialogs/EditHotkeyDialog.cpp
@@ -72,6 +72,7 @@ void EditHotkeyDialog::setFromHotkey(std::shared_ptr<Hotkey> hotkey)
         this->ui_->argumentsDescription->setVisible(false);
 
         this->ui_->easyArgsPicker->clear();
+        this->ui_->easyArgsLabel->setText(def->argumentDescription);
         int matchIdx = -1;
         for (int i = 0; i < def->possibleArguments.size(); i++)
         {
@@ -397,6 +398,7 @@ void EditHotkeyDialog::updateArgumentsInput()
             this->ui_->easyArgsPicker->addItem(displayText);
         }
         this->ui_->easyArgsPicker->setCurrentIndex(0);
+        this->ui_->easyArgsLabel->setText(def.argumentDescription);
     }
 }
 
