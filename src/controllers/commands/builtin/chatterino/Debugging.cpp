0f017fd37b829c82908256ab6f5c0901ed945195
Nerixyz <nerixdev@outlook.de>
2023-05-15 23:06:55 +0200

    feat: add command to set logging/filter rules

---
+++ src/controllers/commands/builtin/chatterino/Debugging.cpp
@@ -0,0 +1,43 @@
+#include "controllers/commands/builtin/chatterino/Debugging.hpp"
+
+#include "common/Channel.hpp"
+#include "controllers/commands/CommandContext.hpp"
+#include "messages/MessageBuilder.hpp"
+
+#include <QLoggingCategory>
+#include <QString>
+
+namespace chatterino::commands {
+
+QString setLoggingRules(const CommandContext &ctx)
+{
+    if (ctx.words.size() < 2)
+    {
+        ctx.channel->addMessage(makeSystemMessage(
+            "Usage: /c2:loggingrules <rules...>. To enable debug logging for "
+            "all "
+            "categories from chatterino, use 'chatterino.*.debug=true'. For "
+            "the format on the rules, see "
+            "https://doc.qt.io/qt-6/"
+            "qloggingcategory.html#configuring-categories"));
+        return {};
+    }
+
+    QLoggingCategory::setFilterRules(ctx.words.sliced(1).join('\n'));
+
+    auto message = QStringLiteral("Updated filter rules.");
+
+    if (!qgetenv("QT_LOGGING_RULES").isEmpty())
+    {
+        message += QStringLiteral(
+            " Warning: Logging rules were previously set by the "
+            "QT_LOGGING_RULES environment variable. This might cause "
+            "interference - see: "
+            "https://doc.qt.io/qt-6/qloggingcategory.html#setFilterRules");
+    }
+
+    ctx.channel->addMessage(makeSystemMessage(message));
+    return "";
+}
+
+}  // namespace chatterino::commands
---
+++ src/controllers/commands/builtin/chatterino/Debugging.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class QString;
+
+namespace chatterino {
+
+struct CommandContext;
+
+namespace commands {
+
+    QString setLoggingRules(const CommandContext &ctx);
+
+}  // namespace commands
+
+}  // namespace chatterino
