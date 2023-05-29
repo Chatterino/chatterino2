b5f71a4655699d1e65614ee465b0d3cfc8931c1f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-05-29 14:21:22 +0200

    GenericListModel clang-tidy: add braces

--- src/widgets/listview/GenericListModel.cpp
+++ src/widgets/listview/GenericListModel.cpp
@@ -15,10 +15,14 @@ int GenericListModel::rowCount(const QModelIndex &parent) const
 QVariant GenericListModel::data(const QModelIndex &index, int /* role */) const
 {
     if (!index.isValid())
+    {
         return QVariant();
+    }
 
     if (index.row() >= this->items_.size())
+    {
         return QVariant();
+    }
 
     auto item = this->items_[index.row()].get();
     // See https://stackoverflow.com/a/44503822 .
@@ -37,7 +41,9 @@ void GenericListModel::addItem(std::unique_ptr<GenericListItem> item)
 void GenericListModel::clear()
 {
     if (this->items_.empty())
+    {
         return;
+    }
 
     // {begin,end}RemoveRows needs to be called to notify attached views
     this->beginRemoveRows(QModelIndex(), 0, this->items_.size() - 1);
