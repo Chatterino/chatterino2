b0a186cc20c40579521f59d31d0e1b4cc6975c12
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-07-02 14:34:40 +0200

    Update header comments

--- src/controllers/twitch/LiveController.hpp
+++ src/controllers/twitch/LiveController.hpp
@@ -49,19 +49,40 @@ public:
     void add(const std::shared_ptr<TwitchChannel> &newChannel) override;
 
 private:
+    /**
+     * Run batched Helix Channels & Stream requests for channels
+     *
+     * If a list of channel IDs is passed to request, we only make a request for those channels
+     *
+     * If no list of channels is passed to request (the default behaviour), we make requests for all channels
+     * in the `channels` map.
+     **/
     void request(std::optional<QStringList> optChannelIDs = std::nullopt);
 
-    // List of channel IDs pointing to their Twitch Channel
-    // These channels will have their stream status updated every INTERVAL seconds
+    /**
+     * List of channel IDs pointing to their Twitch Channel
+     *
+     * These channels will have their stream status updated every REFRESH_INTERVAL seconds
+     **/
     std::unordered_map<QString, std::weak_ptr<TwitchChannel>> channels;
     std::shared_mutex channelsMutex;
 
-    QTimer refreshTimer;
-
-    // List of channels that need an immediate live status update
+    /**
+     * List of channels that need an immediate live status update
+     *
+     * These channels will have their stream status updated after at most IMMEDIATE_REQUEST_INTERVAL seconds
+     **/
     std::unordered_set<QString> immediateRequests;
     std::mutex immediateRequestsMutex;
 
+    /**
+     * Timer responsible for refreshing `channels`
+     **/
+    QTimer refreshTimer;
+
+    /**
+     * Timer responsible for refreshing `immediateRequests`
+     **/
     QTimer immediateRequestTimer;
 };
 
