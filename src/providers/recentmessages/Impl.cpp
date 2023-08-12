38ea0d07e96e24b8a94413ad9047d9509b0d0155
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-08-12 12:32:39 +0200

    Split up api & impl

--- src/providers/recentmessages/Impl.cpp
+++ src/providers/recentmessages/Impl.cpp
@@ -1,23 +1,14 @@
-#include "providers/RecentMessagesApi.hpp"
+#include "providers/recentmessages/Impl.hpp"
 
-#include "common/Channel.hpp"
-#include "common/Common.hpp"
 #include "common/Env.hpp"
-#include "common/NetworkRequest.hpp"
-#include "common/NetworkResult.hpp"
 #include "common/QLogging.hpp"
-#include "messages/Message.hpp"
 #include "providers/twitch/IrcMessageHandler.hpp"
 #include "providers/twitch/TwitchChannel.hpp"
 #include "providers/twitch/TwitchMessageBuilder.hpp"
 #include "singletons/Settings.hpp"
 #include "util/FormatTime.hpp"
-#include "util/PostToThread.hpp"
 
-#include <IrcMessage>
 #include <QJsonArray>
-#include <QJsonObject>
-#include <QUrl>
 #include <QUrlQuery>
 
 namespace {
@@ -169,81 +160,3 @@ QUrl constructRecentMessagesUrl(const QString &name)
 }
 
 }  // namespace chatterino::recentmessages::detail
-
-namespace chatterino {
-
-using namespace recentmessages::detail;
-
-void RecentMessagesApi::loadRecentMessages(const QString &channelName,
-                                           std::weak_ptr<Channel> channelPtr,
-                                           ResultCallback onLoaded,
-                                           ErrorCallback onError)
-{
-    qCDebug(LOG) << "Loading recent messages for" << channelName;
-
-    QUrl url = constructRecentMessagesUrl(channelName);
-
-    NetworkRequest(url)
-        .onSuccess([channelPtr, onLoaded](const auto &result) -> Outcome {
-            auto shared = channelPtr.lock();
-            if (!shared)
-            {
-                return Failure;
-            }
-
-            qCDebug(LOG) << "Successfully loaded recent messages for"
-                         << shared->getName();
-
-            auto root = result.parseJson();
-            auto parsedMessages = parseRecentMessages(root);
-
-            // build the Communi messages into chatterino messages
-            auto builtMessages =
-                buildRecentMessages(parsedMessages, shared.get());
-
-            postToThread([shared = std::move(shared), root = std::move(root),
-                          messages = std::move(builtMessages),
-                          onLoaded]() mutable {
-                // Notify user about a possible gap in logs if it returned some messages
-                // but isn't currently joined to a channel
-                if (QString errorCode = root.value("error_code").toString();
-                    !errorCode.isEmpty())
-                {
-                    qCDebug(LOG)
-                        << QString("Got error from API: error_code=%1, "
-                                   "channel=%2")
-                               .arg(errorCode, shared->getName());
-                    if (errorCode == "channel_not_joined" && !messages.empty())
-                    {
-                        shared->addMessage(makeSystemMessage(
-                            "Message history service recovering, there may "
-                            "be gaps in the message history."));
-                    }
-                }
-
-                onLoaded(messages);
-            });
-
-            return Success;
-        })
-        .onError([channelPtr, onError](const NetworkResult &result) {
-            auto shared = channelPtr.lock();
-            if (!shared)
-            {
-                return;
-            }
-
-            qCDebug(LOG) << "Failed to load recent messages for"
-                         << shared->getName();
-
-            shared->addMessage(makeSystemMessage(
-                QStringLiteral(
-                    "Message history service unavailable (Error: %1)")
-                    .arg(result.formatError())));
-
-            onError();
-        })
-        .execute();
-}
-
-}  // namespace chatterino
--- src/providers/recentmessages/Impl.hpp
+++ src/providers/recentmessages/Impl.hpp
@@ -1,21 +1,15 @@
 #pragma once
 
-#include "ForwardDecl.hpp"
+#include "common/Channel.hpp"
+#include "messages/Message.hpp"
 
+#include <IrcMessage>
+#include <QJsonObject>
 #include <QString>
 #include <QUrl>
 
-#include <functional>
 #include <memory>
 #include <vector>
-
-class QJsonObject;
-
-namespace Communi {
-
-class IrcMessage;
-
-}  // namespace Communi
 
 namespace chatterino::recentmessages::detail {
 
@@ -39,27 +33,3 @@ std::vector<MessagePtr> buildRecentMessages(
 QUrl constructRecentMessagesUrl(const QString &name);
 
 }  // namespace chatterino::recentmessages::detail
-
-namespace chatterino {
-
-class RecentMessagesApi
-{
-public:
-    using ResultCallback = std::function<void(const std::vector<MessagePtr> &)>;
-    using ErrorCallback = std::function<void()>;
-
-    /**
-     * @brief Loads recent messages for a channel using the Recent Messages API
-     * 
-     * @param channelName Name of Twitch channel
-     * @param channelPtr Weak pointer to Channel to use to build messages
-     * @param onLoaded Callback taking the built messages as a const std::vector<MessagePtr> &
-     * @param onError Callback called when the network request fails
-     */
-    static void loadRecentMessages(const QString &channelName,
-                                   std::weak_ptr<Channel> channelPtr,
-                                   ResultCallback onLoaded,
-                                   ErrorCallback onError);
-};
-
-}  // namespace chatterino
