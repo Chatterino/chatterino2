fd5e10045b5e92e32f54f9e0b37efcf0d3ab1579
Nerixyz <nerixdev@outlook.de>
2023-09-14 17:14:43 +0200

    fix: try workaround from desktop-app

--- cmake/AVIF.cmake
+++ cmake/AVIF.cmake
@@ -5,6 +5,14 @@ if (libavif_FOUND)
 
     add_library(kimageformats STATIC ${kimageformats_SRC}/avif.cpp)
     set_target_properties(kimageformats PROPERTIES AUTOMOC ON)
-    target_link_libraries(kimageformats PRIVATE Qt${MAJOR_QT_VERSON}::Gui avif)
+    target_link_libraries(kimageformats PRIVATE Qt${MAJOR_QT_VERSON}::Gui)
+
+    if (WIN32)
+        target_link_libraries(kimageformats PRIVATE avif)
+    else()
+        # See https://github.com/desktop-app/cmake_helpers/blob/491a7fdbae6629dd06a53fc17ac06e6827f4b295/target_link_static_libraries.cmake#L16
+        target_link_libraries(kimageformats PRIVATE "-Wl,--push-state,-Bstatic,-lavif,--pop-state")
+    endif()
+
     target_compile_definitions(kimageformats PRIVATE QT_STATICPLUGIN)
 endif()
