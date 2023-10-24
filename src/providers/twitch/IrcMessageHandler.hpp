c3a3aa080177af6a1ed552957dbc819f8e18b06b
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-10-24 09:32:34 +0200

    IrcMessageHandler: Comment parseMessageWithReply

--- src/providers/twitch/IrcMessageHandler.hpp
+++ src/providers/twitch/IrcMessageHandler.hpp
@@ -29,6 +29,10 @@ class IrcMessageHandler
 public:
     static IrcMessageHandler &instance();
 
+    /**
+     * Parse an IRC message into 0 or more Chatterino messages
+     * Takes previously loaded messages into consideration to add reply contexts
+     **/
     std::vector<MessagePtr> parseMessageWithReply(
         Channel *channel, Communi::IrcMessage *message,
         std::vector<MessagePtr> &otherLoaded);
