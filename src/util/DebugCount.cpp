74edcf8faf023fc4e18161f5eca80b201bb21871
Nerixyz <nerixdev@outlook.de>
2023-10-27 20:22:00 +0200

    refactor: move implementations to translation unit

--- src/util/DebugCount.cpp
+++ src/util/DebugCount.cpp
@@ -1,7 +1,79 @@
-#include "DebugCount.hpp"
+#include "util/DebugCount.hpp"
+
+#include "common/UniqueAccess.hpp"
+
+#include <QMap>
+
+namespace {
+
+using namespace chatterino;
+
+struct Count {
+    int64_t value;
+};
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+UniqueAccess<QMap<QString, Count>> COUNTS;
+
+}  // namespace
 
 namespace chatterino {
 
-UniqueAccess<QMap<QString, int64_t>> DebugCount::counts_;
+void DebugCount::set(const QString &name, const int64_t &amount)
+{
+    auto counts = COUNTS.access();
+
+    auto it = counts->find(name);
+    if (it == counts->end())
+    {
+        counts->insert(name, {amount});
+    }
+    else
+    {
+        it.value().value = amount;
+    }
+}
+
+void DebugCount::increase(const QString &name, const int64_t &amount)
+{
+    auto counts = COUNTS.access();
+
+    auto it = counts->find(name);
+    if (it == counts->end())
+    {
+        counts->insert(name, {amount});
+    }
+    else
+    {
+        it.value().value += amount;
+    }
+}
+
+void DebugCount::decrease(const QString &name, const int64_t &amount)
+{
+    auto counts = COUNTS.access();
+
+    auto it = counts->find(name);
+    if (it == counts->end())
+    {
+        counts->insert(name, {-amount});
+    }
+    else
+    {
+        it.value().value -= amount;
+    }
+}
+
+QString DebugCount::getDebugText()
+{
+    auto counts = COUNTS.access();
+
+    QString text;
+    for (auto it = counts->begin(); it != counts->end(); it++)
+    {
+        text += it.key() + ": " + QString::number(it.value().value) + "\n";
+    }
+    return text;
+}
 
 }  // namespace chatterino
--- src/util/DebugCount.hpp
+++ src/util/DebugCount.hpp
@@ -1,111 +1,27 @@
 #pragma once
 
-#include "common/UniqueAccess.hpp"
-
-#include <QMap>
 #include <QString>
-
-#include <mutex>
-#include <typeinfo>
 
 namespace chatterino {
 
 class DebugCount
 {
 public:
+    static void set(const QString &name, const int64_t &amount);
+
+    static void increase(const QString &name, const int64_t &amount);
     static void increase(const QString &name)
     {
-        auto counts = counts_.access();
-
-        auto it = counts->find(name);
-        if (it == counts->end())
-        {
-            counts->insert(name, 1);
-        }
-        else
-        {
-            reinterpret_cast<int64_t &>(it.value())++;
-        }
+        DebugCount::increase(name, 1);
     }
 
-    static void set(const QString &name, const int64_t &amount)
-    {
-        auto counts = counts_.access();
-
-        auto it = counts->find(name);
-        if (it == counts->end())
-        {
-            counts->insert(name, amount);
-        }
-        else
-        {
-            reinterpret_cast<int64_t &>(it.value()) = amount;
-        }
-    }
-
-    static void increase(const QString &name, const int64_t &amount)
-    {
-        auto counts = counts_.access();
-
-        auto it = counts->find(name);
-        if (it == counts->end())
-        {
-            counts->insert(name, amount);
-        }
-        else
-        {
-            reinterpret_cast<int64_t &>(it.value()) += amount;
-        }
-    }
-
+    static void decrease(const QString &name, const int64_t &amount);
     static void decrease(const QString &name)
     {
-        auto counts = counts_.access();
-
-        auto it = counts->find(name);
-        if (it == counts->end())
-        {
-            counts->insert(name, -1);
-        }
-        else
-        {
-            reinterpret_cast<int64_t &>(it.value())--;
-        }
-    }
-    static void decrease(const QString &name, const int64_t &amount)
-    {
-        auto counts = counts_.access();
-
-        auto it = counts->find(name);
-        if (it == counts->end())
-        {
-            counts->insert(name, -amount);
-        }
-        else
-        {
-            reinterpret_cast<int64_t &>(it.value()) -= amount;
-        }
+        DebugCount::decrease(name, 1);
     }
 
-    static QString getDebugText()
-    {
-        auto counts = counts_.access();
-
-        QString text;
-        for (auto it = counts->begin(); it != counts->end(); it++)
-        {
-            text += it.key() + ": " + QString::number(it.value()) + "\n";
-        }
-        return text;
-    }
-
-    QString toString()
-    {
-        return "";
-    }
-
-private:
-    static UniqueAccess<QMap<QString, int64_t>> counts_;
+    static QString getDebugText();
 };
 
 }  // namespace chatterino
