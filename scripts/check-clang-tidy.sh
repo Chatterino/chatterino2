5baeefcbd67b61e06fe8d26b478de6725ba58624
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-01-14 16:39:13 +0100

    Update the script with parallel version

--- scripts/check-clang-tidy.sh
+++ scripts/check-clang-tidy.sh
@@ -4,23 +4,9 @@ set -eu
 
 clang-tidy --version
 
-# clang-tidy --checks '-*,readability-braces-around-statements' src/RunGui.cpp
-while read -r file; do
-    echo "Running clang-tidy on $file"
-    clang-tidy --quiet "$@" "$file"
-done < <(find src/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
-
-while read -r file; do
-    echo "Running clang-tidy on $file"
-    clang-tidy --quiet "$@" "$file"
-done < <(find tests/src/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
-
-while read -r file; do
-    echo "Running clang-tidy on $file"
-    clang-tidy --quiet "$@" "$file"
-done < <(find benchmarks/src/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
-
-while read -r file; do
-    echo "Running clang-tidy on $file"
-    clang-tidy --quiet "$@" "$file"
-done < <(find mocks/include/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
+find \
+    src/ \
+    tests/src/ \
+    benchmarks/src/ \
+    mocks/include/ \
+    -type f \( -name "*.hpp" -o -name "*.cpp" \) -print0 | parallel -0 -j16 -I {} clang-tidy --quiet "$@" "{}"
