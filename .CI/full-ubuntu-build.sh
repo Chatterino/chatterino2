e3c9c1359860711a4bd6941e94116a3bb2860f4c
pajlada <rasmus.karlsson@pajlada.com>
2024-02-10 22:13:53 +0100

    chore: update the ci full build script (#5168)

--- .CI/full-ubuntu-build.sh
+++ .CI/full-ubuntu-build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# TODO: Investigate if the -fno-sized-deallocation flag is still necessary
+# TODO: Test appimage/deb creation
+
 set -e
 
+env
+
+rm -rf build
 mkdir build
-cmake -B build
-CXXFLAGS=-fno-sized-deallocation cmake \
+cmake \
     -B build \
     -DCMAKE_INSTALL_PREFIX=appdir/usr/ \
     -DCMAKE_BUILD_TYPE=Release \
@@ -13,9 +18,9 @@ CXXFLAGS=-fno-sized-deallocation cmake \
     -DCHATTERINO_PLUGINS="$C2_PLUGINS" \
     -DCMAKE_PREFIX_PATH="$Qt6_DIR/lib/cmake" \
     -DBUILD_WITH_QT6="$C2_BUILD_WITH_QT6" \
-    ..
+    -DCMAKE_CXX_FLAGS="-fno-sized-deallocation" \
+    .
 cmake --build build
 
-# TODO: Test appimage/deb creation
 # sh ./../.CI/CreateAppImage.sh
 # sh ./../.CI/CreateUbuntuDeb.sh
