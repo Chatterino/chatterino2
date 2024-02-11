fe1e498a5f1c213dc6a9c5571e5e077ef0c14911
pajlada <rasmus.karlsson@pajlada.com>
2024-02-11 19:11:30 +0100

    chore: Run Ubuntu tests in Docker (#5170)
    Co-authored-by: Wissididom <30803034+Wissididom@users.noreply.github.com>

--- .CI/full-ubuntu-build.sh
+++ .CI/full-ubuntu-build.sh
@@ -13,11 +13,15 @@ cmake \
     -B build \
     -DCMAKE_INSTALL_PREFIX=appdir/usr/ \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_APP=On \
+    -DBUILD_TESTS=On \
+    -DBUILD_BENCHMARKS=On \
     -DUSE_PRECOMPILED_HEADERS=OFF \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
     -DCHATTERINO_PLUGINS="$C2_PLUGINS" \
     -DCMAKE_PREFIX_PATH="$Qt6_DIR/lib/cmake" \
     -DBUILD_WITH_QT6="$C2_BUILD_WITH_QT6" \
+    -DCHATTERINO_STATIC_QT_BUILD=On \
     -DCMAKE_CXX_FLAGS="-fno-sized-deallocation" \
     .
 cmake --build build
