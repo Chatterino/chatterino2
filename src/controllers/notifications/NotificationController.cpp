168e3ab4b02d47d75f5ee2b8a4b58862808859a7
Kleber <kleber.pandino@gmail.com>
2024-02-12 21:15:24 -0300

    Fix formatting

--- src/controllers/notifications/NotificationController.cpp
+++ src/controllers/notifications/NotificationController.cpp
@@ -227,7 +227,8 @@ void NotificationController::removeFakeChannel(const QString channelName)
         {
             const auto &s = snapshot[i];
 
-            if (QString::compare(s->messageText, liveMessageSearchText, Qt::CaseInsensitive))
+            if (QString::compare(s->messageText, liveMessageSearchText,
+                                 Qt::CaseInsensitive))
             {
                 s->flags.set(MessageFlag::Disabled);
                 break;
