Chatterino/chatterino2

commit efcfb1918766cf4b0c78949e1be47d3c05707cf8

Add safe checks around use of QImageReader (#3736)
Co-authored-by: pajlada <rasmus.karlsson@pajlada.com>

src/providers/twitch/TwitchBadges.cpp
@@ -204,8 +204,18 @@ void TwitchBadges::loadEmoteImage(const QString &name, ImagePtr image,
             buffer.open(QIODevice::ReadOnly);
             QImageReader reader(&buffer);
 
-            QImage image;
-            if (reader.imageCount() == 0 || !reader.read(&image))
+            if (!reader.canRead() || reader.size().isEmpty())
+            {
+                return Failure;
+            }
+
+            QImage image = reader.read();
+            if (image.isNull())
+            {
+                return Failure;
+            }
+
+            if (reader.imageCount() <= 0)
             {
                 return Failure;
             }

src/messages/Image.cpp
@@ -138,14 +138,6 @@ namespace detail {
     {
         QVector<Frame<QImage>> frames;
 
-        if (reader.imageCount() == 0)
-        {
-            qCDebug(chatterinoImage)
-                << "Error while reading image" << url.string << ": '"
-                << reader.errorString() << "'";
-            return frames;
-        }
-
         QImage image;
         for (int index = 0; index < reader.imageCount(); ++index)
         {
@@ -413,8 +405,30 @@ void Image::actuallyLoad()
             buffer.open(QIODevice::ReadOnly);
             QImageReader reader(&buffer);
 
+            if (!reader.canRead())
+            {
+                qCDebug(chatterinoImage)
+                    << "Error: image cant be read " << shared->url().string;
+                return Failure;
+            }
+
+            const auto size = reader.size();
+            if (size.isEmpty())
+            {
+                return Failure;
+            }
+
+            // returns 1 for non-animated formats
+            if (reader.imageCount() <= 0)
+            {
+                qCDebug(chatterinoImage)
+                    << "Error: image has less than 1 frame "
+                    << shared->url().string << ": " << reader.errorString();
+                return Failure;
+            }
+
             // use "double" to prevent int overflows
-            if (double(reader.size().width()) * double(reader.size().height()) *
+            if (double(size.width()) * double(size.height()) *
                     double(reader.imageCount()) * 4.0 >
                 double(Image::maxBytesRam))
             {

src/messages/MessageBuilder.cpp
@@ -14,7 +14,6 @@
 #include "util/FormatTime.hpp"
 
 #include <QDateTime>
-#include <QImageReader>
 
 namespace chatterino {
 

commit d5b4237822ed9ddc62111b83250da94b57e24072

fix: correctly load twitch badges in highlights page
this was broken in https://github.com/Chatterino/chatterino2/pull/3736

src/providers/twitch/TwitchBadges.cpp
@@ -242,10 +242,11 @@ void TwitchBadges::getBadgeIcons(const QList<DisplayBadge> &badges,
 void TwitchBadges::loadEmoteImage(const QString &name, ImagePtr image,
                                   BadgeIconCallback &&callback)
 {
-    NetworkRequest(image->url().string)
+    auto url = image->url().string;
+    NetworkRequest(url)
         .concurrent()
         .cache()
-        .onSuccess([this, name, callback](auto result) {
+        .onSuccess([this, name, callback, url](auto result) {
             auto data = result.getData();
 
             // const cast since we are only reading from it
@@ -255,17 +256,18 @@ void TwitchBadges::loadEmoteImage(const QString &name, ImagePtr image,
 
             if (!reader.canRead() || reader.size().isEmpty())
             {
+                qCWarning(chatterinoTwitch)
+                    << "Can't read badge image at" << url << "for" << name
+                    << reader.errorString();
                 return;
             }
 
             QImage image = reader.read();
             if (image.isNull())
             {
-                return;
-            }
-
-            if (reader.imageCount() <= 0)
-            {
+                qCWarning(chatterinoTwitch)
+                    << "Failed reading badge image at" << url << "for" << name
+                    << reader.errorString();
                 return;
             }
 