4bde57574e9aba669d0f7b7d1a0f077ed17365de
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-03-03 12:21:38 +0100

    nit: namespace combining

--- src/util/IpcQueue.hpp
+++ src/util/IpcQueue.hpp
@@ -10,34 +10,34 @@ namespace chatterino {
 
 class Paths;
 
-namespace ipc {
-
-    void initPaths(const Paths *paths);
-
-    void sendMessage(const char *name, const QByteArray &data);
-
-    class IpcQueuePrivate;
-    class IpcQueue
-    {
-    public:
-        ~IpcQueue();
-
-        static std::pair<std::unique_ptr<IpcQueue>, QString> tryReplaceOrCreate(
-            const char *name, size_t maxMessages, size_t maxMessageSize);
-
-        // TODO: use std::expected
-        /// Try to receive a message.
-        /// In the case of an error, the buffer is empty.
-        QByteArray receive();
-
-    private:
-        IpcQueue(IpcQueuePrivate *priv);
-
-        std::unique_ptr<IpcQueuePrivate> private_;
-
-        friend class IpcQueuePrivate;
-    };
-
-}  // namespace ipc
-
 }  // namespace chatterino
+
+namespace chatterino::ipc {
+
+void initPaths(const Paths *paths);
+
+void sendMessage(const char *name, const QByteArray &data);
+
+class IpcQueuePrivate;
+class IpcQueue
+{
+public:
+    ~IpcQueue();
+
+    static std::pair<std::unique_ptr<IpcQueue>, QString> tryReplaceOrCreate(
+        const char *name, size_t maxMessages, size_t maxMessageSize);
+
+    // TODO: use std::expected
+    /// Try to receive a message.
+    /// In the case of an error, the buffer is empty.
+    QByteArray receive();
+
+private:
+    IpcQueue(IpcQueuePrivate *priv);
+
+    std::unique_ptr<IpcQueuePrivate> private_;
+
+    friend class IpcQueuePrivate;
+};
+
+}  // namespace chatterino::ipc
