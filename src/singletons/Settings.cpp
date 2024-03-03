b9bb8dd4621f7f69ce6b9e7754a0ad8682b2931f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-03-03 15:19:53 +0100

    chore: benchmark save/restore snapshot

--- src/singletons/Settings.cpp
+++ src/singletons/Settings.cpp
@@ -8,6 +8,7 @@
 #include "controllers/ignores/IgnorePhrase.hpp"
 #include "controllers/moderationactions/ModerationAction.hpp"
 #include "controllers/nicknames/Nickname.hpp"
+#include "debug/Benchmark.hpp"
 #include "util/Clamp.hpp"
 #include "util/PersistSignalVector.hpp"
 #include "util/WindowsHelper.hpp"
@@ -195,6 +196,8 @@ Settings::~Settings()
 
 void Settings::saveSnapshot()
 {
+    BenchmarkGuard benchmark("Settings::saveSnapshot");
+
     rapidjson::Document *d = new rapidjson::Document(rapidjson::kObjectType);
     rapidjson::Document::AllocatorType &a = d->GetAllocator();
 
@@ -229,6 +232,8 @@ void Settings::restoreSnapshot()
     {
         return;
     }
+
+    BenchmarkGuard benchmark("Settings::restoreSnapshot");
 
     const auto &snapshot = *(this->snapshot_.get());
 
