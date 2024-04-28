43b5257b58df03fa7d61b1e3f244cde4f5c9bac3
Mm2PL <miau@mail.kotmisia.pl>
2024-04-29 01:11:55 +0200

    Render the emote image in settings dialog

--- src/controllers/moderationactions/ModerationActionModel.cpp
+++ src/controllers/moderationactions/ModerationActionModel.cpp
@@ -1,7 +1,59 @@
 #include "controllers/moderationactions/ModerationActionModel.hpp"
 
+#include "Application.hpp"
+#include "common/network/NetworkRequest.hpp"
+#include "common/network/NetworkResult.hpp"
+#include "common/QLogging.hpp"
 #include "controllers/moderationactions/ModerationAction.hpp"
+#include "messages/Image.hpp"
 #include "util/StandardItemHelper.hpp"
+
+#include <QIcon>
+#include <QLoggingCategory>
+#include <QPixmap>
+namespace {
+
+using namespace chatterino;
+
+// This was copied from TwitchBadges::loadEmoteImage and modified.
+void loadEmoteImage(const ImagePtr &image,
+                    std::function<void(QPixmap)> &&callback)
+{
+    auto url = image->url().string;
+    NetworkRequest(url)
+        .concurrent()
+        .cache()
+        .onSuccess(
+            [callback = std::move(callback), url](const NetworkResult &result) {
+                auto data = result.getData();
+
+                // const cast since we are only reading from it
+                QBuffer buffer(const_cast<QByteArray *>(&data));
+                buffer.open(QIODevice::ReadOnly);
+                QImageReader reader(&buffer);
+
+                if (!reader.canRead() || reader.size().isEmpty())
+                {
+                    qCWarning(chatterinoSettings)
+                        << "Can't read mod action image at" << url << ":"
+                        << reader.errorString();
+                    return;
+                }
+
+                QImage image = reader.read();
+                if (image.isNull())
+                {
+                    qCWarning(chatterinoSettings)
+                        << "Failed reading mod action image at" << url << ":"
+                        << reader.errorString();
+                    return;
+                }
+
+                callback(QPixmap::fromImage(image));
+            })
+        .execute();
+}
+}  // namespace
 
 namespace chatterino {
 
@@ -26,6 +78,12 @@ void ModerationActionModel::getRowFromItem(const ModerationAction &item,
 {
     setStringItem(row[Column::Command], item.getAction());
     setFilePathItem(row[Column::Icon], item.iconPath());
+    if (!item.iconPath().isEmpty())
+    {
+        loadEmoteImage(*item.getImage(), [row](const QPixmap &pixmap) {
+            row[Column::Icon]->setData(pixmap, Qt::DecorationRole);
+        });
+    }
 }
 
 }  // namespace chatterino
