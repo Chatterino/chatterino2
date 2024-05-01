d9ff6ca62ccb438a599f5b015f38fe5cf9a2e921
Mm2PL <miau@mail.kotmisia.pl>
2024-05-01 20:25:35 +0200

    Document callback param

--- src/util/LoadPixmap.hpp
+++ src/util/LoadPixmap.hpp
@@ -7,6 +7,9 @@ namespace chatterino {
 
 /**
  * Loads an image from url into a QPixmap. Allows for file:// protocol links. Uses cacheing.
+ *
+ * @param callback The callback you will get the pixmap by. It will be invoked concurrently with no guarantees on which thread.
  */
 void loadPixmapFromUrl(const Url &url, std::function<void(QPixmap)> &&callback);
+
 }  // namespace chatterino
