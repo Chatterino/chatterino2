23a141ee87ecb15fe5d2301b84dfeebbd53264ba
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-05-04 14:46:54 +0200

    Test custom icon path parsing

--- tests/src/ModerationAction.cpp
+++ tests/src/ModerationAction.cpp
@@ -44,6 +44,7 @@ TEST_F(ModerationActionTest, Parse)
 {
     struct TestCase {
         QString action;
+        QString iconPath;
 
         QString expectedLine1;
         QString expectedLine2;
@@ -84,21 +85,27 @@ TEST_F(ModerationActionTest, Parse)
             .expectedLine2 = "rs",
             .expectedType = ModerationAction::Type::Custom,
         },
+        {
+            .action = "forsen",
+            .iconPath = "file:///this-is-the-path-to-the-icon.png",
+            .expectedLine1 = "fo",
+            .expectedLine2 = "rs",
+            .expectedImage =
+                Image::fromUrl(Url("file:///this-is-the-path-to-the-icon.png")),
+            .expectedType = ModerationAction::Type::Custom,
+        },
     };
 
     for (const auto &test : tests)
     {
-        ModerationAction moderationAction(test.action);
+        ModerationAction moderationAction(test.action, test.iconPath);
 
         EXPECT_EQ(moderationAction.getAction(), test.action);
 
         EXPECT_EQ(moderationAction.getLine1(), test.expectedLine1);
         EXPECT_EQ(moderationAction.getLine2(), test.expectedLine2);
 
-        if (test.expectedImage.has_value())
-        {
-            EXPECT_EQ(moderationAction.getImage(), test.expectedImage);
-        }
+        EXPECT_EQ(moderationAction.getImage(), test.expectedImage);
 
         EXPECT_EQ(moderationAction.getType(), test.expectedType);
     }
