68a2f4fea7de8e371197d030e3138167cf3fc993
Nerixyz <nerixdev@outlook.de>
2024-05-11 11:00:27 +0200

    fix: qt stuff

--- src/util/QCompareCaseInsensitive.hpp
+++ src/util/QCompareCaseInsensitive.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <QLatin1String>
+#include <QString>
 #include <QStringView>
 #include <QtGlobal>
-#include <QUtf8StringView>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
+#    include <QUtf8StringView>
+#endif
 
 namespace chatterino {
 
@@ -11,29 +16,29 @@ struct QCompareCaseInsensitive {
     using is_transparent = void;
 
     // clang-format off
-    bool operator()(const QString &   a, const QString &   b) const noexcept;
-    bool operator()(QStringView       a, QStringView       b) const noexcept;
-    bool operator()(QLatin1StringView a, QLatin1StringView b) const noexcept;
+    bool operator()(const QString & a, const QString & b) const noexcept;
+    bool operator()(QStringView     a, QStringView     b) const noexcept;
+    bool operator()(QLatin1String   a, QLatin1String   b) const noexcept;
 
-    bool operator()(const QString &   a, QStringView       b) const noexcept;
-    bool operator()(const QString &   a, QLatin1StringView b) const noexcept;
+    bool operator()(const QString & a, QStringView     b) const noexcept;
+    bool operator()(const QString & a, QLatin1String   b) const noexcept;
 
-    bool operator()(QStringView       a, const QString &   b) const noexcept;
-    bool operator()(QLatin1StringView a, const QString &   b) const noexcept;
+    bool operator()(QStringView     a, const QString & b) const noexcept;
+    bool operator()(QLatin1String   a, const QString & b) const noexcept;
 
-    bool operator()(QStringView       a, QLatin1StringView b) const noexcept;
-    bool operator()(QLatin1StringView a, QStringView       b) const noexcept;
+    bool operator()(QStringView     a, QLatin1String   b) const noexcept;
+    bool operator()(QLatin1String   a, QStringView     b) const noexcept;
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-    bool operator()(QUtf8StringView   a, QUtf8StringView   b) const noexcept;
+    bool operator()(QUtf8StringView a, QUtf8StringView b) const noexcept;
 
-    bool operator()(const QString &   a, QUtf8StringView   b) const noexcept;
-    bool operator()(QStringView       a, QUtf8StringView   b) const noexcept;
-    bool operator()(QLatin1StringView a, QUtf8StringView   b) const noexcept;
+    bool operator()(const QString & a, QUtf8StringView b) const noexcept;
+    bool operator()(QStringView     a, QUtf8StringView b) const noexcept;
+    bool operator()(QLatin1String   a, QUtf8StringView b) const noexcept;
 
-    bool operator()(QUtf8StringView   a, const QString &   b) const noexcept;
-    bool operator()(QUtf8StringView   a, QStringView       b) const noexcept;
-    bool operator()(QUtf8StringView   a, QLatin1StringView b) const noexcept;
+    bool operator()(QUtf8StringView a, const QString & b) const noexcept;
+    bool operator()(QUtf8StringView a, QStringView     b) const noexcept;
+    bool operator()(QUtf8StringView a, QLatin1String   b) const noexcept;
 #endif
     // clang-format on
 };
@@ -50,8 +55,8 @@ inline bool QCompareCaseInsensitive::operator()(QStringView a,
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(
-    QLatin1StringView a, QLatin1StringView b) const noexcept
+inline bool QCompareCaseInsensitive::operator()(QLatin1String a,
+                                                QLatin1String b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
@@ -62,8 +67,8 @@ inline bool QCompareCaseInsensitive::operator()(const QString &a,
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(
-    const QString &a, QLatin1StringView b) const noexcept
+inline bool QCompareCaseInsensitive::operator()(const QString &a,
+                                                QLatin1String b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
@@ -74,19 +79,19 @@ inline bool QCompareCaseInsensitive::operator()(QStringView a,
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(QLatin1StringView a,
+inline bool QCompareCaseInsensitive::operator()(QLatin1String a,
                                                 const QString &b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(
-    QStringView a, QLatin1StringView b) const noexcept
+inline bool QCompareCaseInsensitive::operator()(QStringView a,
+                                                QLatin1String b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(QLatin1StringView a,
+inline bool QCompareCaseInsensitive::operator()(QLatin1String a,
                                                 QStringView b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
@@ -112,7 +117,7 @@ inline bool QCompareCaseInsensitive::operator()(
 }
 
 inline bool QCompareCaseInsensitive::operator()(
-    QLatin1StringView a, QUtf8StringView b) const noexcept
+    QLatin1String a, QUtf8StringView b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
@@ -129,8 +134,8 @@ inline bool QCompareCaseInsensitive::operator()(QUtf8StringView a,
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
 
-inline bool QCompareCaseInsensitive::operator()(
-    QUtf8StringView a, QLatin1StringView b) const noexcept
+inline bool QCompareCaseInsensitive::operator()(QUtf8StringView a,
+                                                QLatin1String b) const noexcept
 {
     return a.compare(b, Qt::CaseInsensitive) < 0;
 }
