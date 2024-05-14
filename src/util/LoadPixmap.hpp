6c11b2400a3e4027ad7bf3fb19c69a4587b0d2ed
Mm2PL <miau@mail.kotmisia.pl>
2024-05-14 14:49:59 +0200

    Add onError callback to load pixmap code

--- src/util/LoadPixmap.hpp
+++ src/util/LoadPixmap.hpp
@@ -8,8 +8,11 @@ namespace chatterino {
 /**
  * Loads an image from url into a QPixmap. Allows for file:// protocol links. Uses cacheing.
  *
- * @param callback The callback you will get the pixmap by. It will be invoked concurrently with no guarantees on which thread.
+ * @param onSuccess The callback you will get the pixmap by. It will be invoked concurrently with no guarantees on which thread.
+ * @param onError The callback that will be called if the request fails or the image cannot be loaded.
  */
-void loadPixmapFromUrl(const Url &url, std::function<void(QPixmap)> &&callback);
+void loadPixmapFromUrl(
+    const Url &url, std::function<void(QPixmap)> &&onSuccess,
+    const std::optional<std::function<void()>> &onError = {});
 
 }  // namespace chatterino
