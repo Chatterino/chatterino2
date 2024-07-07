5f9268811f0201e9c04d87a6b55f4f5a83037045
Mm2PL <miau@mail.kotmisia.pl>
2024-07-07 13:30:09 +0200

    Create src/util/drop.hpp

---
+++ src/util/drop.hpp
@@ -0,0 +1,23 @@
+namespace chatterino {
+
+/**
+ * Calls the destructor of var immediately.
+ *
+ * This is useful before noreturn functions to prevent leaking memory from complex types.
+ */
+template <typename T>
+inline void drop(T &var)
+{
+    var.~T();
+}
+
+/**
+ * Helps you avoid accidentally dropping a pointer not the object behind it.
+ */
+template <typename T>
+inline void drop(T * /*var*/)
+{
+    static_assert(false, "Use delete on a pointer instead of drop().");
+}
+
+}  // namespace chatterino
