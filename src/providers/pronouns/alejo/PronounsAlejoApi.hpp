3495cd95c0152f673acd44f93ebcb42727922bce
Timo Zuccarello <timo@zuccarello.eu>
2024-08-29 14:04:05 +0200

    Move private below public in PronounsAlejoApi.

--- src/providers/pronouns/alejo/PronounsAlejoApi.hpp
+++ src/providers/pronouns/alejo/PronounsAlejoApi.hpp
@@ -13,6 +13,14 @@ namespace chatterino::pronouns {
 
 class AlejoApi
 {
+public:
+    explicit AlejoApi();
+    /** Fetches pronouns from the alejo.io API for a username and calls onDone when done.
+        onDone can be invoked from any thread. The argument is std::nullopt if and only if the request failed.
+     */
+    void fetch(const QString &username,
+               std::function<void(std::optional<UserPronouns>)> onDone);
+
 private:
     std::shared_mutex mutex;
     /** A map from alejo.io ids to human readable representation like theythem -> they/them, other -> other. */
@@ -22,14 +30,6 @@ private:
     inline static const QString API_URL = "https://api.pronouns.alejo.io/v1";
     inline static const QString API_USERS = "/users";
     inline static const QString API_PRONOUNS = "/pronouns";
-
-public:
-    explicit AlejoApi();
-    /** Fetches pronouns from the alejo.io API for a username and calls onDone when done.
-        onDone can be invoked from any thread. The argument is std::nullopt if and only if the request failed.
-     */
-    void fetch(const QString &username,
-               std::function<void(std::optional<UserPronouns>)> onDone);
 };
 
 }  // namespace chatterino::pronouns
