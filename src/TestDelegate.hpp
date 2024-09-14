f77912d0163e024102bcd3874f25d433f5bcf13c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-09-15 00:17:51 +0200

    more tests

--- src/TestDelegate.hpp
+++ src/TestDelegate.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <qpushbutton.h>
 #include <QSize>
 #include <QStyledItemDelegate>
 
@@ -9,12 +10,23 @@ class TestDelegate : public QStyledItemDelegate
 {
 public:
     explicit TestDelegate(QObject *parent);
+    ~TestDelegate();
 
     QSize sizeHint(const QStyleOptionViewItem &option,
                    const QModelIndex &index) const override;
 
     void paint(QPainter *painter, const QStyleOptionViewItem &option,
                const QModelIndex &index) const override;
+
+    void setEditorData(QWidget *editor,
+                       const QModelIndex &index) const override;
+
+    bool editorEvent(QEvent *event, QAbstractItemModel *model,
+                     const QStyleOptionViewItem &option,
+                     const QModelIndex &index) override;
+
+private:
+    QPushButton *btn;
 };
 
 }  // namespace chatterino
