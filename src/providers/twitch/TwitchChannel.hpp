d8960ffae379ac433322aee1cf054cebd6d7feba
Nerixyz <nerixdev@outlook.de>
2024-10-18 12:10:40 +0200

    refactor: move MessageBuilder test to IrcMessageHandler

--- src/providers/twitch/TwitchChannel.hpp
+++ src/providers/twitch/TwitchChannel.hpp
@@ -25,7 +25,7 @@
 #include <optional>
 #include <unordered_map>
 
-class TestMessageBuilderP;
+class TestIrcMessageHandlerP;
 
 namespace chatterino {
 
@@ -464,7 +464,7 @@ private:
     friend class MessageBuilder;
     friend class IrcMessageHandler;
     friend class Commands_E2E_Test;
-    friend class ::TestMessageBuilderP;
+    friend class ::TestIrcMessageHandlerP;
 };
 
 }  // namespace chatterino
