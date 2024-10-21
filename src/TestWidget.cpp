29549f24ead5402f4e420ad0fc0d69e2773c8e4f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-10-21 09:58:09 +0200

    test

--- src/TestWidget.cpp
+++ src/TestWidget.cpp
@@ -1,15 +1,22 @@
 #include "TestWidget.hpp"
 
+#include "controllers/highlights/HighlightPhrase.hpp"
+#include "widgets/helper/color/ColorButton.hpp"
+
 #include <qboxlayout.h>
+#include <qcheckbox.h>
+#include <qlabel.h>
+#include <qlineedit.h>
 #include <qpushbutton.h>
 #include <QStringBuilder>
+#include <qtextedit.h>
 namespace chatterino {
 
 class TestWidgetImpl
 {
 public:
     TestWidgetImpl(QWidget *parent)
-        : btn(new QPushButton("xd", parent))
+        : btn(new QPushButton("xd5", parent))
     {
     }
 
@@ -28,6 +35,63 @@ TestWidget::TestWidget(QWidget *parent)
 void TestWidget::update(const QString &data)
 {
     this->impl->btn->setText("AAAAA" % data);
+}
+
+TestWidget2::TestWidget2(QWidget *parent, const HighlightPhrase &data)
+    : QWidget(parent)
+{
+    auto *layout = new QGridLayout(this);
+
+    auto *lbl = new QLineEdit(data.getPattern());
+    layout->addWidget(lbl, 0, 0, 1, 2);
+
+    {
+        auto *cb = new ColorButton(*data.getColor());
+        layout->addWidget(cb, 0, 3);
+    }
+
+    int row = 0;
+    int column = 0;
+
+    {
+        auto *cb = new QCheckBox("Show in mentions");
+        cb->setChecked(data.showInMentions());
+        layout->addWidget(cb, 1, 0);
+    }
+
+    {
+        auto *cb = new QCheckBox("Flash taskbar");
+        cb->setChecked(data.hasAlert());
+        layout->addWidget(cb, 1, 1);
+    }
+
+    {
+        auto *cb = new QCheckBox("Enable regex");
+        cb->setChecked(data.isRegex());
+        layout->addWidget(cb, 2, 0);
+    }
+
+    {
+        auto *cb = new QCheckBox("Case-sensitive");
+        cb->setChecked(data.isCaseSensitive());
+        layout->addWidget(cb, 2, 1);
+    }
+
+    {
+        auto *cb = new QCheckBox("Play sound");
+        cb->setChecked(data.hasSound());
+        layout->addWidget(cb, 3, 0);
+    }
+
+    {
+        auto *cb = new QCheckBox("Custom sound");
+        // cb->setChecked(data.hasSound());
+        layout->addWidget(cb, 3, 1);
+    }
+
+    // layout->addLayout(gridLayout);
+
+    layout->setSizeConstraint(QLayout::SetFixedSize);
 }
 
 }  // namespace chatterino
