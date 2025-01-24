b8548ad202367eff22b7fe18a82c88b170ffffc6
Nerixyz <nerixdev@outlook.de>
2025-01-24 16:48:46 +0100

    feat(eventsub): generate JSON impls in cmake

--- lib/twitch-eventsub-ws/ast/generate-and-replace-dir.py
+++ lib/twitch-eventsub-ws/ast/generate-and-replace-dir.py
@@ -27,6 +27,11 @@ def main():
         log.error(f"usage: {sys.argv[0]} <path-to-dir>")
         sys.exit(1)
 
+    additional_includes = []
+    if len(sys.argv) >= 4 and sys.argv[2] == "--includes":
+        additional_includes = sys.argv[3].split(";")
+        log.debug(f"additional includes: {additional_includes}")
+
     dir = realpath(sys.argv[1])
     log.debug(f"Searching for header files in {dir}")
 
@@ -42,19 +47,15 @@ def main():
 
         header_path = path.path
         source_path = re.sub(r"\.hpp$", ".cpp", header_path)
-        source_path = re.sub(
-            r"include[/\\]twitch-eventsub-ws[/\\]", "src/", source_path
-        )
+        source_path = re.sub(r"include[/\\]twitch-eventsub-ws[/\\]", "src/", source_path)
 
         if not os.path.isfile(source_path):
-            log.warning(
-                f"Header file {header_path} did not have a matching source file at {source_path}"
-            )
+            log.warning(f"Header file {header_path} did not have a matching source file at {source_path}")
             continue
 
         log.debug(f"Found header & source {header_path} / {source_path}")
 
-        (definition, implementation) = generate(header_path)
+        (definition, implementation) = generate(header_path, additional_includes)
 
         replace_in_file(header_path, definition_markers, definition)
         replace_in_file(source_path, implementation_markers, implementation)
--- lib/twitch-eventsub-ws/ast/lib/build.py
+++ lib/twitch-eventsub-ws/ast/lib/build.py
@@ -1,11 +1,11 @@
-from typing import List, Optional
+from typing import List
 
 import logging
 import os
 
 import clang.cindex
 
-from .helpers import get_clang_builtin_include_dirs, get_cmake_include_dirs
+from .helpers import get_clang_builtin_include_dirs
 from .struct import Struct
 from .enum import Enum
 from .walker import Walker
@@ -13,9 +13,7 @@ from .walker import Walker
 log = logging.getLogger(__name__)
 
 
-def build_structs(
-    filename: str, build_commands: Optional[str] = None
-) -> List[Struct | Enum]:
+def build_structs(filename: str, additional_includes: list[str] = []) -> List[Struct | Enum]:
     if not os.path.isfile(filename):
         raise ValueError(f"Path {filename} is not a file. cwd: {os.getcwd()}")
 
@@ -26,8 +24,7 @@ def build_structs(
     ]
 
     parse_options = (
-        clang.cindex.TranslationUnit.PARSE_INCOMPLETE
-        | clang.cindex.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES
+        clang.cindex.TranslationUnit.PARSE_INCOMPLETE | clang.cindex.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES
     )
 
     extra_includes, system_includes = get_clang_builtin_include_dirs()
@@ -38,7 +35,7 @@ def build_structs(
     for include_dir in extra_includes:
         parse_args.append(f"-I{include_dir}")
 
-    for dir in get_cmake_include_dirs():
+    for dir in additional_includes:
         parse_args.append("-I")
         parse_args.append(dir)
 
@@ -47,9 +44,7 @@ def build_structs(
     file_dir = os.path.dirname(os.path.realpath(filename))
     parse_args.append(f"-I{file_dir}")
     # - Append project include dir
-    file_subdir = os.path.realpath(
-        os.path.join(os.path.dirname(os.path.realpath(filename)), "../..")
-    )
+    file_subdir = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(filename)), "../.."))
     parse_args.append(f"-I{file_subdir}")
 
     # TODO: Use build_commands if available
