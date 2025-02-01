2e38d4f44386f263e296bd3abd672759201c8e05
Nerixyz <nerixdev@outlook.de>
2025-02-01 17:52:51 +0100

    feat: add "Open in custom player" to toast options

---
+++ src/util/CustomPlayer.cpp
@@ -0,0 +1,26 @@
+#include "util/CustomPlayer.hpp"
+
+#include "common/QLogging.hpp"
+#include "singletons/Settings.hpp"
+
+#include <QStringBuilder>
+#include <QStringView>
+
+namespace chatterino {
+
+void openInCustomPlayer(QStringView channelName)
+{
+    QString scheme = getSettings()->customURIScheme.getValue();
+    if (scheme.isEmpty())
+    {
+        qCWarning(chatterinoApp)
+            << "Can't open" << channelName
+            << "in custom player because no URI scheme is set";
+        return;
+    }
+
+    QDesktopServices::openUrl(
+        QUrl{scheme % u"https://twitch.tv/" % channelName});
+}
+
+}  // namespace chatterino
