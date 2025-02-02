3e273947b6c9052a72997f28ebe94182e28b0789
apa420 <apa420@apa420.com>
2025-02-02 12:26:46 +0100

    Add nicknames page tables to global search

--- src/widgets/settingspages/NicknamesPage.cpp
+++ src/widgets/settingspages/NicknamesPage.cpp
@@ -22,31 +22,38 @@ NicknamesPage::NicknamesPage()
         "filters."
         "\nWith those features you will still need to use the user's original "
         "name.");
-    EditableModelView *view =
-        layout
-            .emplace<EditableModelView>(
-                (new NicknamesModel(nullptr))
-                    ->initialized(&getSettings()->nicknames))
-            .getElement();
+    this->view_ = layout
+                      .emplace<EditableModelView>(
+                          (new NicknamesModel(nullptr))
+                              ->initialized(&getSettings()->nicknames))
+                      .getElement();
 
-    view->setTitles({"Username", "Nickname", "Enable regex", "Case-sensitive"});
-    view->getTableView()->horizontalHeader()->setSectionResizeMode(
+    this->view_->setTitles(
+        {"Username", "Nickname", "Enable regex", "Case-sensitive"});
+    this->view_->getTableView()->horizontalHeader()->setSectionResizeMode(
         QHeaderView::Fixed);
-    view->getTableView()->horizontalHeader()->setSectionResizeMode(
+    this->view_->getTableView()->horizontalHeader()->setSectionResizeMode(
         0, QHeaderView::Stretch);
-    view->getTableView()->horizontalHeader()->setSectionResizeMode(
+    this->view_->getTableView()->horizontalHeader()->setSectionResizeMode(
         1, QHeaderView::Stretch);
 
-    // We can safely ignore this signal connection since we own the view
-    std::ignore = view->addButtonPressed.connect([] {
+    // We can safely ignore this signal connection since we own the this->view_
+    std::ignore = this->view_->addButtonPressed.connect([] {
         getSettings()->nicknames.append(
             Nickname{"Username", "Nickname", false, false});
     });
 
-    QTimer::singleShot(1, [view] {
-        view->getTableView()->resizeColumnsToContents();
-        view->getTableView()->setColumnWidth(0, 200);
+    QTimer::singleShot(1, [this] {
+        this->view_->getTableView()->resizeColumnsToContents();
+        this->view_->getTableView()->setColumnWidth(0, 200);
     });
+}
+
+bool NicknamesPage::filterElements(const QString &query)
+{
+    std::array fields{0, 1};
+
+    return this->view_->filterSearchResults(query, fields);
 }
 
 }  // namespace chatterino
