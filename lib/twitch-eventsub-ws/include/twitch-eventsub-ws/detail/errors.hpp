d82d4291f44638b6d48947dbf81059e73ba42e11
Nerixyz <nerixdev@outlook.de>
2025-02-05 20:51:26 +0100

    gcc

--- lib/twitch-eventsub-ws/include/twitch-eventsub-ws/detail/errors.hpp
+++ lib/twitch-eventsub-ws/include/twitch-eventsub-ws/detail/errors.hpp
@@ -2,9 +2,8 @@
 
 #include "twitch-eventsub-ws/errors.hpp"
 
-#define EVENTSUB_BAIL_HERE(kind)                                         \
-    {                                                                    \
-        static constexpr boost::source_location loc =                    \
-            std::source_location::current();                             \
-        return ::chatterino::eventsub::lib::error::makeCode(kind, &loc); \
+#define EVENTSUB_BAIL_HERE(kind)                                              \
+    {                                                                         \
+        static constexpr boost::source_location loc = BOOST_CURRENT_LOCATION; \
+        return ::chatterino::eventsub::lib::error::makeCode(kind, &loc);      \
     }
