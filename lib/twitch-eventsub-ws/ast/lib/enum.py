f2b25792bc3b7467e24a51bee034d53f2c7e2cb6
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-02-07 20:07:24 +0100

    fix: Default to `snake_case` json transform
    This includes a refactor of how comment commands are stored
    
    Now, instead of each place doing the parsing/handling, it's just one
    class handling the parsing with some helper functions for applying
    certain commands.

--- lib/twitch-eventsub-ws/ast/lib/enum.py
+++ lib/twitch-eventsub-ws/ast/lib/enum.py
@@ -17,7 +17,7 @@ class Enum:
         self.name = name
         self.constants: List[EnumConstant] = []
         self.parent: str = ""
-        self.comment_commands: CommentCommands = []
+        self.comment_commands = CommentCommands()
         self.inner_root: str = ""
         self.namespace = namespace
 
@@ -47,18 +47,4 @@ class Enum:
         return env.get_template("enum-definition.tmpl").render(enum=self)
 
     def apply_comment_commands(self, comment_commands: CommentCommands) -> None:
-        for command, value in comment_commands:
-            match command:
-                case "json_rename":
-                    # Do nothing on enums
-                    pass
-                case "json_dont_fail_on_deserialization":
-                    # Do nothing on enums
-                    pass
-                case "json_transform":
-                    # Do nothing on enums
-                    pass
-                case "json_inner":
-                    self.inner_root = value
-                case other:
-                    log.warning(f"Unknown comment command found: {other} with value {value}")
+        self.inner_root = comment_commands.inner_root
--- lib/twitch-eventsub-ws/ast/lib/enum_constant.py
+++ lib/twitch-eventsub-ws/ast/lib/enum_constant.py
@@ -6,7 +6,7 @@ import logging
 
 import clang.cindex
 
-from .comment_commands import CommentCommands, json_transform, parse_comment_commands
+from .comment_commands import CommentCommands
 
 log = logging.getLogger(__name__)
 
@@ -23,31 +23,15 @@ class EnumConstant:
         self.dont_fail_on_deserialization: bool = False
 
     def apply_comment_commands(self, comment_commands: CommentCommands) -> None:
-        for command, value in comment_commands:
-            match command:
-                case "json_rename":
-                    # Rename the key that this field will use in json terms
-                    log.debug(f"Rename json key from {self.json_name} to {value}")
-                    self.json_name = value
-                case "json_dont_fail_on_deserialization":
-                    # Don't fail when an optional object exists and its data is bad
-                    log.debug(f"Don't fail on deserialization for {self.name}")
-                    self.dont_fail_on_deserialization = bool(value.lower() == "true")
-                case "json_transform":
-                    # Transform the key from whatever-case to case specified by `value`
-                    self.json_name = json_transform(self.json_name, value)
-                case "json_inner":
-                    # Do nothing on members
-                    pass
-                case "json_tag":
-                    # Rename the key that this field will use in json terms
-                    log.debug(f"Applied json tag on {self.json_name}: {value}")
-                    self.tag = value
-                case other:
-                    log.warning(f"Unknown comment command found: {other} with value {value}")
+        self.json_name = comment_commands.apply_name_transform(self.json_name)
+        self.tag = comment_commands.tag
+        self.dont_fail_on_deserialization = comment_commands.dont_fail_on_deserialization
 
     @staticmethod
-    def from_node(node: clang.cindex.Cursor) -> EnumConstant:
+    def from_node(
+        node: clang.cindex.Cursor,
+        comment_commands: CommentCommands,
+    ) -> EnumConstant:
         assert node.type is not None
 
         name = node.spelling
@@ -55,8 +39,9 @@ class EnumConstant:
         enum = EnumConstant(name)
 
         if node.raw_comment is not None:
-            comment_commands = parse_comment_commands(node.raw_comment)
-            enum.apply_comment_commands(comment_commands)
+            comment_commands.parse(node.raw_comment)
+
+        enum.apply_comment_commands(comment_commands)
 
         return enum
 
