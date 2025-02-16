6061d2a7edfd06d6bb2f81c2ee5e30a71e289c79
Kyle Scarmack <scarmackkyle@gmail.com>
2025-02-16 12:23:38 -0500

    Fix: Resolved Opening an usercard from /live tab makes a bad API call #5878

--- src/providers/IvrApi.cpp
+++ src/providers/IvrApi.cpp
@@ -15,11 +15,6 @@ void IvrApi::getSubage(QString userName, QString channelName,
 {
     assert(!userName.isEmpty() && !channelName.isEmpty());
 
-    if (channelName == "/live")
-    {
-        channelName = userName;
-    }
-
     this->makeRequest(
             QString("twitch/subage/%1/%2").arg(userName).arg(channelName), {})
         .onSuccess([successCallback, failureCallback](auto result) {
