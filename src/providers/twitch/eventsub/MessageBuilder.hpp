212a76bc2bc2ce9a54fa3fc044c340fb48825d07
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-02-26 11:48:19 +0100

    feat(eventsub): implement (shared chat) delete

--- src/providers/twitch/eventsub/MessageBuilder.hpp
+++ src/providers/twitch/eventsub/MessageBuilder.hpp
@@ -54,4 +54,10 @@ void makeModerateMessage(
     const lib::payload::channel_moderate::v2::Event &event,
     const lib::payload::channel_moderate::v2::Unban &action);
 
+/// <MODERATOR> deleted message from <USER>[ in <CHANNEL>] saying: <MESSAGE>
+void makeModerateMessage(
+    EventSubMessageBuilder &builder,
+    const lib::payload::channel_moderate::v2::Event &event,
+    const lib::payload::channel_moderate::v2::Delete &action);
+
 }  // namespace chatterino::eventsub
