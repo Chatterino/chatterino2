a915890ff63b2c2efeefaccdce62e2fae9a96a59
Yara <miguyt1@gmail.com>
2025-02-26 18:59:04 +0100

    fix: take back comment in NativeMessagingServer

--- src/singletons/NativeMessaging.cpp
+++ src/singletons/NativeMessaging.cpp
@@ -171,7 +171,7 @@ NativeMessagingServer::~NativeMessagingServer()
     }
     this->thread.requestInterruption();
     this->thread.quit();
-
+    // Most likely, the receiver thread will still wait for a message
     if (!this->thread.wait(250))
     {
         this->thread.terminate();
