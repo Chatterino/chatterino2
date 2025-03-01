2a488ed2c977e55ac6bb007b632ae9ad07b0d686
Nerixyz <nerixdev@outlook.de>
2025-03-01 14:58:07 +0100

    feat(eventsub): implement automod message update

--- src/providers/twitch/TwitchChannel.hpp
+++ src/providers/twitch/TwitchChannel.hpp
@@ -510,6 +510,7 @@ private:
 
     eventsub::SubscriptionHandle eventSubChannelModerateHandle;
     eventsub::SubscriptionHandle eventSubAutomodMessageHoldHandle;
+    eventsub::SubscriptionHandle eventSubAutomodMessageUpdateHandle;
     eventsub::SubscriptionHandle eventSubSuspiciousUserMessageHandle;
 
     friend class TwitchIrcServer;
