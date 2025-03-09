ffe28b88a4450ddc26ff3d820718a8d3da728ef9
8thony <114905842+8thony@users.noreply.github.com>
2025-03-09 15:48:26 +0100

    fix: special shortcut for Windows live notifications (#5975)

--- src/widgets/settingspages/NotificationPage.cpp
+++ src/widgets/settingspages/NotificationPage.cpp
@@ -49,6 +49,16 @@ NotificationPage::NotificationPage()
 #if defined(Q_OS_WIN) || defined(CHATTERINO_WITH_LIBNOTIFY)
                 settings.append(this->createCheckBox(
                     "Show notification", getSettings()->notificationToast));
+#endif
+#ifdef Q_OS_WIN
+                settings.append(this->createCheckBox(
+                    "Create start menu shortcut (requires "
+                    "restart)",
+                    getSettings()->createShortcutForToasts,
+                    "When enabled, a shortcut will be created inside your "
+                    "start menu folder if needed by live notifications."
+                    "\n(On portable mode, this is disabled by "
+                    "default)"));
 
                 auto openIn = settings.emplace<QHBoxLayout>().withoutMargin();
                 {
