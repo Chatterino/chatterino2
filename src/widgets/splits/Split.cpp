fea5f274f9c3ab6f5e5dabe41b24bb4c82d483f2
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-04-07 10:22:53 +0200

    refactor(Split): remove unused `select_randomly` function

--- src/widgets/splits/Split.cpp
+++ src/widgets/splits/Split.cpp
@@ -1642,21 +1642,6 @@ void Split::dropEvent(QDropEvent *event)
         BaseWidget::dropEvent(event);
     }
 }
-template <typename Iter, typename RandomGenerator>
-static Iter select_randomly(Iter start, Iter end, RandomGenerator &g)
-{
-    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
-    std::advance(start, dis(g));
-    return start;
-}
-
-template <typename Iter>
-static Iter select_randomly(Iter start, Iter end)
-{
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    return select_randomly(start, end, gen);
-}
 
 void Split::drag()
 {
