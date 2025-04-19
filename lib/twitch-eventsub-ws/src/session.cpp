a85307f0f8d940f9ff3d4cf2f16f41db87b47758
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-04-19 15:14:19 +0200

    try fix macos build

--- lib/twitch-eventsub-ws/src/session.cpp
+++ lib/twitch-eventsub-ws/src/session.cpp
@@ -18,7 +18,8 @@
 #include <memory>
 #include <unordered_map>
 
-#if __cpp_lib_format >= 201907L
+#if __cpp_lib_format >= 201907L && \
+    !defined(_LIBCPP_AVAILABILITY_HAS_NO_TO_CHARS_FLOATING_POINT)
 #    include <format>
 using std::format;
 #else
