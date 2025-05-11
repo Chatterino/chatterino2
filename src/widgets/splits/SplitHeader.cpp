bd2235aff3834c58924be883a1e5dec81e6f2fc5
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-05-11 12:03:44 +0200

    fix(SplitHeader): don't update header if app is about to quit
                      this change might not be necessary if we don't call
                      any callbacks from network

--- src/widgets/splits/SplitHeader.cpp
+++ src/widgets/splits/SplitHeader.cpp
@@ -848,6 +848,11 @@ void SplitHeader::updateChannelText()
                 NetworkRequest(url, NetworkRequestType::Get)
                     .caller(this)
                     .onSuccess([this](auto result) {
+                        if (isAppAboutToQuit())
+                        {
+                            return;
+                        }
+
                         // NOTE: We do not follow the redirects, so we need to make sure we only treat code 200 as a valid image
                         if (result.status() == 200)
                         {
