095ac38d820c35536a8b4c23180d8bccd265d0f6
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-06-14 21:11:10 +0200

    feat: parallelize `clang-format-all.sh`

--- scripts/clang-format-all.sh
+++ scripts/clang-format-all.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+NUM_FORMAT_JOBS=${NUM_FORMAT_JOBS:-$(nproc)}
+
+if ! command -v parallel >/dev/null 2>&1; then
+    echo "Missing parallel command"
+    exit 1
+fi
+
+if ! command -v clang-format >/dev/null 2>&1; then
+    echo "Missing clang-format command"
+    exit 1
+fi
 
 read -p "Are you sure you want to run clang-format on all source files? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    find src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
-    find tests/src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
-    find benchmarks/src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
-    find mocks/include/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -i {} \;
+    find . \( \
+        -regex '\./src/.*\.\(hpp\|cpp\)' -o \
+        -regex '\./tests/src/.*\.\(hpp\|cpp\)' -o \
+        -regex '\./benchmarks/src/.*\.\(hpp\|cpp\)' -o \
+        -regex '\./mocks/include/.*\.\(hpp\|cpp\)' \
+        \) | parallel --verbose --jobs "$NUM_FORMAT_JOBS" clang-format -i
 fi
