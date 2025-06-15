4761d22e798e3122d14f06a66d156d9c671e0748
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-06-15 12:16:14 +0200

    refactor: move the click effect moving to a protected method
    this means derived classes can play their own click effects when they
    want to simulate a click event

--- src/widgets/buttons/Button.cpp
+++ src/widgets/buttons/Button.cpp
@@ -172,7 +172,7 @@ void Button::mousePressEvent(QMouseEvent *event)
         return;
     }
 
-    this->clickEffects_.emplace_back(event->pos());
+    this->addClickEffect(event->pos());
 
     this->mouseDown_ = true;
 
@@ -227,6 +227,11 @@ void Button::mouseMoveEvent(QMouseEvent *event)
 
         this->update();
     }
+}
+
+void Button::addClickEffect(QPoint position)
+{
+    this->clickEffects_.emplace_back(position);
 }
 
 void Button::onMouseEffectTimeout()
