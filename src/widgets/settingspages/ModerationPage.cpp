87c5d8a9dd26415f849fe4766bfb387a65fe3027
droidicus <droidicus@dr.com>
2025-07-19 10:58:01 -0400

    update formatting

--- src/widgets/settingspages/ModerationPage.cpp
+++ src/widgets/settingspages/ModerationPage.cpp
@@ -165,9 +165,9 @@ ModerationPage::ModerationPage()
         logTimestampFormat->setToolTip("a = am/pm, zzz = milliseconds");
         logsTimestampFormatLayout.append(logTimestampFormat);
 
-        QCheckBox *logTimestampFromMessage =
-            this->createCheckBox("Use server recieve timestamp from message for logging",
-                                 getSettings()->logTimestampFromMessage);
+        QCheckBox *logTimestampFromMessage = this->createCheckBox(
+            "Use server recieve timestamp from message for logging",
+            getSettings()->logTimestampFromMessage);
 
         logTimestampFromMessage->setEnabled(getSettings()->enableLogging);
         logs.append(logTimestampFromMessage);
