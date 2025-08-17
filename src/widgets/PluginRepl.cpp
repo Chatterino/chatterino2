f8018e6d63a62f7c62093356bd3979342f9fe92c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-17 13:43:12 +0200

    remove unused pixmap button include

--- src/widgets/PluginRepl.cpp
+++ src/widgets/PluginRepl.cpp
@@ -11,7 +11,6 @@
 #    include "singletons/Resources.hpp"
 #    include "singletons/Settings.hpp"
 #    include "singletons/Theme.hpp"
-#    include "widgets/buttons/PixmapButton.hpp"
 #    include "widgets/buttons/SvgButton.hpp"
 
 #    include <QBoxLayout>
