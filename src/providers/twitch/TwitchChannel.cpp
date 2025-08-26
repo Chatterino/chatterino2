b661cfdaac604a5905913234f796f230f4f94d37
Brian <18603393+brian6932@users.noreply.github.com>
2025-08-26 08:39:56 -0400

    Remove Qt 6.1.0 ifdef

--- src/providers/twitch/TwitchChannel.cpp
+++ src/providers/twitch/TwitchChannel.cpp
@@ -79,11 +79,7 @@ bool isUnknownCommand(const QString &text)
 using detail::isUnknownCommand;
 
 namespace {
-#if QT_VERSION < QT_VERSION_CHECK(6, 1, 0)
-const QString MAGIC_MESSAGE_SUFFIX = QString((const char *)u8" \u034f");
-#else
 const QString MAGIC_MESSAGE_SUFFIX = u" \u034f"_s;
-#endif
 constexpr int CLIP_CREATION_COOLDOWN = 5000;
 const QString CLIPS_LINK("https://clips.twitch.tv/%1");
 const QString CLIPS_FAILURE_CLIPS_UNAVAILABLE_TEXT(
