19886a2e70e55cd8b61b201be0664e64d7866741
Jacob Thompson <jacobalthompson@gmail.com>
2025-09-28 10:23:58 -0700

    remove stylesheet

--- src/widgets/Label.cpp
+++ src/widgets/Label.cpp
@@ -161,13 +161,6 @@ void Label::paintEvent(QPaintEvent * /*event*/)
         this->markdownDocument_->setTextWidth(textRect.width());
         this->markdownDocument_->setDefaultFont(
             getApp()->getFonts()->getFont(this->getFontStyle(), this->scale()));
-
-        QString colorName = this->palette().windowText().color().name();
-        this->markdownDocument_->setDefaultStyleSheet(
-            QString("body { color: %1; } p { margin: 0; } h1, h2, h3, h4, h5, "
-                    "h6 { margin: 0; }")
-                .arg(colorName));
-
         this->markdownDocument_->setMarkdown(this->text_);
 
         painter.save();
