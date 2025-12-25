1fa0d6c19103603e9ff53551b1f46d368738f427
Jacob Thompson <jacobalthompson@gmail.com>
2025-12-25 09:40:58 -0800

    documentation

--- src/widgets/helper/CmdDeleteKeyFilter.hpp
+++ src/widgets/helper/CmdDeleteKeyFilter.hpp
@@ -2,6 +2,8 @@
 
 #include <QObject>
 
+/// CmdDeleteKeyFilter is an event filter for QTextEdit objects
+///  to handle the behavior of the Command + Delete shortcut on macOS
 class CmdDeleteKeyFilter : public QObject
 {
     Q_OBJECT
