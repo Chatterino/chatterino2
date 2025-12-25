8907a8be3ffd3528a067567164a1247186e91b78
Jacob Thompson <jacobalthompson@gmail.com>
2025-12-25 09:28:06 -0800

    fix nestiness

--- src/widgets/helper/CmdDeleteKeyFilter.cpp
+++ src/widgets/helper/CmdDeleteKeyFilter.cpp
@@ -12,26 +12,34 @@ CmdDeleteKeyFilter::CmdDeleteKeyFilter(QObject *parent)
 bool CmdDeleteKeyFilter::eventFilter(QObject *obj, QEvent *event)
 {
 #ifdef Q_OS_MACOS
-    if (event->type() == QEvent::KeyPress)
+    if (event->type() != QEvent::KeyPress)
     {
-        auto *textEdit = qobject_cast<QTextEdit *>(obj);
-        const auto *keyEvent = dynamic_cast<QKeyEvent *>(event);
-        if (textEdit && keyEvent)  // types are verified
+        return false;
+    }
+
+    auto *textEdit = qobject_cast<QTextEdit *>(obj);
+    const auto *keyEvent = dynamic_cast<QKeyEvent *>(event);
+
+    if (!textEdit || !keyEvent)
+    {
+        return false;
+    }
+
+    if (keyEvent->modifiers() == Qt::ControlModifier &&
+        keyEvent->key() == Qt::Key_Backspace)
+    {
+        QTextCursor cursor = textEdit->textCursor();
+
+        if (!cursor.hasSelection())
         {
-            if (keyEvent->modifiers() == Qt::ControlModifier &&
-                keyEvent->key() == Qt::Key_Backspace)
-            {
-                QTextCursor cursor = textEdit->textCursor();
-                if (!cursor.hasSelection())
-                {
-                    cursor.movePosition(QTextCursor::StartOfLine,
-                                        QTextCursor::KeepAnchor);
-                }
-                cursor.removeSelectedText();
-                textEdit->setTextCursor(cursor);
-                return true;
-            }
+            cursor.movePosition(QTextCursor::StartOfLine,
+                                QTextCursor::KeepAnchor);
         }
+
+        cursor.removeSelectedText();
+        textEdit->setTextCursor(cursor);
+
+        return true;
     }
 #endif
     return false;
