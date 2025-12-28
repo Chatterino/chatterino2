869aba3df8ee9ada9baa6e608da03bdf78e835e8
drogeek <bilbo0o0the0o0hobbit@gmail.com>
2025-12-28 10:43:56 +0100

    feat: Automatically refresh chatter list

--- src/widgets/ChatterListWidget.hpp
+++ src/widgets/ChatterListWidget.hpp
@@ -17,6 +17,22 @@ public:
     ChatterListWidget(const TwitchChannel *twitchChannel, QWidget *parent);
 
     Q_SIGNAL void userClicked(QString userLogin);
+
+private:
+    const TwitchChannel *twitchChannel_ = nullptr;
+    QLabel *loadingLabel_ = nullptr;
+    QVBoxLayout *dockVbox_ = nullptr;
+    QLineEdit *searchBar_ = nullptr;
+    QListWidget *resultList_ = nullptr;
+    QListWidget *chattersList_ = nullptr;
+
+    QStringList chatterList_;
+    QStringList modChatters_;
+    QStringList vipChatters_;
+
+    void refresh();
+    void setupUi();
+    void clearUi();
 };
 
 }  // namespace chatterino
