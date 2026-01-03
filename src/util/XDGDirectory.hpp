6b8cecf9c69e99e4a718deaf33b5d53fe319dfda
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-01-03 12:05:58 +0100

    feat(xdg): return only base or user directories
    Our current `getXDGDirectories` implementation returns both the user
    _and_ system directory for the given user type.
    For some use cases (e.g. dictionaries), we want to have more granural
    control.
    
    I have split up the implementation into two separate functions:
     - `getXDGBaseDirectories` which returns the base directories from
       XDG_CONFIG_DIRS
     - `getXDGUserDirectories` which returns the user directory from
       XDG_CONFIG_HOME

--- src/util/XDGDirectory.hpp
+++ src/util/XDGDirectory.hpp
@@ -2,14 +2,24 @@
 
 #include <QStringList>
 
+#include <cstdint>
+
 namespace chatterino {
 
 #if defined(Q_OS_UNIX) and !defined(Q_OS_DARWIN)
 
-enum class XDGDirectoryType {
+enum class XDGDirectoryType : std::uint8_t {
+    /// The values from XDG_CONFIG_HOME and XDG_CONFIG_DIRS.
     Config,
+    /// The values from XDG_DATA_HOME and XDG_DATA_DIRS
     Data,
 };
+
+/// Return the directories from the base environment variables only (e.g. XDG_CONFIG_DIRS)
+QStringList getXDGBaseDirectories(XDGDirectoryType directory);
+
+/// Return the directories from the user environment variables only (e.g. XDG_CONFIG_HOME)
+QStringList getXDGUserDirectories(XDGDirectoryType directory);
 
 /// getXDGDirectories returns a list of directories given a directory type
 ///
