6935c7d7a088cffed0a58d112ce529f94df31eff
pajlada <rasmus.karlsson@pajlada.com>
2026-01-04 14:23:08 +0100

    chore: reduce wait interval in plugin test (#6708)
    Reviewed-by: Mm2PL <mm2pl+gh@kotmisia.pl>

--- tests/src/Plugins.cpp
+++ tests/src/Plugins.cpp
@@ -34,6 +34,7 @@
 
 using namespace chatterino;
 using chatterino::mock::MockChannel;
+using namespace std::chrono_literals;
 
 namespace {
 
@@ -626,7 +627,7 @@ TEST_F(PluginTest, testTimerRec)
         end
         c2.later(f, 1)
     )lua");
-    waiter.waitForRequest();
+    waiter.waitForRequest(1ms);
 }
 
 TEST_F(PluginTest, tryCallTest)
