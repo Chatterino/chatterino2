ffa4b29002d96252faec589a7aeeeb1c3d5f7d8f
Nerixyz <nerixdev@outlook.de>
2026-01-22 20:20:20 +0100

    refactor: move `PluginMeta` to its own file

--- src/controllers/plugins/Plugin.hpp
+++ src/controllers/plugins/Plugin.hpp
@@ -7,8 +7,7 @@
 #ifdef CHATTERINO_HAVE_PLUGINS
 #    include "controllers/plugins/api/EventType.hpp"
 #    include "controllers/plugins/api/HTTPRequest.hpp"
-#    include "controllers/plugins/LuaUtilities.hpp"
-#    include "controllers/plugins/PluginPermission.hpp"
+#    include "controllers/plugins/PluginMeta.hpp"
 
 #    include <boost/signals2/signal.hpp>
 #    include <QDir>
@@ -31,45 +30,6 @@ enum class LogLevel;
 }  // namespace chatterino::lua::api
 
 namespace chatterino {
-
-struct PluginMeta {
-    // for more info on these fields see docs/plugin-info.schema.json
-
-    // display name of the plugin
-    QString name;
-
-    // description shown to the user
-    QString description;
-
-    // plugin authors shown to the user
-    std::vector<QString> authors;
-
-    // license name
-    QString license;
-
-    // version of the plugin
-    semver::version version;
-
-    // optionally a homepage link
-    QString homepage;
-
-    // optionally tags that might help in searching for the plugin
-    std::vector<QString> tags;
-
-    std::vector<PluginPermission> permissions;
-
-    // errors that occurred while parsing info.json
-    std::vector<QString> errors;
-
-    bool isValid() const
-    {
-        return this->errors.empty();
-    }
-
-    explicit PluginMeta(const QJsonObject &obj);
-    // This is for tests
-    PluginMeta() = default;
-};
 
 class Plugin
 {
