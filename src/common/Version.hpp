fc8771c2929a56bd78182510a9b38af1834f32d8
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-02-07 15:39:15 +0100

    add comment to the isNightly function

--- src/common/Version.hpp
+++ src/common/Version.hpp
@@ -63,6 +63,9 @@ public:
     // Returns an extra string about this specific build
     const QString &extraString() const;
 
+    /// Returns true if this build is classified as a "nightly" (i.e. dev or unstable) build.
+    ///
+    /// This is controlled by the CMake parameter CHATTERINO_NIGHTLY_BUILD, which defaults to off.
     bool isNightly() const;
 
 #ifdef Q_OS_WIN
