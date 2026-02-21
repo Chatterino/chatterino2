7ebf5992eb3013e9217eac1e5aa8e6027ade510c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-02-21 13:14:55 +0100

    comment Backup.hpp

--- src/util/Backup.hpp
+++ src/util/Backup.hpp
@@ -29,6 +29,7 @@ enum class BackupState : uint8_t {
     BadContents,
 };
 
+/// A backup file (e.g. `settings.json.bkp-7`) and its state.
 struct BackupFile {
     std::filesystem::path path;
     std::filesystem::path dstPath;
@@ -37,6 +38,7 @@ struct BackupFile {
     BackupState state = BackupState::Ok;
 };
 
+/// Specifies where to load the file from and descriptions about the file and its contents.
 struct FileData {
     /// "settings.json", "window-layout.json"
     QString fileName;
@@ -47,9 +49,13 @@ struct FileData {
     QString fileDescription;
 };
 
+/// Find a list of backups for the given `filename` in the given `directory`.
 std::vector<BackupFile> findBackupsFor(const QString &directory,
                                        const QString &filename);
 
+/// Attempt to load the file described in `fileData` using the `load` param.
+///
+/// If the load fails and any backups are available, spawn a restore backups dialog.
 void loadWithBackups(const FileData &fileData,
                      const std::function<ExpectedStr<void>()> &load);
 
