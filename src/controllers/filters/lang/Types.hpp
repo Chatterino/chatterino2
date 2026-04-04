5a984def830f8b2d4989c1a423a6dc11d9341a20
Arne <78976058+4rneee@users.noreply.github.com>
2026-04-04 09:30:53 +0000

    refactor: remove operator!= (#6891)
    Reviewed-by: pajlada <rasmus.karlsson@pajlada.com>

--- src/controllers/filters/lang/Types.hpp
+++ src/controllers/filters/lang/Types.hpp
@@ -42,9 +42,6 @@ struct TypeClass {
     bool operator==(Type t) const;
     bool operator==(const TypeClass &t) const;
     bool operator==(const IllTyped &t) const;
-    bool operator!=(Type t) const;
-    bool operator!=(const TypeClass &t) const;
-    bool operator!=(const IllTyped &t) const;
 };
 
 struct IllTyped {
