fea034b57930bbc114b198e9fbaca0c7f8215b17
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-04-18 12:31:32 +0200

    add missing copyright/spdx header

--- src/singletons/FileLogger.cpp
+++ src/singletons/FileLogger.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+//
+// SPDX-License-Identifier: MIT
+
 #include "singletons/FileLogger.hpp"
 
 #include "common/Env.hpp"
