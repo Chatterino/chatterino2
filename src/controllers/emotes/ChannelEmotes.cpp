e18006c7c8647718849e5938b3653caccfee2f82
Nerixyz <nerixdev@outlook.de>
2026-05-01 22:12:15 +0200

    chore: more stuff happened upstream

--- src/controllers/emotes/ChannelEmotes.cpp
+++ src/controllers/emotes/ChannelEmotes.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+//
+// SPDX-License-Identifier: MIT
+
 #include "controllers/emotes/ChannelEmotes.hpp"
 
 #include "controllers/emotes/EmoteController.hpp"
--- src/controllers/emotes/EmoteProvider.cpp
+++ src/controllers/emotes/EmoteProvider.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+//
+// SPDX-License-Identifier: MIT
+
 #include "controllers/emotes/EmoteProvider.hpp"
 
 #include "controllers/emotes/ChannelEmotes.hpp"
