597a2c57dd9ca9ac82595e49d53dbf631ded54cb
pajlada <rasmus.karlsson@pajlada.com>
2026-07-11 13:08:33 +0200

    reuse: annotate some more source files (#7086)

--- src/controllers/plugins/PluginMeta.cpp
+++ src/controllers/plugins/PluginMeta.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+// SPDX-FileCopyrightText: 2023 Contributors to Chatterino <https://chatterino.com>
 //
 // SPDX-License-Identifier: MIT
 
