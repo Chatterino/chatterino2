3f481749795afd72217d40048af3621c5a97dc8e
Nerixyz <nerixdev@outlook.de>
2026-07-29 17:58:37 +0200

    build(windows): Enable segment heap if possible

---
+++ cmake/EnableSegmentHeap.cmake
@@ -0,0 +1,17 @@
+if (NOT WIN32 OR MINGW)
+    return()
+endif()
+
+if(NOT DEFINED ENV{VSINSTALLDIR})
+    message(WARNING "Missing VSINSTALLDIR environment variable - not enabling segment heap.")
+    return()
+endif()
+
+set(segment_heap_path "$ENV{VSINSTALLDIR}Common7/IDE/CommonExtensions/Microsoft/CMake/cmake/Microsoft/SegmentHeap.cmake")
+if (NOT EXISTS "${segment_heap_path}")
+    message(STATUS "Missing '${segment_heap_path}'. Segment heap will be disabled - consider updating Visual Studio.")
+    return()
+endif()
+
+message(STATUS "Including segment heap script from '${segment_heap_path}'.")
+include("${segment_heap_path}")
