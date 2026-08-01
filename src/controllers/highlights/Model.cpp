82f268133e0acd90fbba707ae58cfca05d97b688
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-08-01 14:26:26 +0200

    progress: add missing copyright headers

--- src/controllers/highlights/Model.cpp
+++ src/controllers/highlights/Model.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+//
+// SPDX-License-Identifier: MIT
+
 #include "controllers/highlights/Model.hpp"
 
 #include "Application.hpp"
