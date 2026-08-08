f154a120caf8ae516fe1e3ec84a7d4c81945d2da
pajlada <rasmus.karlsson@pajlada.com>
2026-08-08 15:07:12 +0200

    fix(plugins): don't crash on invalid plugin meta (#7166)
    This makes our plugin storage use a variant for the different plugin
    states:
    
    - UnloadedPlugin (used if the plugin's meta was invalid)
    - PluginPtr (a normal valid plugin, which may still contain lua errors)
    
    How to test the crash that doesn't crash with this PR:
    Add an invalid permission in your `info.json` file
    
    Co-authored-by: Mm2PL <mm2pl+gh@kotmisia.pl>

--- src/controllers/plugins/PluginController.cpp
+++ src/controllers/plugins/PluginController.cpp
@@ -32,6 +32,7 @@
 #    include "singletons/Paths.hpp"
 #    include "singletons/Settings.hpp"
 #    include "singletons/WindowManager.hpp"
+#    include "util/Variant.hpp"
 #    include "widgets/splits/SplitContainer.hpp"
 #    include "widgets/Window.hpp"
 
@@ -131,9 +132,10 @@ bool PluginController::tryLoadFromDir(const QDir &pluginDir)
         {
             qCWarning(chatterinoLua) << "- " << why;
         }
-        auto plugin = std::make_unique<Plugin>(pluginDir.dirName(), nullptr,
-                                               meta, pluginDir);
-        this->plugins_.insert({pluginDir.dirName(), std::move(plugin)});
+
+        this->plugins_.insert(
+            {pluginDir.dirName(),
+             UnloadedPlugin{pluginDir.dirName(), meta, pluginDir}});
         return false;
     }
     this->load(index, pluginDir, meta);
@@ -357,11 +359,26 @@ bool PluginController::reload(const QString &id)
         return false;
     }
 
-    for (const auto &[cmd, _] : it->second->ownedCommands)
+    if (const auto *oPlugin = std::get_if<PluginPtr>(&it->second))
     {
-        getApp()->getCommands()->unregisterPluginCommand(cmd);
+        const auto &plugin = *oPlugin;
+
+        for (const auto &[cmd, _] : plugin->ownedCommands)
+        {
+            getApp()->getCommands()->unregisterPluginCommand(cmd);
+        }
     }
-    QDir loadDir = it->second->loadDirectory_;
+
+    const auto loadDir = std::visit(variant::Overloaded{
+                                        [&](const PluginPtr &plugin) {
+                                            return plugin->loadDirectory_;
+                                        },
+                                        [&](const UnloadedPlugin &plugin) {
+                                            return plugin.loadDirectory;
+                                        },
+                                    },
+                                    it->second);
+
     // Since Plugin owns the state, it will clean up everything related to it
     this->plugins_.erase(id);
     this->queueChangeNotification();
@@ -372,8 +389,15 @@ bool PluginController::reload(const QString &id)
 QString PluginController::tryExecPluginCommand(const QString &commandName,
                                                const CommandContext &ctx)
 {
-    for (auto &[name, plugin] : this->plugins_)
+    for (const auto &[name, anyPlugin] : this->allPlugins())
     {
+        const auto *oPl = std::get_if<PluginPtr>(&anyPlugin);
+        if (oPl == nullptr)
+        {
+            continue;
+        }
+        const auto &plugin = *oPl;
+
         if (auto it = plugin->ownedCommands.find(commandName);
             it != plugin->ownedCommands.end())
         {
@@ -421,8 +445,15 @@ Plugin *PluginController::getPluginByStatePtr(lua_State *L)
     auto *mainL = lua_tothread(L, -1);
     lua_pop(L, 1);
     L = mainL;
-    for (auto &[name, plugin] : this->plugins_)
+    for (const auto &[name, anyPlugin] : this->allPlugins())
     {
+        const auto *oPl = std::get_if<PluginPtr>(&anyPlugin);
+        if (oPl == nullptr)
+        {
+            continue;
+        }
+        const auto &plugin = *oPl;
+
         if (plugin->state_ == L)
         {
             return plugin.get();
@@ -431,8 +462,19 @@ Plugin *PluginController::getPluginByStatePtr(lua_State *L)
     return nullptr;
 }
 
-const std::map<QString, std::unique_ptr<Plugin>> &PluginController::plugins()
-    const
+void PluginController::forEachPlugin(
+    FunctionRef<void(const std::unique_ptr<Plugin> &)> cb) const
+{
+    for (const auto &[_, anyPlugin] : this->allPlugins())
+    {
+        if (const auto *plugin = std::get_if<PluginPtr>(&anyPlugin))
+        {
+            cb(*plugin);
+        }
+    }
+}
+
+const std::map<QString, AnyPlugin> &PluginController::allPlugins() const
 {
     return this->plugins_;
 }
@@ -443,8 +485,16 @@ std::pair<bool, QStringList> PluginController::updateCustomCompletions(
 {
     QStringList results;
 
-    for (const auto &[name, pl] : this->plugins())
+    for (const auto &[name, anyPlugin] : this->allPlugins())
     {
+        const auto *oPl = std::get_if<PluginPtr>(&anyPlugin);
+        if (oPl == nullptr)
+        {
+            continue;
+        }
+
+        const auto &pl = *oPl;
+
         if (!pl->error().isNull() || pl->state_ == nullptr)
         {
             continue;
