93e024388ebdc9bc1b15181779d8214bcd37acbe
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-08-09 15:12:37 +0200

    feat(plugins): add setter to MessageElement words

--- src/messages/MessageElement.hpp
+++ src/messages/MessageElement.hpp
@@ -310,6 +310,14 @@ public:
         return this->words_;
     }
 
+    /// Set the words of this element.
+    ///
+    /// Normally set during construction, but can be overridden in plugins.
+    void setWords(const QStringList &newWords)
+    {
+        this->words_ = newWords;
+    }
+
 protected:
     QStringList words_;
 
@@ -356,6 +364,14 @@ public:
     QStringList words() const
     {
         return this->words_;
+    }
+
+    /// Set the words of this element.
+    ///
+    /// Normally set during construction, but can be overridden in plugins.
+    void setWords(const QStringList &newWords)
+    {
+        this->words_ = newWords;
     }
 
 private:
