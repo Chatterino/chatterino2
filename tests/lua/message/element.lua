58f9ff32a29d476dce740bd125f824b1f6ffdf42
b_drink <166573010+brilliantdrink@users.noreply.github.com>
2026-08-15 16:06:33 -0700

    fix(plugins): Message:append_element(MessageElement) unreachable (#7176)
    When passing a `MessageElement` instead of an init table to
    `Message:append_element` in a lua plugin, it would error with "Invalid
    message type" suggesting that it calls `elementFromTable`, i.e. acts as
    if a `MessageElementInit` table was passed.
    
    ```
    
    Reviewed-by: Nerixyz <nerixdev@outlook.de>
    Reviewed-by: pajlada <rasmus.karlsson@pajlada.com>

---
+++ tests/lua/message/element.lua
@@ -0,0 +1,48 @@
+-- SPDX-FileCopyrightText: 2026 Contributors to Chatterino <https://chatterino.com>
+--
+-- SPDX-License-Identifier: CC0-1.0
+
+local chan = c2.Channel.by_name("mm2pl")
+assert(chan)
+
+local tests = {
+    append_element = function()
+        local msg = c2.Message.new({
+            elements = {
+                {
+                    type = "text",
+                    text = "abcde",
+                },
+                { type = "twitch-moderation" },
+            },
+        })
+        chan:add_message(msg)
+        local new_msg = c2.Message.new(msg)
+        local append_init_ok, append_init_err = pcall(function()
+            new_msg:append_element({
+                type = 'text',
+                text = 'foo'
+            })
+        end)
+        assert(append_init_ok)
+        assert(new_msg:elements()[1])
+        assert(new_msg:elements()[1].type == 'text' and new_msg:elements()[1].words[1] == 'foo')
+        local append_element_ok, append_element_err = pcall(function()
+            for index, element in ipairs(msg:elements()) do
+                new_msg:append_element(element)
+            end
+        end)
+        assert(append_element_ok)
+        assert(new_msg:elements()[2])
+        assert(new_msg:elements()[2].type == 'text' and new_msg:elements()[2].words[1] == 'abcde')
+    end,
+}
+
+for name, fn in pairs(tests) do
+    chan:clear_messages() -- start off without any messages
+
+    local ok, res = pcall(fn)
+    if not ok then
+        error(name .. " failed: " .. res)
+    end
+end
