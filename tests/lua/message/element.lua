9f523a5d3f5bbcbf1272aa5f13ceff03e25046db
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-08-22 15:35:25 +0200

    add plugin api

--- tests/lua/message/element.lua
+++ tests/lua/message/element.lua
@@ -36,6 +36,30 @@ local tests = {
         assert(new_msg:elements()[2])
         assert(new_msg:elements()[2].type == 'text' and new_msg:elements()[2].words[1] == 'abcde')
     end,
+    exhaustive_flags = function()
+        local msg = c2.Message.new({
+            elements = {
+                {
+                    type = "text",
+                    text = "a",
+                    -- default
+                },
+                {
+                    type = "text",
+                    text = "b",
+                    exhaustive_flags = false,
+                },
+                {
+                    type = "text",
+                    text = "c",
+                    exhaustive_flags = true,
+                },
+            },
+        })
+        assert(msg:elements()[1].type == "text" and msg:elements()[1].exhaustive_flags == false)
+        assert(msg:elements()[2].type == "text" and msg:elements()[2].exhaustive_flags == false)
+        assert(msg:elements()[3].type == "text" and msg:elements()[3].exhaustive_flags == true)
+    end,
 }
 
 for name, fn in pairs(tests) do
