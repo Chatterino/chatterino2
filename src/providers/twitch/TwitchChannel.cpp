dbc3973349c817bfc16cefc0a07ed9dea71194b9
Nerixyz <nerixdev@outlook.de>
2026-08-28 14:57:53 +0200

    refactor: use string views for emote lookup (#7200)
    #7194 added view types for our aliases and this PR uses them for emote
    lookup. Most of this only changes the parameter types. Notably, it makes
    `MessageBuilder::tryAppendEmote` and the input highlighter's `isEmote`
    accept a view.
    
    It's not much yet, since other parts still expect owned strings.
    
    Reviewed-by: pajlada <rasmus.karlsson@pajlada.com>

--- src/providers/twitch/TwitchChannel.cpp
+++ src/providers/twitch/TwitchChannel.cpp
@@ -1052,7 +1052,7 @@ SharedAccessGuard<const TwitchChannel::StreamStatus>
     return this->streamStatus_.accessConst();
 }
 
-std::optional<EmotePtr> TwitchChannel::twitchEmote(const EmoteName &name) const
+std::optional<EmotePtr> TwitchChannel::twitchEmote(EmoteNameView name) const
 {
     auto emotes = this->localTwitchEmotes();
     auto it = emotes->find(name);
@@ -1064,7 +1064,7 @@ std::optional<EmotePtr> TwitchChannel::twitchEmote(const EmoteName &name) const
     return it->second;
 }
 
-std::optional<EmotePtr> TwitchChannel::bttvEmote(const EmoteName &name) const
+std::optional<EmotePtr> TwitchChannel::bttvEmote(EmoteNameView name) const
 {
     auto emotes = this->bttvEmotes_.get();
     auto it = emotes->find(name);
@@ -1076,7 +1076,7 @@ std::optional<EmotePtr> TwitchChannel::bttvEmote(const EmoteName &name) const
     return it->second;
 }
 
-std::optional<EmotePtr> TwitchChannel::ffzEmote(const EmoteName &name) const
+std::optional<EmotePtr> TwitchChannel::ffzEmote(EmoteNameView name) const
 {
     auto emotes = this->ffzEmotes_.get();
     auto it = emotes->find(name);
@@ -1088,7 +1088,7 @@ std::optional<EmotePtr> TwitchChannel::ffzEmote(const EmoteName &name) const
     return it->second;
 }
 
-std::optional<EmotePtr> TwitchChannel::seventvEmote(const EmoteName &name) const
+std::optional<EmotePtr> TwitchChannel::seventvEmote(EmoteNameView name) const
 {
     auto emotes = this->seventvEmotes_.get();
     auto it = emotes->find(name);
