72abbb7482ca89ed5126f2a6c432bf7e9ffe2de7
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-09-05 13:03:01 +0200

    progress

--- src/controllers/highlights/types/AutomodCaughtHighlight.hpp
+++ src/controllers/highlights/types/AutomodCaughtHighlight.hpp
@@ -39,7 +39,6 @@ struct AutomodCaughtHighlight {
     static constexpr bool ENABLED_BY_DEFAULT = true;
     static constexpr bool SHOW_IN_MENTIONS_DEFAULT = false;
     static constexpr bool ALERT_DEFAULT = false;
-    static constexpr bool SUPPORT_PLAY_SOUND = false;
     static constexpr QColor BACKGROUND_COLOR_DEFAULT = QColor(64, 64, 64, 127);
 
     QString name;
